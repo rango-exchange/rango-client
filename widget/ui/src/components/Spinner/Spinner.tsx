ed023d79e1d70c521fcf7154b934e30cbf6c340b
mikasackermn <mikasa@rango.exchange>
2023-01-16 17:38:37 +0330

    update spinner

--- widget/ui/src/components/Spinner/Spinner.tsx
+++ widget/ui/src/components/Spinner/Spinner.tsx
@@ -8,18 +8,65 @@ const spin = keyframes({
 });
 
 const SpinnerContainer = styled('div', {
-  width: '$4',
-  height: '$4',
+ 
   borderRadius: '50%',
   position: 'relative',
-  border: `2px solid $neutral-300`,
-  borderRight: `2px solid $primary-500`,
-  animation: `${spin} 1s linear infinite`,
-  margin: `0px $m`,
-});
+  border: `2px solid $neutrals300`,
+  borderRight: `2px solid`,
+  animation: `${spin} 1.5s linear infinite`,
+  margin: `0px $8`,
+  variants: {
+    color: {
+      primary: {
+        borderRightColor:'$primary'
+      },
+      error: {
+        borderRightColor:'$error'
+      },
+      warning: {
+        borderRightColor:'$warning'
+      },
+      success: {
+        borderRightColor:'$success'
+      },
+      black:{
+        borderRightColor:'$black'
+      },
+      white:{
+        borderRightColor:'$white'
+      }
+    },
 
-function Spinner() {
-  return <SpinnerContainer />;
+    size:{
+      16:{
+        width: '$16',
+        height: '$16', 
+      },
+      20:{
+        width: '$20',
+        height: '$20', 
+      },
+      24:{
+        width: '$24',
+        height: '$24', 
+      }
+    }
+  },
+
+});
+export interface PropTypes {
+  size?: 16 | 20 | 24; 
+  color?: 'primary' | 'error' | 'warning' | 'success' | 'black' | 'white'; 
+}
+
+function Spinner({
+  size =16 , 
+  color= 'primary' ,
+
+}:PropTypes) {
+  console.log({size, color});
+  
+  return <SpinnerContainer  size={size} color={color}/>;
 }
 
 export default Spinner;
