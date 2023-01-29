f76c24b3a8fb006740c857d9afff70cf1ae6611e
mikasackermn <mikasa@rango.exchange>
2023-01-29 19:31:49 +0330

    Revert "Add drawer component"
    This reverts commit acd2ac91292d9813207127d61a68c203fbf42167.

--- widget/ui/src/components/Modal/Modal.stories.tsx
+++ widget/ui/src/components/Modal/Modal.stories.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { ComponentMeta } from '@storybook/react';
 import { Modal, PropTypes } from './Modal';
 
@@ -6,17 +6,4 @@ export default { name: 'Modal', component: Modal } as ComponentMeta<
   typeof Modal
 >;
 
-export const Main = (args: PropTypes) => {
-  const [open, setOpen] = useState<boolean>(false);
-  return (
-    <div>
-      <button onClick={() => setOpen(true)}>Open Modal</button>
-      <Modal
-        {...args}
-        open={open}
-        title="I'm modal"
-        onClose={() => setOpen(false)}
-      />
-    </div>
-  );
-};
+export const Main = (args: PropTypes) => <Modal {...args} />;
