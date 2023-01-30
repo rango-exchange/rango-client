879fd3a25ab37d32aff92ffecb4ad08e024f15ce
samobasquiat <samo.basquiat@rango.exchange>
2023-01-30 08:49:05 +0330

    add solana network to frontier

--- wallets/provider-frontier/src/helpers.ts
+++ wallets/provider-frontier/src/helpers.ts
@@ -1,14 +1,35 @@
-import { Network } from '@rangodev/wallets-shared';
+import { Network, ProviderConnectResult } from '@rangodev/wallets-shared';
 
 export function frontier() {
-  const { frontier } : any = window;
+  const { frontier }: any = window;
 
   const instances = new Map();
 
   if (frontier?.ethereum) instances.set(Network.ETHEREUM, frontier?.ethereum);
   if (!!frontier?.solana) instances.set(Network.SOLANA, frontier?.solana);
+  if (!!frontier?.cosmos) instances.set(Network.COSMOS, frontier?.cosmos);
+
 
   if (instances.size === 0) return null;
 
   return instances;
+}
+
+export async function getSolanaAccounts(
+  instance: any
+): Promise<ProviderConnectResult[]> {
+  const solanaInstance = await instance.get(Network.SOLANA);
+  const results: ProviderConnectResult[] = [];
+
+  if (!!solanaInstance) {
+    await solanaInstance.connect();
+    const account = solanaInstance.publicKey.toString();
+
+    results.push({
+      accounts: account ? [account] : [],
+      chainId: Network.SOLANA,
+    });
+  }
+
+  return results;
 }
