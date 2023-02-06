5eca109a78fc3fb8f55635e36240ea8e9d3d97e3
mikasackermn <120931936+mikasackermn@users.noreply.github.com>
2023-02-06 12:59:58 +0000

    wallet adapter (#14)
    * added wallet adapter lib
    
    * update wallet adapter
    
    * update adapter
    
    * get wallets list from providers
    
    * use create portal for modal component
    
    * Add drawer component
    
    * Add status drawer component
    
    * Revert "Add status drawer component"
    
    This reverts commit 66697d0d17f7020dfa426afdebc8085091fc7c38.
    
    * Revert "Add drawer component"
    
    This reverts commit acd2ac91292d9813207127d61a68c203fbf42167.
    
    * fixed problems
    
    * add rango sdk to wallet adapter demo and env file
    
    * update
    
    * use @rangodev/wallets-shared for types and fix env
    
    * add .env to gitignore
    
    * delete .env
    
    * update
    
    * add a dev command
    
    ---------
    
    Co-authored-by: Eren Yeager <eren@rango.exchange>

---
+++ wallets/wallet-adapter/src/modal.tsx
@@ -0,0 +1,44 @@
+import { ConnectWalletsModal } from '@rangodev/ui';
+import { useWallets } from '@rangodev/wallets-core';
+import { WalletType } from '@rangodev/wallets-shared';
+import React, { useState } from 'react';
+import { getlistWallet } from './helpers';
+export interface PropTypes {
+  open: boolean;
+  onClose: () => void;
+  list: WalletType[];
+}
+
+function Modal(props: PropTypes) {
+  const { open, onClose, list } = props;
+  const [walletMessage, setWalletErrorMessage] = useState('');
+
+  const { state, disconnect, getWalletInfo, connect } = useWallets();
+  const allWallets = getlistWallet(state, getWalletInfo, list);
+
+  const onSelectWallet = async (type: WalletType) => {
+    const wallet = state(type);
+    try {
+      if (wallet.connected) {
+        await disconnect(type);
+      } else {
+        await connect(type);
+      }
+    } catch (e) {
+      if (e instanceof Error) {
+        setWalletErrorMessage('Error: ' + e.message);
+      }
+    }
+  };
+  return (
+    <ConnectWalletsModal
+      list={allWallets}
+      open={open}
+      onClose={onClose}
+      onSelect={onSelectWallet}
+      error={walletMessage}
+    />
+  );
+}
+
+export default Modal;
