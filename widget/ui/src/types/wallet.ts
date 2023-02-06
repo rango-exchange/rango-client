b859ea09dc7b701c7ecc2b3f3af456f10ae5f6e6
mikasackermn <mikasa@rango.exchange>
2023-02-06 08:50:56 +0000

    Integrate Wallets into Widget Lib

--- widget/ui/src/types/wallet.ts
+++ widget/ui/src/types/wallet.ts
@@ -1,3 +1,5 @@
+import { WalletType } from '@rangodev/wallets-shared';
+
 export enum WalletState {
   NOT_INSTALLED = 'not installed',
   DISCONNECTED = 'disconnected',
@@ -5,15 +7,14 @@ export enum WalletState {
   CONNECTED = 'connected',
 }
 
-export type WalletInfo = (
-  | {
-      state?:
-        | WalletState.CONNECTED
-        | WalletState.DISCONNECTED
-        | WalletState.CONNECTING;
-    }
-  | { state?: WalletState.NOT_INSTALLED; installLink: string }
-) & {
+export type WalletInfo = {
+  state:
+    | WalletState.CONNECTED
+    | WalletState.DISCONNECTED
+    | WalletState.CONNECTING
+    | WalletState.NOT_INSTALLED;
+  installLink: string;
   name: string;
   image: string;
+  type: WalletType;
 };
