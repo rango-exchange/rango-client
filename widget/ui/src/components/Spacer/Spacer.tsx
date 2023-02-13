8dbe9d1ce796fc87ece4d68b96255ce9a6d9699b
mikasackermn <mikasa@rango.exchange>
2023-02-13 15:42:26 +0000

    fixed padding LiquiditySourceType title

--- widget/ui/src/components/Spacer/Spacer.tsx
+++ widget/ui/src/components/Spacer/Spacer.tsx
@@ -4,25 +4,95 @@ import { styled } from '../../theme';
 const SpacerContainer = styled('div', {
   variants: {
     size: {
-      12: {
+      12: {},
+      16: {},
+      18: {},
+      20: {},
+      24: {},
+    },
+    scale: {
+      vertical: {},
+      horizontal: {},
+    },
+  },
+  compoundVariants: [
+    {
+      size: 12,
+      scale: 'horizontal',
+      css: {
         width: '$12',
       },
-      16: {
+    },
+    {
+      size: 12,
+      scale: 'vertical',
+      css: {
+        height: '$12',
+      },
+    },
+    {
+      size: 16,
+      scale: 'horizontal',
+      css: {
         width: '$16',
       },
-      18: {
+    },
+    {
+      size: 16,
+      scale: 'vertical',
+      css: {
+        height: '$16',
+      },
+    },
+    {
+      size: 18,
+      scale: 'horizontal',
+      css: {
         width: '$18',
       },
-      20: {
+    },
+    {
+      size: 18,
+      scale: 'vertical',
+      css: {
+        height: '$18',
+      },
+    },
+    {
+      size: 20,
+      scale: 'horizontal',
+      css: {
         width: '$20',
       },
     },
-  },
+    {
+      size: 20,
+      scale: 'vertical',
+      css: {
+        height: '$20',
+      },
+    },
+    {
+      size: 24,
+      scale: 'horizontal',
+      css: {
+        width: '$24',
+      },
+    },
+    {
+      size: 24,
+      scale: 'vertical',
+      css: {
+        height: '$24',
+      },
+    },
+  ],
 });
 export interface PropTypes {
-  size?: 12 | 16 | 18 | 20;
+  size?: 12 | 16 | 18 | 20 | 24;
+  scale?: 'vertical' | 'horizontal';
 }
 
-export function Spacer({ size = 12 }: PropTypes) {
-  return <SpacerContainer size={size} />;
+export function Spacer({ size = 12, scale = 'horizontal' }: PropTypes) {
+  return <SpacerContainer size={size} scale={scale} />;
 }
