5b1f165810d937448d78b3d3efe693321842a597
Eren Yeager <eren@rango.exchange>
2023-02-19 14:24:42 +0000

    reorder to test publish

--- widget/embedded/src/globalStyles.ts
+++ widget/embedded/src/globalStyles.ts
@@ -3,14 +3,14 @@ import { globalCss } from '@rangodev/ui';
 export const globalStyles = globalCss({
   '*': {
     '&::-webkit-scrollbar': { width: '$8' },
-    '&::-webkit-scrollbar-track': {
-      backgroundColor: '$neutrals300',
-    },
     '&::-webkit-scrollbar-thumb': {
       backgroundColor: '$neutrals400',
     },
     '&::-webkit-scrollbar-thumb:hover': {
       backgroundColor: '$neutrals500',
+    },
+    '&::-webkit-scrollbar-track': {
+      backgroundColor: '$neutrals300',
     },
   },
 });
