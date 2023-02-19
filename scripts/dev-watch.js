86955739d6809768fe2b6817dee81472b8b4706f
Eren Yeager <eren@rango.exchange>
2023-02-19 14:24:42 +0000

    write a custom script for our development process

---
+++ scripts/dev-watch.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+'use strict';
+const { join } = require('node:path');
+const commandLineArgs = require('command-line-args');
+const { dev } = require('./subcommands/dev');
+const { watch } = require('./subcommands/watch');
+const { awake } = require('./utils');
+
+const cwd = join(__dirname, '..');
+const nx = join(cwd, 'node_modules', '.bin', 'nx');
+
+async function run() {
+  const optionDefinitions = [{ name: 'project', type: String }];
+  const { project } = commandLineArgs(optionDefinitions);
+
+  awake();
+  dev({ project, nx, cwd });
+  watch({ project, nx, cwd });
+}
+
+run();
