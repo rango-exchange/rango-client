d75f738cc651742c8717a1fc3492e2c821cf79c4
mikasackermn <mikasa@rango.exchange>
2023-02-28 19:18:29 +0000

    fixed all problems

--- widget/config-client/src/components/mock.ts
+++ widget/config-client/src/components/mock.ts
@@ -1,13 +1,4 @@
-export const langueges = [
-  {
-    name: 'English (uS)',
-    logo: 'https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/800px-Flag_of_the_United_States.svg.png?20151118161041',
-  },
-  {
-    name: 'German',
-    logo: 'https://upload.wikimedia.org/wikipedia/en/thumb/b/ba/Flag_of_Germany.svg/800px-Flag_of_Germany.svg.png?20111003033442',
-  },
-];
+
 
 export const fonts = [
   {
