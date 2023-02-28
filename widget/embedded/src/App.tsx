042f7167d39f31453662cdd49e779f2a524609bd
mikasackermn <mikasa@rango.exchange>
2023-02-28 19:18:29 +0000

    fixed all problems

--- widget/embedded/src/App.tsx
+++ widget/embedded/src/App.tsx
@@ -19,13 +19,6 @@ interface Token {
   contractAddress?: string;
 }
 
-// const Container = styled('div', {
-//   display: 'flex',
-//   backgroundColor: '$neutrals300',
-//   padding: 10,
-//   justifyContent: 'center',
-// });
-
 //todo: update interface and update widget state based on WidgetProps change
 export type WidgetProps = {
   fromChain?: string;
