5b7b149501183ed3b7d1d1673dd70de8cfba939c
mikasackermn <mikasa@rango.exchange>
2023-02-28 19:32:52 +0000

    use swap box to config page

--- widget/config-client/src/App.tsx
+++ widget/config-client/src/App.tsx
@@ -3,6 +3,15 @@ import { useMetaStore } from './store/meta';
 import { Config } from './containers/Config';
 import { SwapBox } from '@rango-dev/widget-embedded';
 
+const Container = styled('div', {
+  display: 'flex',
+  justifyContent: 'center',
+});
+const SwapContent = styled('div', {
+  position: 'sticky',
+  top: 0,
+  marginTop: 115,
+});
 export function App() {
   const fetchMeta = useMetaStore((state) => state.fetchMeta);
 
