36d5725e02857753da44fb35c0e25864536333bb
mikasackermn <mikasa@rango.exchange>
2023-03-01 09:47:45 +0000

    test embedded

--- widget/embedded/tsdx.config.js
+++ widget/embedded/tsdx.config.js
@@ -1,8 +1,8 @@
 // Not transpiled with TypeScript or Babel, so use plain Es6/Node.js!
 module.exports = {
-    // This function will run for each entry/format/env combination
-    rollup(config, options) {
-        console.log("tsdx config", config, options)
-      return config; // always return a config.
-    },
-  };
+  // This function will run for each entry/format/env combination
+  rollup(config) {
+    config.plugins = config.plugins.filter((plugin) => plugin.name !== 'terser');
+    return config; // always return a config.
+  },
+};
