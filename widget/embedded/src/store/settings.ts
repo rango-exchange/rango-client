0942bb0e24f829a356af0811956a15248cbf2f5d
Shinji <shinji@rango.exchange>
2023-03-07 06:11:03 +0000

    remove immer

--- widget/embedded/src/store/settings.ts
+++ widget/embedded/src/store/settings.ts
@@ -1,5 +1,5 @@
 import { create } from 'zustand';
-import { persist } from 'zustand/middleware';
+import { persist, subscribeWithSelector } from 'zustand/middleware';
 import { immer } from 'zustand/middleware/immer';
 import { useMetaStore } from './meta';
 import createSelectors from './selectors';
@@ -23,47 +23,51 @@ interface Settings {
 export const useSettingsStore = createSelectors(
   create<Settings>()(
     persist(
-      immer((set) => ({
-        slippage: 1,
-        customSlippage: null,
-        infinitApprove: false,
-        disabledLiquiditySources: [],
-        theme: 'auto',
-        setSlippage: (slippage) =>
-          set((state) => {
-            state.slippage = slippage;
-          }),
-        setCustomSlippage: (customSlippage) => {
-          return set((state) => {
-            state.customSlippage = customSlippage;
-          });
-        },
-        toggleAllLiquiditySources: () =>
-          set((state) => {
-            const { swappers } = useMetaStore.getState().meta;
+      immer(
+        subscribeWithSelector((set) => ({
+          slippage: 1,
+          customSlippage: null,
+          infinitApprove: false,
+          disabledLiquiditySources: [],
+          theme: 'auto',
+          setSlippage: (slippage) =>
+            set((state) => {
+              state.slippage = slippage;
+            }),
+          setCustomSlippage: (customSlippage) => {
+            return set((state) => {
+              state.customSlippage = customSlippage;
+            });
+          },
+          toggleAllLiquiditySources: () =>
+            set((state) => {
+              const { swappers } = useMetaStore.getState().meta;
 
-            if (swappers.length - state.disabledLiquiditySources.length === 0)
-              state.disabledLiquiditySources = [];
-            else {
-              const allSwappers = swappers.map((swapper) => swapper.swapperGroup);
-              state.disabledLiquiditySources = allSwappers;
-            }
-          }),
-        toggleInfinitApprove: () =>
-          set((state) => {
-            state.infinitApprove = !state.infinitApprove;
-          }),
-        toggleLiquiditySource: (name) =>
-          set((state) => {
-            state.disabledLiquiditySources = state.disabledLiquiditySources.includes(name)
-              ? state.disabledLiquiditySources.filter((liquiditySource) => liquiditySource != name)
-              : state.disabledLiquiditySources.concat(name);
-          }),
-        setTheme: (theme) =>
-          set((state) => {
-            state.theme = theme;
-          }),
-      })),
+              if (swappers.length - state.disabledLiquiditySources.length === 0)
+                state.disabledLiquiditySources = [];
+              else {
+                const allSwappers = swappers.map((swapper) => swapper.swapperGroup);
+                state.disabledLiquiditySources = allSwappers;
+              }
+            }),
+          toggleInfinitApprove: () =>
+            set((state) => {
+              state.infinitApprove = !state.infinitApprove;
+            }),
+          toggleLiquiditySource: (name) =>
+            set((state) => {
+              state.disabledLiquiditySources = state.disabledLiquiditySources.includes(name)
+                ? state.disabledLiquiditySources.filter(
+                    (liquiditySource) => liquiditySource != name,
+                  )
+                : state.disabledLiquiditySources.concat(name);
+            }),
+          setTheme: (theme) =>
+            set((state) => {
+              state.theme = theme;
+            }),
+        })),
+      ),
       {
         name: 'user-settings',
       },
