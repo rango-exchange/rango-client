8cb33bb5f492f507bc2c56c8abd6d35f59c96390
Eren Yeager <eren@rango.exchange>
2023-03-14 15:55:52 +0000

    Explicity say not set, when an enviroment variable not set.

--- scripts/publish/config.mjs
+++ scripts/publish/config.mjs
@@ -2,8 +2,8 @@ const scope = `@rango-dev`;
 export const VERCEL_ORG_ID = process.env.VERCEL_ORG_ID;
 export const VERCEL_TOKEN = process.env.VERCEL_TOKEN;
 export const VERCEL_PACKAGES = {
-  [`${scope}/wallets-demo`]: process.env.VERCEL_PROJECT_WALLETS,
-  [`${scope}/queue-manager-demo`]: process.env.VERCEL_PROJECT_Q,
-  [`${scope}/wallets-adapter-demo`]: process.env.VERCEL_PROJECT_WALLET_ADAPTER,
-  [`${scope}/config-client`]: process.env.VERCEL_PROJECT_WIDGET_CONFIG,
+  [`${scope}/wallets-demo`]: getProjectIdFromEnv('VERCEL_PROJECT_WALLETS'),
+  [`${scope}/queue-manager-demo`]: getProjectIdFromEnv('VERCEL_PROJECT_Q'),
+  [`${scope}/wallets-adapter-demo`]: getProjectIdFromEnv('VERCEL_PROJECT_WALLET_ADAPTER'),
+  [`${scope}/config-client`]: getProjectIdFromEnv('VERCEL_PROJECT_WIDGET_CONFIG'),
 };
