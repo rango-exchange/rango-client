430885eb25c04f2a76aa760c92519549b7ee5608
Shinji <shinji@rango.exchange>
2023-03-14 11:57:21 +0000

    update confirm swap warnings and erros

--- widget/embedded/src/hooks/useConfirmSwap.ts
+++ widget/embedded/src/hooks/useConfirmSwap.ts
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import { useRef } from 'react';
 import { PendingSwap } from '@rangodev/ui/dist/containers/History/types';
 import { SwapSavedSettings } from '@rangodev/ui/dist/types/swaps';
 import BigNumber from 'bignumber.js';
@@ -17,17 +17,26 @@ import {
 import {
   calculatePendingSwap,
   createBestRouteRequestBody,
+  getBalanceWarnings,
+  getMinRequiredSlippage,
+  getOutputRatio,
+  getPercentageChange,
+  getRouteOutputAmount,
   getWalletsForNewSwap,
-  hasEnoughBalanceAndProperSlippage,
   hasProperSlippage,
+  isOutputAmountChangedALot,
+  outputRatioHasWarning,
 } from '../utils/swap';
-import { Typography } from '@rangodev/ui';
-import { ChangeSlippageButton } from '../components/ChangeSlippageButton';
+import { numberToString } from '../utils/numbers';
+import { MinRequiredSlippage } from '../components/warnings/MinRequiredSlippage';
+import { PendingSwapSettings } from '../types';
+import { BalanceWarnings } from '../components/warnings/BalanceWarnings';
 
 export function useConfirmSwap() {
   const fromToken = useBestRouteStore.use.fromToken();
   const toToken = useBestRouteStore.use.toToken();
   const inputAmount = useBestRouteStore.use.inputAmount();
+  const inputUsdValue = useBestRouteStore.use.inputUsdValue();
   const bestRoute = useBestRouteStore.use.bestRoute();
   const setBestRoute = useBestRouteStore.use.setBestRoute();
   const accounts = useWalletsStore.use.accounts();
@@ -38,15 +47,15 @@ export function useConfirmSwap() {
   const customSlippage = useSettingsStore.use.customSlippage();
   const disabledLiquiditySources = useSettingsStore.use.disabledLiquiditySources();
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState<ReactNode>(null);
-  const [warning, setWarning] = useState<ReactNode>(null);
-  const [enoughBalance, setEnoughBalance] = useState<boolean | null>(null);
-  const [minRequiredSlippage, setMinRequiredSlippage] = useState<null | number>(null);
+  const [errors, setErrors] = useState<ReactNode[]>([]);
+  const [warnings, setWarnings] = useState<ReactNode[]>([]);
   const userSlippage = customSlippage || slippage;
   const swapConfirmed = useRef(false);
+  const addError = (error: ReactNode) => setErrors((prevState) => prevState.concat(error));
+  const addWarning = (warning: ReactNode) => setWarnings((prevState) => prevState.concat(warning));
 
   if (!fromToken || !toToken || !inputAmount || !bestRoute)
-    return { loading: false, error: '', warning: '', data: null, swap: () => {} };
+    return { loading: false, errors: [], warnings: [], data: null, swap: null };
 
   const swap = async () => {
     try {
@@ -63,41 +72,55 @@ export function useConfirmSwap() {
 
       const confirmBestRoute = await httpService.getBestRoute(requestBody);
       setLoading(false);
-      if (!confirmBestRoute || !confirmBestRoute.result || !bestRoute) setError('no routes found');
+      if (!confirmBestRoute || !confirmBestRoute.result || !bestRoute) addError('no routes found');
       if (
         !!confirmBestRoute &&
         !new BigNumber(confirmBestRoute.requestAmount).isEqualTo(new BigNumber(inputAmount || '-1'))
       )
-        setError('No routes found. Please try again later.');
+        addError('No routes found. Please try again later.');
       const routeChanged = isRouteChanged(bestRoute, confirmBestRoute!);
+
       setBestRoute(confirmBestRoute!);
       if (routeChanged) {
+        const newRouteOutputUsdValue = new BigNumber(
+          confirmBestRoute.result?.outputAmount || '0',
+        ).multipliedBy(toToken.usdPrice || 0);
+
+        const outputRatio = getOutputRatio(inputUsdValue, newRouteOutputUsdValue);
+        const highValueLoss = outputRatioHasWarning(inputUsdValue, outputRatio);
+
         if (isNumberOfSwapsChanged(bestRoute, confirmBestRoute))
-          setWarning('Route has been updated.');
+          addWarning('Route has been updated.');
         if (isRouteSwappersUpdated(bestRoute, confirmBestRoute))
-          setWarning('Route swappers has been updated.');
+          addWarning('Route swappers has been updated.');
         if (isRouteInternalCoinsUpdated(bestRoute, confirmBestRoute))
-          setWarning('Route internal coins has been updated.');
+          addWarning('Route internal coins has been updated.');
+        if (highValueLoss) addError('Route updated and price impact is too high, try again later!');
+        if (isOutputAmountChangedALot(bestRoute, confirmBestRoute))
+          addWarning(`Output amount changed to ${numberToString(
+            getRouteOutputAmount(confirmBestRoute),
+          )} 
+      (${numberToString(
+        getPercentageChange(
+          getRouteOutputAmount(bestRoute)!,
+          getRouteOutputAmount(confirmBestRoute)!,
+        ),
+        null,
+        2,
+      )}% change).`);
       }
 
-      if (!hasProperSlippage(userSlippage.toString(), minRequiredSlippage))
-        setError(
-          <Typography variant="body2">
-            Your slippage should be {minRequiredSlippage} at least
-            <ChangeSlippageButton />
-          </Typography>,
-        );
+      const balanceWarnings = getBalanceWarnings(confirmBestRoute, selectedWallets);
+      const enoughBalance = balanceWarnings.length === 0;
 
-      if (
-        hasEnoughBalanceAndProperSlippage(
-          bestRoute,
-          selectedWallets,
-          userSlippage.toString(),
-          minRequiredSlippage,
-        ) &&
-        (!routeChanged || (routeChanged && swapConfirmed.current))
-      ) {
-        const settings: Omit<SwapSavedSettings, 'disabledSwappersIds'> = {
+      if (!enoughBalance) addWarning(BalanceWarnings(balanceWarnings));
+
+      const minRequiredSlippage = getMinRequiredSlippage(bestRoute);
+      if (!hasProperSlippage(userSlippage.toString(), minRequiredSlippage))
+        addError(MinRequiredSlippage(minRequiredSlippage));
+
+      if (!routeChanged || (routeChanged && swapConfirmed.current)) {
+        const swapSettings: PendingSwapSettings = {
           slippage: userSlippage.toString(),
           disabledSwappersGroups: disabledLiquiditySources,
         };
@@ -105,22 +128,25 @@ export function useConfirmSwap() {
           inputAmount!.toString(),
           bestRoute!,
           getWalletsForNewSwap(selectedWallets),
-          settings,
+          swapSettings,
           false,
           tokens,
         );
+        console.log('new swap:', newSwap);
       } else if (!swapConfirmed.current) swapConfirmed.current = true;
     } catch (error) {
       setLoading(false);
-      if (error instanceof Error) setError(error.message);
-      else setError('');
+      if (error.message) addError(error.message);
+      else if (!!error.response)
+        addError(`Failed to confirm swap 'status': ${error.response.status}), please try again.`);
+      else addError('Failed to confirm swap, please try again.');
     }
   };
 
   return {
     loading,
-    error,
-    warning,
+    errors,
+    warnings,
     swap,
   };
 }
