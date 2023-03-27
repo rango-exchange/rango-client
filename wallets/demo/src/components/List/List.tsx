6e238a43404c75b1a08307166b7d26963596fdef
RanGojo <rangojo@rango.exchange>
2023-03-27 14:38:26 +0000

    refactor

--- wallets/demo/src/components/List/List.tsx
+++ wallets/demo/src/components/List/List.tsx
@@ -3,7 +3,7 @@ import { useWallets } from '@rango-dev/wallets-core';
 import { sortWalletsBasedOnState, WalletInfo, WalletType } from '@rango-dev/wallets-shared';
 import Item from './Item';
 import './styles.css';
-const excludedWallets = [WalletType.UNKNOWN, WalletType.TERRA_STATION, WalletType.LEAP, WalletType.TRON_LINK];
+const excludedWallets = [WalletType.UNKNOWN, WalletType.TERRA_STATION, WalletType.LEAP];
 
 function List() {
   const { state, getWalletInfo } = useWallets();
