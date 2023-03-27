d691f7912fcb569b081994a752464fa4e5b88991
RanGojo <rangojo@rango.exchange>
2023-03-27 15:07:45 +0000

    refactor external signer name

--- signers/signer-starknet/src/signer.ts
+++ signers/signer-starknet/src/signer.ts
@@ -9,13 +9,13 @@ export interface IStarknetSigner extends ISigner<StarknetTransaction> {}
 
 // TODO - replace with real type
 // tslint:disable-next-line: no-any
-type StarknetExternalSigner = any;
+type StarknetExternalProvider = any;
 
 export class StarknetSigner implements IStarknetSigner {
-  private signer: StarknetExternalSigner;
+  private provider: StarknetExternalProvider;
 
-  constructor(signer: StarknetExternalSigner) {
-    this.signer = signer;
+  constructor(provider: StarknetExternalProvider) {
+    this.provider = provider;
   }
 
   async signMessage(): Promise<string> {
@@ -24,7 +24,9 @@ export class StarknetSigner implements IStarknetSigner {
 
   async signAndSendTx(tx: StarknetTransaction): Promise<string> {
     try {
-      const { transaction_hash } = await this.signer.account.execute(tx.calls);
+      const { transaction_hash } = await this.provider.account.execute(
+        tx.calls
+      );
       return transaction_hash;
     } catch (error) {
       throw new SignerError(SignerErrorCode.SEND_TX_ERROR, undefined, error);
