5c36a7ff656e63f09ea4eb4e0538d6d1ab7cb778
samobasquiat <samo.basquiat@rango.exchange>
2023-03-28 05:53:25 +0000

    redirect after creating swap & fix some ui bugs on history page

--- widget/embedded/src/pages/SwapDetailsPage.tsx
+++ widget/embedded/src/pages/SwapDetailsPage.tsx
@@ -1,15 +1,33 @@
+import React, { useEffect, useRef } from 'react';
+import { useLocation } from 'react-router-dom';
+import { useManager } from '@rango-dev/queue-manager-react';
 import { SwapHistory } from '@rango-dev/ui';
-import React from 'react';
 import { navigationRoutes } from '../constants/navigationRoutes';
 import { useNavigateBack } from '../hooks/useNavigateBack';
-import { pendingSwap } from '../mockData/pendingSwap';
+import { getPendingSwaps } from '../utils/queue';
 
 export function SwapDetailsPage() {
   const { navigateBackFrom } = useNavigateBack();
+  const location = useLocation();
+  const { manager } = useManager();
+  const requestIdRef = useRef<string>(null);
+
+  useEffect(() => {
+    if (location?.state?.requestId) {
+      requestIdRef.current = location?.state?.requestId;
+    }
+  }, [location?.state?.requestId]);
+
+  const pendingSwaps = getPendingSwaps(manager);
+
+  const currentSwap = pendingSwaps.find(
+    (pendingSwap) => pendingSwap.swap.requestId === requestIdRef.current
+  );
+
   return (
     <SwapHistory
       onBack={navigateBackFrom.bind(null, navigationRoutes.swapDetails)}
-      pendingSwap={pendingSwap}
+      pendingSwap={currentSwap?.swap}
     />
   );
 }
