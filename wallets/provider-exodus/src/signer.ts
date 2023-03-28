348bf4d3806da379f349ca1ac5e56174a06a126a
RanGojo <rangojo@rango.exchange>
2023-03-28 17:48:00 +0000

    fix exodus and frontier signers

--- wallets/provider-exodus/src/signer.ts
+++ wallets/provider-exodus/src/signer.ts
@@ -1,16 +1,13 @@
 import { DefaultEvmSigner } from '@rango-dev/signer-evm';
-import { DefaultCosmosSigner } from '@rango-dev/signer-cosmos';
+import { DefaultSolanaSigner } from '@rango-dev/signer-solana';
 import { Network, getNetworkInstance } from '@rango-dev/wallets-shared';
 import { SignerFactory, TransactionType as TxType } from 'rango-types';
 
 export default function getSigners(provider: any): SignerFactory {
   const ethProvider = getNetworkInstance(provider, Network.ETHEREUM);
-  const cosmosProvider = getNetworkInstance(provider, Network.COSMOS);
+  const solProvider = getNetworkInstance(provider, Network.SOLANA);
   const signers = new SignerFactory();
   signers.registerSigner(TxType.EVM, new DefaultEvmSigner(ethProvider));
-  signers.registerSigner(
-    TxType.COSMOS,
-    new DefaultCosmosSigner(cosmosProvider)
-  );
+  signers.registerSigner(TxType.COSMOS, new DefaultSolanaSigner(solProvider));
   return signers;
 }
