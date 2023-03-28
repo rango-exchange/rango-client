67dcff817b7da9c5971dad5877dec15a09c0b457
RanGojo <rangojo@rango.exchange>
2023-03-28 11:11:56 +0000

    refactor signer names

--- wallets/provider-argentx/src/signer.ts
+++ wallets/provider-argentx/src/signer.ts
@@ -1,8 +1,8 @@
-import { CustomStarknetSigner } from '@rango-dev/signer-starknet';
+import { DefaultStarknetSigner } from '@rango-dev/signer-starknet';
 import { SignerFactory, TransactionType as TxType } from 'rango-types';
 
 export default function getSigners(provider: any): SignerFactory {
   const signers = new SignerFactory();
-  signers.registerSigner(TxType.STARKNET, new CustomStarknetSigner(provider));
+  signers.registerSigner(TxType.STARKNET, new DefaultStarknetSigner(provider));
   return signers;
 }
