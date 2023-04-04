bd07cab333f18819dd92cca014177c401a41e51a
Eren Yeager <eren@rango.exchange>
2023-04-04 22:32:50 +0000

    CRA only keeps REACT_APP_* enviroments. Renamed the envs.

--- widget/config-client/src/services/httpService.ts
+++ widget/config-client/src/services/httpService.ts
@@ -1,2 +1,2 @@
 import { RangoClient } from 'rango-sdk';
-export const rango = new RangoClient(process.env.REACT_API_KEY as string);
+export const rango = new RangoClient(process.env.REACT_APP_API_KEY as string);
