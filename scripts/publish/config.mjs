838c5bf5b560ea26ae258f794629aa99b5b7fa72
mikasackermn <mikasa@rango.exchange>
2023-04-05 10:52:22 +0000

    Changed config-client name to widget-playground

--- scripts/publish/config.mjs
+++ scripts/publish/config.mjs
@@ -4,8 +4,8 @@ const scope = `@rango-dev`;
 export const VERCEL_ORG_ID = process.env.VERCEL_ORG_ID;
 export const VERCEL_TOKEN = process.env.VERCEL_TOKEN;
 export const VERCEL_PACKAGES = {
-  [`${scope}/wallets-demo`]: getProjectIdFromEnv('VERCEL_PROJECT_WALLETS'),
-  [`${scope}/queue-manager-demo`]: getProjectIdFromEnv('VERCEL_PROJECT_Q'),
-  [`${scope}/wallets-adapter-demo`]: getProjectIdFromEnv('VERCEL_PROJECT_WALLET_ADAPTER'),
-  [`${scope}/config-client`]: getProjectIdFromEnv('VERCEL_PROJECT_WIDGET_CONFIG'),
+  [`${scope}/wallets-demo`]: process.env.VERCEL_PROJECT_WALLETS,
+  [`${scope}/queue-manager-demo`]: process.env.VERCEL_PROJECT_Q,
+  [`${scope}/wallet-adapter-demo`]: process.env.VERCEL_PROJECT_WALLET_ADAPTER,
+  [`${scope}/widget-playground`]: process.env.VERCEL_PROJECT_WIDGET_CONFIG,
 };
