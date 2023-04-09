641180a7feba4b1eb00155b5b416593fe2b331ae
mikasackermn <mikasa@rango.exchange>
2023-04-09 04:56:25 +0000

    Fixed types

--- widget/embedded/src/utils/numbers.ts
+++ widget/embedded/src/utils/numbers.ts
@@ -68,6 +68,7 @@ export const convertBigNumberToHex = (value: BigNumber, decimals: number): strin
 
 export const uint8ArrayToHex = (buffer: Uint8Array): string => {
   // buffer is an ArrayBuffer
+  // @ts-ignore
   return [...buffer].map((x) => x.toString(16).padStart(2, '0')).join('');
 };
 
