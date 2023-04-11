cdf6bb8a391df8abd08ed4bf7356b46df0d1b80f
Eren Yeager <eren@rango.exchange>
2023-04-11 21:04:02 +0000

    set font family on body

--- widget/embedded/src/globalStyles.ts
+++ widget/embedded/src/globalStyles.ts
@@ -20,7 +20,7 @@ export const globalFont = (fontFamily: string) =>
       "url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap')",
       "url('https://fonts.cdnfonts.com/css/times-new-roman')",
     ],
-    [`.font_${fontFamily.replace(/ /g, '')} ._text`]: {
+    body: {
       fontFamily,
     },
   })();
