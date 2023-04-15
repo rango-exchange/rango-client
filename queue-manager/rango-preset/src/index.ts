ea82df8c7405f9d49ce82a5117eb134dcc3665cd
RanGojo <rangojo@rango.exchange>
2023-04-15 13:35:43 +0000

    just for testing

--- queue-manager/rango-preset/src/index.ts
+++ queue-manager/rango-preset/src/index.ts
@@ -11,25 +11,32 @@ export {
 } from './shared';
 // TODO: This is for our frontend, some of them can be removed.
 export {
-  updateSwapStatus,
-  checkWaitingForNetworkChange,
-  getCurrentStep,
-  getEvmProvider,
-  cancelSwap,
   splitWalletNetwork,
   resetRunningSwapNotifsOnPageLoad,
   getRunningSwaps,
 } from './helpers';
 export {
-  getRelatedWallet,
-  getRelatedWalletOrNull,
+  // getRelatedWallet,
   getCurrentBlockchainOf,
-  getCurrentBlockchainOfOrNull,
   SwapProgressNotification,
-  PendingSwap,
-  PendingSwapNetworkStatus,
-  PendingSwapWithQueueID,
-  PendingSwapStep,
   EventType,
 } from './shared';
+export {
+  PendingSwapWithQueueID,
+  getCurrentBlockchainOfOrNull,
+  getRelatedWalletOrNull,
+  MessageSeverity,
+  PendingSwapStep,
+  PendingSwapNetworkStatus,
+  PendingSwap,
+} from './shared';
+// TODO: This is for our frontend, some of them can be removed.
+export {
+  updateSwapStatus,
+  checkWaitingForNetworkChange,
+  getCurrentStep,
+  getEvmProvider,
+  getRelatedWallet,
+  cancelSwap,
+} from './helpers';
 export { useMigration, useQueueManager } from './hooks';
