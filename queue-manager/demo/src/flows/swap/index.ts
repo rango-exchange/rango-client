105c62e08fd271f43034b1e01c4e312ac35122c5
mikasackermn <mikasa@rango.exchange>
2023-04-15 21:19:27 +0000

    use preset in demo and clean old codes

--- queue-manager/demo/src/flows/swap/index.ts
+++
@@ -1,5 +0,0 @@
-export { start } from './actions/start';
-export { scheduleNextStep } from './actions/scheduleNextStep';
-export { createTransaction } from './actions/createTransaction';
-export { executeTransaction } from './actions/executeTransaction';
-export { checkStatus } from './actions/checkStatus';
