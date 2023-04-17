01d92f894f360a9bd4e85cfcdb558c81a62bbe36
samobasquiat <samo.basquiat@rango.exchange>
2023-04-17 15:27:44 +0000

    add getConciseeAddress function

--- widget/ui/src/helper/index.ts
+++ widget/ui/src/helper/index.ts
@@ -1,2 +1,16 @@
 export const containsText = (text: string, searchText: string) =>
   text.toLowerCase().indexOf(searchText.toLowerCase()) > -1;
+
+export const getConciseAddress = (
+  address: string | null,
+  maxChars = 8,
+  ellipsisLength = 3
+): string | null => {
+  if (!address) return null;
+  if (address.length < 2 * maxChars + ellipsisLength) return address;
+  const start = Math.ceil((address.length - maxChars) / 2);
+  const end = address.length - maxChars;
+  return `${address.substr(start, maxChars)}${'.'.repeat(
+    ellipsisLength
+  )}${address.substr(end)}`;
+};
