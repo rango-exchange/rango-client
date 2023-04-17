61332a21ed8d7c0400cd1c20a4ba631ce94a2735
Eren Yeager <eren@rango.exchange>
2023-04-17 10:47:16 +0000

    fix wrong import

--- scripts/publish/config.mjs
+++ scripts/publish/config.mjs
@@ -1,11 +1,11 @@
-import { getProjectIdFromEnv } from "./utils.mjs";
+import { getEnvWithFallback } from './utils.mjs';
 
 const scope = `@rango-dev`;
 export const VERCEL_ORG_ID = process.env.VERCEL_ORG_ID;
 export const VERCEL_TOKEN = process.env.VERCEL_TOKEN;
 export const VERCEL_PACKAGES = {
-  [`${scope}/wallets-demo`]: process.env.VERCEL_PROJECT_WALLETS,
-  [`${scope}/queue-manager-demo`]: process.env.VERCEL_PROJECT_Q,
-  [`${scope}/wallet-adapter-demo`]: process.env.VERCEL_PROJECT_WALLET_ADAPTER,
-  [`${scope}/widget-playground`]: process.env.VERCEL_PROJECT_WIDGET_CONFIG,
+  [`${scope}/wallets-demo`]: getEnvWithFallback('VERCEL_PROJECT_WALLETS'),
+  [`${scope}/queue-manager-demo`]: getEnvWithFallback('VERCEL_PROJECT_Q'),
+  [`${scope}/wallets-adapter-demo`]: getEnvWithFallback('VERCEL_PROJECT_WALLET_ADAPTER'),
+  [`${scope}/config-client`]: getEnvWithFallback('VERCEL_PROJECT_WIDGET_CONFIG'),
 };
