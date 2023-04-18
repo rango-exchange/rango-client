71337a410ebae75ce0811895282e7a75dc4100fe
RanGojo <rangojo@rango.exchange>
2023-04-18 19:04:47 +0000

    added a default api key for demo purpose

--- widget/embedded/src/services/httpService.ts
+++ widget/embedded/src/services/httpService.ts
@@ -1,5 +1,9 @@
 import { RangoClient } from 'rango-sdk';
 
+// this API key is limited and
+// it is only for test purpose
+const RANGO_PUBLIC_API_KEY = 'c6381a79-2817-4602-83bf-6a641a409e32';
+
 export const httpService = new RangoClient(
-  process.env.REACT_APP_API_KEY as string
+  (process.env.REACT_APP_API_KEY as string) || RANGO_PUBLIC_API_KEY
 );
--- widget/playground/src/services/httpService.ts
+++ widget/playground/src/services/httpService.ts
@@ -1,2 +1,9 @@
 import { RangoClient } from 'rango-sdk';
-export const rango = new RangoClient(process.env.REACT_APP_API_KEY as string);
+
+// this API key is limited and
+// it is only for test purpose
+const RANGO_PUBLIC_API_KEY = 'c6381a79-2817-4602-83bf-6a641a409e32';
+
+export const rango = new RangoClient(
+  (process.env.REACT_APP_API_KEY as string) || RANGO_PUBLIC_API_KEY,
+);
