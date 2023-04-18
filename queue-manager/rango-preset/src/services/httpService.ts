db68c7724aabc669a12bb3c439745b090b5b37f1
RanGojo <rangojo@rango.exchange>
2023-04-18 19:04:47 +0000

    added a default api key for demo purpose

--- queue-manager/rango-preset/src/services/httpService.ts
+++ queue-manager/rango-preset/src/services/httpService.ts
@@ -1,7 +1,11 @@
 import { RangoClient } from 'rango-sdk';
 import { RANGO_DAPP_API_KEY, RANGO_DAPP_API_BASE_URL } from '../constants';
 
+// this API key is limited and
+// it is only for test purpose
+const RANGO_PUBLIC_API_KEY = 'c6381a79-2817-4602-83bf-6a641a409e32';
+
 export const httpService = new RangoClient(
-  RANGO_DAPP_API_KEY || '',
+  RANGO_DAPP_API_KEY || RANGO_PUBLIC_API_KEY,
   RANGO_DAPP_API_BASE_URL
 );
