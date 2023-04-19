1983eee164644e2d13755a8030bd203bef2d3c8c
samobasquiat <samo.basquiat@rango.exchange>
2023-04-19 17:26:43 +0000

    check structuredClone type

--- widget/embedded/src/utils/configs.ts
+++ widget/embedded/src/utils/configs.ts
@@ -19,6 +19,12 @@ export function setConfig(name: keyof Configs, value: any) {
 }
 
 export function initConfig(nextConfigs: Configs) {
-  configs = structuredClone(nextConfigs);
+  let clonedConfigs;
+  if (typeof structuredClone === 'function') {
+    clonedConfigs = structuredClone(nextConfigs);
+  } else {
+    clonedConfigs = JSON.parse(JSON.stringify(nextConfigs));
+  }
+  configs = clonedConfigs;
   return configs;
 }
