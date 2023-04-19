004fa9aad71cbff42fcf9333a5b29e24c71b938e
samobasquiat <samo.basquiat@rango.exchange>
2023-04-19 15:46:02 +0000

    test to update preset version

--- queue-manager/rango-preset/src/configs.ts
+++ queue-manager/rango-preset/src/configs.ts
@@ -4,6 +4,7 @@ export interface Configs {
 
 // this API key is limited and
 // it is only for test purpose
+
 const RANGO_PUBLIC_API_KEY = 'c6381a79-2817-4602-83bf-6a641a409e32';
 
 let configs: Configs = {
