aa00d66d345a38f94e5093d705b372aa0e604f28
samobasquiat <samo.basquiat@rango.exchange>
2023-04-19 14:59:14 +0000

    test to update deps

--- widget/embedded/src/components/ConfirmSwapErrors.tsx
+++ widget/embedded/src/components/ConfirmSwapErrors.tsx
@@ -7,14 +7,14 @@ export function ConfirmSwapErrors(errors: ConfirmSwapError[]) {
   return errors.flatMap((error) => {
     switch (error.type) {
       case ConfirmSwapErrorTypes.NO_ROUTE:
-        return "No routes found. Please try again later."
+        return 'No routes found. Please try again later.';
       case ConfirmSwapErrorTypes.REQUEST_FAILED:
         return `Failed to confirm swap ${
-            error.status ? `'status': ${error.status})` : ''
-          }, please try again.`
+          error.status ? `'status': ${error.status})` : ''
+        }, please try again.`;
 
       case ConfirmSwapErrorTypes.ROUTE_UPDATED_WITH_HIGH_VALUE_LOSS:
-        return "Route updated and price impact is too high, try again later!"
+        return 'Route updated and price impact is too high, try again later!';
       case ConfirmSwapErrorTypes.INSUFFICIENT_SLIPPAGE:
         return (
           <MinRequiredSlippage
