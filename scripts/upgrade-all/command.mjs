fb520228496764613b4d934744c688601f4f5753
Eren Yeager <eren@rango.exchange>
2023-04-20 14:28:42 +0000

    add a fallback to check all workspace

--- scripts/upgrade-all/command.mjs
+++ scripts/upgrade-all/command.mjs
@@ -1,6 +1,6 @@
 import { $ } from 'execa';
 import commandLineArgs from 'command-line-args';
-import { updateVersion } from './utils.mjs';
+import { getAllPackages, upgradeDepndendentsOf } from './utils.mjs';
 
 async function run() {
   const optionDefinitions = [{ name: 'project', type: String }];
@@ -9,40 +9,19 @@ async function run() {
   const { stdout: currentBranch } = await $`git branch --show-current`;
   const dist = currentBranch === 'main' ? 'latest' : 'next';
 
-  console.log(`Running upgrade-all for ${project}@${dist} \n`);
+  if (!project) {
+    console.log(`you didn't specify any "project". So we will check all the workspace packages.`);
+    const packages = await getAllPackages();
+    console.log(`All packages (without clients/demos): ${packages.length}`);
+    console.log(`Dist channel: ${dist}`);
 
-  if (!project) throw '`project` is required';
-
-  const { stdout: info } = await $`yarn workspaces info`;
-  const workspaces = JSON.parse(info);
-
-  // Going through all workspace and find packages which depends on `project`
-  const dependents = [];
-  const pkgs = Object.keys(workspaces);
-  pkgs.forEach((pkg) => {
-    if (
-      workspaces[pkg].workspaceDependencies.includes(project) ||
-      workspaces[pkg].mismatchedWorkspaceDependencies.includes(project)
-    ) {
-      dependents.push(pkg);
-    }
-  });
-
-  if (dependents.length === 0) throw new Error(`It seems ${project} isn't used by any packages.`);
-
-  console.log(`These packages are using ${project}: ${dependents.join(',')} \n`);
-
-  const { stdout: npmInfo } = await $`yarn info ${project}@${dist} --json`;
-  const versions = JSON.parse(npmInfo).data['dist-tags'];
-  const version = versions[dist];
-
-  console.log(`NPM version for ${project} is ${version}. \n`);
-
-  await Promise.all(
-    dependents.map((pkg) =>
-      updateVersion({ path: workspaces[pkg].location }, { name: project, version }),
-    ),
-  );
+    await Promise.all(packages.map((project) => upgradeDepndendentsOf(project, dist)));
+    return;
+  } else {
+    console.log(`Running upgrade-all for ${project} \n`);
+    console.log(`Dist channel: ${dist}`);
+    await upgradeDepndendentsOf(project, dist);
+  }
 
   console.log(`package.json has been updated. Trying to install... \n`);
 
--- scripts/upgrade-all/utils.mjs
+++ scripts/upgrade-all/utils.mjs
@@ -1,5 +1,6 @@
 import fs from 'node:fs/promises';
 import { join } from 'node:path';
+import { $ } from 'execa';
 import { printDirname } from '../common/utils.mjs';
 
 const cwd = join(printDirname(), '..', '..');
@@ -20,4 +21,51 @@ export async function updateVersion(target, upgrade) {
   }
 
   await fs.writeFile(pkgPath, JSON.stringify(updatedPkgJson, null, 2));
+}
+
+export async function getAllPackages() {
+  const { stdout: info } = await $`yarn workspaces info`;
+  const workspaces = JSON.parse(info);
+  const pkgs = Object.keys(workspaces);
+  return pkgs;
+}
+
+/*
+  By passing a project (package) name, this function will go through all workspace 
+  and will upgrade the version if any package has `project` inside its `dependency` or `devDependency`. 
+*/
+export async function upgradeDepndendentsOf(project, dist) {
+  const { stdout: info } = await $`yarn workspaces info`;
+  const workspaces = JSON.parse(info);
+
+  // Going through all workspace and find packages which depends on `project`
+  const dependents = [];
+  const pkgs = Object.keys(workspaces);
+  pkgs.forEach((pkg) => {
+    if (
+      workspaces[pkg].workspaceDependencies.includes(project) ||
+      workspaces[pkg].mismatchedWorkspaceDependencies.includes(project)
+    ) {
+      dependents.push(pkg);
+    }
+  });
+
+  if (dependents.length === 0) {
+    console.log(`It seems ${project} isn't used by any packages. Skip...`);
+    return;
+  }
+
+  console.log(`These packages are using ${project}: ${dependents.join(',')} \n`);
+
+  const { stdout: npmInfo } = await $`yarn info ${project}@${dist} --json`;
+  const versions = JSON.parse(npmInfo).data['dist-tags'];
+  const version = versions[dist];
+
+  console.log(`NPM version for ${project} is ${version}. \n`);
+
+  await Promise.all(
+    dependents.map((pkg) =>
+      updateVersion({ path: workspaces[pkg].location }, { name: project, version }),
+    ),
+  );
 }
