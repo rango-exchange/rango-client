7365cbdde25537040bc70578db59e5792e6000b5
RanGojo <rangojo@rango.exchange>
2023-04-25 10:22:00 +0000

    added is loaded to manager react state

--- queue-manager/react/src/types.ts
+++ queue-manager/react/src/types.ts
@@ -1,3 +1,8 @@
 import { Manager } from '@rango-dev/queue-manager-core';
 
 export type ManagerContext = Manager | undefined;
+export type ManagerState =
+  | {
+      isLoaded: boolean;
+    }
+  | undefined;
