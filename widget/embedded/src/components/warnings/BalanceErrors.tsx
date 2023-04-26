9ca88e8d3133da4fdffc12fd8f6c82c6651c36b6
RanGojo <rangojo@rango.exchange>
2023-04-26 18:04:30 +0000

    fix warning in balance errors comp

--- widget/embedded/src/components/warnings/BalanceErrors.tsx
+++ widget/embedded/src/components/warnings/BalanceErrors.tsx
@@ -36,8 +36,8 @@ export function BalanceErrors({ messages }: PropTypes) {
       </Typography>
       <Spacer size={8} direction="vertical" />
       <List showListStyle={showListStyle}>
-        {messages.map((warning) => (
-          <ListItem showListStyle={showListStyle}>
+        {messages.map((warning, index) => (
+          <ListItem showListStyle={showListStyle} key={index}>
             <Message variant="body2">{warning}</Message>
           </ListItem>
         ))}
