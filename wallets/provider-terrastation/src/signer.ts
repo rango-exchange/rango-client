3e178f7b0f823b584ba3a0a4c16eeb728c1af198
mikasackermn <mikasa@rango.exchange>
2023-04-29 04:47:52 +0000

    Added TERRA to terra station supported Chains
    (cherry picked from commit d139d666464c77af4e7d21922503ed40de1c273f)

--- wallets/provider-terrastation/src/signer.ts
+++ wallets/provider-terrastation/src/signer.ts
@@ -1,85 +1,10 @@
-import { JSONSerializable } from '@terra-money/terra.js/dist/util/json';
-import { Coin, CreateTxOptions, Msg as TerraMsg } from '@terra-money/terra.js';
-import { Fee } from '@terra-money/terra.js/dist/core';
-import {
-  CosmosTransaction,
-  defaultSigners,
-  WalletSigners,
-  WalletType,
-} from '@rango-dev/wallets-shared';
+import { DefaultCosmosSigner } from '@rango-dev/signer-cosmos';
+import { SignerFactory, TransactionType as TxType } from 'rango-types';
 
-export default function getSigners(provider: any): WalletSigners {
-  return {
-    ...defaultSigners({
-      provider,
-      walletType: WalletType.TERRA_STATION,
-      supportEvm: false,
-    }),
-    executeCosmosMessage: async (
-      cosmos: CosmosTransaction
-    ): Promise<string> => {
-      return new Promise<string>((resolve, reject) => {
-        provider
-          .post(cosmosTxToTerraTx(cosmos))
-          .then(
-            (result: { result: { txhash: string | PromiseLike<string> } }) =>
-              resolve(result?.result?.txhash)
-          )
-          .catch((error: any) => {
-            console.log({ error });
-            reject(error);
-          });
-      });
-    },
-  };
-}
+// TODO: Change signer to Terra signer  
 
-function cosmosTxToTerraTx(tx: CosmosTransaction): CreateTxOptions {
-  let tmpStdFee: Fee | undefined = undefined;
-  if (tx.data.fee) {
-    const tmpCoinsFee = tx.data.fee.amount.map(
-      (item) => new Coin(item.denom, item.amount)
-    );
-    // let coins = Coins()
-    tmpStdFee = new Fee(parseInt(tx.data.fee.gas), tmpCoinsFee);
-  }
-
-  const msgs = tx.data.msgs.map(
-    (m) => new TerraMessageGeneralJsonSerializable(m) as unknown as TerraMsg
-  );
-
-  return {
-    msgs,
-    fee: tmpStdFee,
-    memo: tx.data.memo || '',
-    gas: tx.data.fee?.gas,
-  };
-}
-
-export class TerraMessageGeneralJsonSerializable extends JSONSerializable<
-  any,
-  any,
-  any
-> {
-  constructor(public raw: any) {
-    super();
-  }
-
-  public static fromData(data: any): TerraMessageGeneralJsonSerializable {
-    return new TerraMessageGeneralJsonSerializable(data);
-  }
-
-  toData(): any {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { __type, value, ...rest } = this.raw;
-    return JSON.parse(JSON.stringify({ ...rest, ...value }));
-  }
-
-  toAmino(): any {
-    return this.toData();
-  }
-
-  toProto(): any {
-    return this.toData();
-  }
+export default function getSigners(provider: any): SignerFactory {
+  const signers = new SignerFactory();
+  signers.registerSigner(TxType.COSMOS, new DefaultCosmosSigner(provider));
+  return signers;
 }
