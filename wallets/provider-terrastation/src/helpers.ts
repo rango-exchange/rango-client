a48f66ad38166651398966544dae140db3e8a060
mikasackermn <mikasa@rango.exchange>
2023-05-06 11:53:57 +0000

    changed terrastation chains

--- wallets/provider-terrastation/src/helpers.ts
+++ wallets/provider-terrastation/src/helpers.ts
@@ -2,29 +2,22 @@ import { NetworkInfo, WalletController } from '@terra-money/wallet-controller';
 
 export const TERRA_STATION_WALLET_ID = 'station';
 
-const testnet: NetworkInfo = {
-  name: 'testnet',
-  chainID: 'tequila-0004',
-  lcd: 'https://tequila-lcd.terra.dev',
-  walletconnectID: 0,
-};
-const mainnet: NetworkInfo = {
-  name: 'mainnet',
+const classic: NetworkInfo = {
+  name: 'classic',
   chainID: 'columbus-5',
   lcd: 'https://lcd.terra.dev',
   walletconnectID: 1,
 };
-const terra: NetworkInfo = {
-  name: 'terra',
+const mainnet: NetworkInfo = {
+  name: 'mainnet',
   chainID: 'phoenix-1',
   lcd: 'https://phoenix-lcd.terra.dev',
   walletconnectID: 2,
 };
 
 const walletConnectChainIds: Record<number, NetworkInfo> = {
-  0: testnet,
+  0: classic,
   1: mainnet,
-  2: terra,
 };
 
 let controller: any;
