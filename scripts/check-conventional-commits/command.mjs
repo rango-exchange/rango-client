07ed3d50a0de96b73ef7b35f89f7b9aa2fb452fa
Eren Yeager <eren@rango.exchange>
2023-05-07 05:52:41 +0000

    chore: adding conventional commit check

---
+++ scripts/check-conventional-commits/command.mjs
@@ -0,0 +1,31 @@
+import { execa } from 'execa';
+import { detectChannel, logAsSection } from '../publish/utils.mjs';
+import parser from 'conventional-commits-parser';
+import filter from 'conventional-commits-filter';
+
+async function run() {
+  const channel = detectChannel();
+
+  logAsSection('Run...', `at ${channel}..HEAD`);
+
+  const { stdout: logs } = await execa('git', [
+    'log',
+    `origin/${channel}..HEAD`,
+    '--pretty=format:%B__________',
+  ]);
+  const commits = logs.split('__________').filter(Boolean);
+  const parsedCommits = filter(commits.map(parser.sync));
+  const hasAnyConventionalCommit = parsedCommits.some((commit) => !!commit.type);
+
+  if (hasAnyConventionalCommit) {
+    console.log('found a conventional commit.');
+    console.debug(parsedCommits);
+  } else {
+    throw new Error('There is no conventional commit. you need at least one.');
+  }
+}
+
+run().catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
