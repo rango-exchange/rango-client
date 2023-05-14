60708fc0ea8ddd21c028ce4f87bcaa6ddc9c7ee9
Shinji <shinji@rango.exchange>
2023-05-14 06:33:48 +0000

    test widget playground deployment

--- widget/playground/src/index.tsx
+++ widget/playground/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
-import { BrowserRouter } from 'react-router-dom';
 
 const container = document.getElementById('app')!;
 const root = createRoot(container);
