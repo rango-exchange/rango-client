e94cb6728fcf4168ec504ccb87890c8e07fe84d4
RanGojo <rangojo@rango.exchange>
2023-05-27 15:04:55 +0000

    refactor(signers): separate DefaultSignerFactory and SignerFactory

--- wallets/provider-cosmostation/src/signer.ts
+++ wallets/provider-cosmostation/src/signer.ts
@@ -1,12 +1,16 @@
 import { DefaultEvmSigner } from '@rango-dev/signer-evm';
 import { DefaultCosmosSigner } from '@rango-dev/signer-cosmos';
 import { Network, getNetworkInstance } from '@rango-dev/wallets-shared';
-import { SignerFactory, TransactionType as TxType } from 'rango-types';
+import {
+  DefaultSignerFactory,
+  SignerFactory,
+  TransactionType as TxType,
+} from 'rango-types';
 
 export default function getSigners(provider: any): SignerFactory {
   const ethProvider = getNetworkInstance(provider, Network.ETHEREUM);
   const cosmosProvider = getNetworkInstance(provider, Network.COSMOS);
-  const signers = new SignerFactory();
+  const signers = new DefaultSignerFactory();
   signers.registerSigner(TxType.EVM, new DefaultEvmSigner(ethProvider));
   signers.registerSigner(
     TxType.COSMOS,
