78c213abcbd2f0651052c47a6093ad72a6e7a755
Eren Yeager <eren@rango.exchange>
2023-05-27 01:48:52 +0000

    clean up widget

---
+++ widget/embedded/src/index.ts
@@ -0,0 +1,18 @@
+import {
+  WidgetTheme,
+  WidgetConfig,
+  WidgetColors,
+  BlockchainAndTokenConfig,
+} from './types';
+import { WidgetProps, Widget } from './Widget';
+import { WalletType } from '@rango-dev/wallets-shared';
+
+export type {
+  WidgetConfig,
+  WalletType,
+  WidgetTheme,
+  WidgetColors,
+  BlockchainAndTokenConfig,
+  WidgetProps,
+};
+export { Widget };
