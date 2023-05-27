8537120ef7663afbd9efc2721e8e815c9b8d5e42
RanGojo <rangojo@rango.exchange>
2023-05-27 17:38:20 +0000

    several improvements in replace tx detection

---
+++ signers/signer-evm/src/helper.ts
@@ -0,0 +1,21 @@
+import { SignerError, SignerErrorCode } from 'rango-types';
+
+export const cleanEvmError = (error: any): SignerError => {
+  let message = undefined;
+  if (
+    error &&
+    Object.prototype.hasOwnProperty.call(error, 'message') &&
+    Object.prototype.hasOwnProperty.call(error, 'code') &&
+    Object.prototype.hasOwnProperty.call(error, 'reason')
+  ) {
+    message = `Error sending the tx (code: ${(error as any).code})`;
+  } else if (
+    error &&
+    Object.prototype.hasOwnProperty.call(error, 'code') &&
+    Object.prototype.hasOwnProperty.call(error, 'message') &&
+    (error as any)?.code === -32603
+  ) {
+    return new SignerError(SignerErrorCode.SEND_TX_ERROR, undefined, (error as any).message);
+  }
+  return new SignerError(SignerErrorCode.SEND_TX_ERROR, message, error);
+};
--- signers/signer-evm/src/signer.ts
+++ signers/signer-evm/src/signer.ts
@@ -1,10 +1,8 @@
-import {
-  TransactionRequest,
-  TransactionResponse,
-} from '@ethersproject/abstract-provider';
+import { TransactionRequest, TransactionResponse } from '@ethersproject/abstract-provider';
 import { GenericSigner, SignerError, SignerErrorCode } from 'rango-types';
 import { EvmTransaction } from 'rango-types/lib/api/main';
 import { providers } from 'ethers';
+import { cleanEvmError } from './helper';
 
 export class DefaultEvmSigner implements GenericSigner<EvmTransaction> {
   private signer: providers.JsonRpcSigner;
@@ -26,7 +24,7 @@ export class DefaultEvmSigner implements GenericSigner<EvmTransaction> {
   async signAndSendTx(
     tx: EvmTransaction,
     address: string,
-    chainId: string | null
+    chainId: string | null,
   ): Promise<{ hash: string; response: TransactionResponse }> {
     try {
       const transaction = DefaultEvmSigner.buildTx(tx);
@@ -39,85 +37,60 @@ export class DefaultEvmSigner implements GenericSigner<EvmTransaction> {
       ) {
         throw new SignerError(
           SignerErrorCode.UNEXPECTED_BEHAVIOUR,
-          `Signer chainId: '${signerChainId}' doesn't match with required chainId: '${chainId}' for tx.`
+          `Signer chainId: '${signerChainId}' doesn't match with required chainId: '${chainId}' for tx.`,
         );
       }
-      if (
-        !!signerAddress &&
-        !!address &&
-        signerAddress.toLowerCase() !== address.toLowerCase()
-      ) {
+      if (!!signerAddress && !!address && signerAddress.toLowerCase() !== address.toLowerCase()) {
         throw new SignerError(
           SignerErrorCode.UNEXPECTED_BEHAVIOUR,
-          `Signer address: '${signerAddress.toLowerCase()}' doesn't match with required address: '${address.toLowerCase()}' for tx.`
+          `Signer address: '${signerAddress.toLowerCase()}' doesn't match with required address: '${address.toLowerCase()}' for tx.`,
         );
       }
       const response = await this.signer.sendTransaction(transaction);
       return { hash: response.hash, response };
     } catch (error) {
-      let message = undefined;
-      if (
-        error &&
-        error.hasOwnProperty('message') &&
-        error.hasOwnProperty('code') &&
-        error.hasOwnProperty('reason')
-      ) {
-        message = `Error sending the tx (code: ${(error as any).code})`;
-      } else if (
-        error &&
-        error.hasOwnProperty('code') &&
-        error.hasOwnProperty('message') &&
-        (error as any)?.code === -32603
-      ) {
-        throw new SignerError(
-          SignerErrorCode.SEND_TX_ERROR,
-          undefined,
-          (error as any).message
-        );
-      }
-      throw new SignerError(SignerErrorCode.SEND_TX_ERROR, message, error);
+      throw cleanEvmError(error);
     }
   }
 
   async wait(
     txHash: string,
     txResponse?: TransactionResponse,
-    confirmations?: number | undefined
+    confirmations?: number | undefined,
   ): Promise<{ hash: string; response?: TransactionResponse }> {
     try {
-      if (!!txResponse) {
+      if (txResponse) {
         await txResponse?.wait(confirmations);
         return { hash: txHash };
       }
       if (!this.provider) return { hash: txHash }; // wallet not connected yet
       const tx = await this.provider.getTransaction(txHash);
-      if (!tx)
-        throw Error(`Transaction hash '${txHash}' not found in blockchain.`);
+      if (!tx) throw Error(`Transaction hash '${txHash}' not found in blockchain.`);
       await tx.wait(confirmations);
       return { hash: txHash };
     } catch (err) {
       const error = err as any; // TODO find a proper type
       if (error?.code === 'TRANSACTION_REPLACED' && error?.replacement)
         return { hash: error?.replacement?.hash, response: error?.replacement };
-      throw err;
+      throw cleanEvmError(error);
     }
   }
 
   static buildTx(evmTx: EvmTransaction): TransactionRequest {
     let tx: TransactionRequest = {};
-    if (!!evmTx.from) tx = { ...tx, from: evmTx.from };
-    if (!!evmTx.to) tx = { ...tx, to: evmTx.to };
-    if (!!evmTx.data) tx = { ...tx, data: evmTx.data };
-    if (!!evmTx.value) tx = { ...tx, value: evmTx.value };
-    if (!!evmTx.nonce) tx = { ...tx, nonce: evmTx.nonce };
-    if (!!evmTx.gasLimit) tx = { ...tx, gasLimit: evmTx.gasLimit };
-    if (!!evmTx.gasPrice)
+    if (evmTx.from) tx = { ...tx, from: evmTx.from };
+    if (evmTx.to) tx = { ...tx, to: evmTx.to };
+    if (evmTx.data) tx = { ...tx, data: evmTx.data };
+    if (evmTx.value) tx = { ...tx, value: evmTx.value };
+    if (evmTx.nonce) tx = { ...tx, nonce: evmTx.nonce };
+    if (evmTx.gasLimit) tx = { ...tx, gasLimit: evmTx.gasLimit };
+    if (evmTx.gasPrice)
       tx = {
         ...tx,
         gasPrice: '0x' + parseInt(evmTx.gasPrice).toString(16),
       };
-    if (!!evmTx.maxFeePerGas) tx = { ...tx, maxFeePerGas: evmTx.maxFeePerGas };
-    if (!!evmTx.maxPriorityFeePerGas)
+    if (evmTx.maxFeePerGas) tx = { ...tx, maxFeePerGas: evmTx.maxFeePerGas };
+    if (evmTx.maxPriorityFeePerGas)
       tx = { ...tx, maxPriorityFeePerGas: evmTx.maxPriorityFeePerGas };
     return tx;
   }
