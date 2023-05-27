4e06622d2b0f76312b65416f4a38bd40428db27e
Eren Yeager <eren@rango.exchange>
2023-05-27 01:37:30 +0000

    using latest if the version is greater

--- scripts/common/npm.mjs
+++ scripts/common/npm.mjs
@@ -2,6 +2,7 @@ import fs from 'node:fs/promises';
 import { $ } from 'execa';
 import { join } from 'node:path';
 import { printDirname } from '../common/utils.mjs';
+import { compareSemVer } from 'semver-parser';
 
 const cwd = join(printDirname(), '..', '..');
 
@@ -27,25 +28,53 @@ export async function readPackageJson(project) {
 }
 
 export async function packageVersionOnNPM(project, dist) {
-  let version;
-
   const { content, update } = await readPackageJson(project);
   const local_version = content.version;
 
   if (content.private) {
     console.log(`[info] local_version as npm_version because it is a private package.`);
-    version = local_version;
-  } else {
-    const { stdout: npmInfo } = await $`yarn info ${project}@${dist} --json`;
-    const versions = JSON.parse(npmInfo).data['dist-tags'];
-    version = versions[dist];
+    return {
+      npm_version: local_version,
+      local_version,
+    };
+  }
+
+  // Continue if it's not a private package.
+  const { stdout: npmInfo } = await $`yarn info ${project}@${dist} --json`;
+  const versions = JSON.parse(npmInfo).data['dist-tags'];
+
+  // Fallback to local version, if package isn't published on NPM yet.
+  let npm_version = versions[dist] || local_version;
+
+  // We should check if `latest` version is greater than `dist` version (next), use latest version instead.
+  // Because it will get stuck on a version forever.
+  console.log(`::debug::${dist} ${JSON.stringify(versions)}`);
+  if (dist !== 'latest') {
+    const latest_version = versions['latest'];
+    const dist_version = versions[dist];
+
+    console.log(
+      `::debug::latest: ${latest_version}, dist: ${dist_version}, result: ${compareSemVer(
+        dist_version,
+        latest_version,
+      )} cond: ${compareSemVer(dist_version, latest_version) > 0} `,
+    );
+    // compareSemVer returns 1 if dist_version is greater than latest_version.
+    if (compareSemVer(dist_version, latest_version) > 0) {
+      npm_version = dist_version;
+    } else {
+      console.log(
+        `[info] latest version: ${latest_version}, ${dist} version: ${dist_version}. we use latest version because it's greater that ${dist_version}`,
+      );
+      npm_version = latest_version;
+    }
   }
 
   // update pkg json
-  content.version = version;
+  content.version = npm_version;
   await update(content);
   return {
-    npm_version: version,
+    npm_version,
     local_version,
   };
 }
@@ -54,7 +83,8 @@ export async function overrideNPMVersionOnLocal(project, dist) {
   const versions = await packageVersionOnNPM(project, dist);
 
   if (versions.npm_version !== versions.local_version) {
-    console.log(`WARNING! Mismatch version on local and npm for ${project}`);
+    console.log(`::warning::Mismatch version on local and npm for ${project}`);
+    console.log(`${JSON.stringify(versions)}`);
   }
 
   return versions;
