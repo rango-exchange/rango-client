c593c165174626ef0c519c3f41ee20f6a6a2f8ee
Eren Yeager <eren@rango.exchange>
2023-05-27 01:36:04 +0000

    Improve logging and measuring each step

--- scripts/publish/command.mjs
+++ scripts/publish/command.mjs
@@ -2,8 +2,6 @@
 'use strict';
 
 import {
-  addChangelogsToStage,
-  addUpdatedPackageJsonToStage,
   buildPackages,
   changed,
   deployProjectsToVercel,
@@ -19,11 +17,12 @@ import {
   packageNamesToPackagesWithInfo,
   publishPackages,
   pushToRemote,
-  tagPackages,
+  tagPackagesAndCommit,
 } from './utils.mjs';
 import { Graph } from '../common/graph/index.mjs';
 import { nxToGraph } from '../common/graph/helpers.mjs';
-import { execa } from 'execa';
+import { execa, $ } from 'execa';
+import { performance } from 'node:perf_hooks';
 
 // TODO: Working directory should be empty.
 async function run() {
@@ -46,33 +45,44 @@ async function run() {
     process.exit(0);
   }
 
+  console.log(`::group::Analyze & Depndency Graph`);
+  performance.mark('start-analyze');
   const nxGraph = await exportNx();
   const graph = new Graph();
   nxToGraph(nxGraph, graph);
   graph.onlyAffected(changedPkgs.map((pkg) => pkg.name));
   const sortedList = graph.sort();
   const sortedPackagesToPublish = await packageNamesToPackagesWithInfo([...sortedList]);
+  performance.mark('end-analyze');
 
-  console.log('Packages will be published, in this order:\n', sortedPackagesToPublish);
+  console.log(
+    `Execution time: ${performance.measure('analyze', 'start-analyze', 'end-analyze').duration}ms`,
+  );
+  console.log(
+    'Packages will be published, in this order:\n',
+    sortedPackagesToPublish.map((pkg) => pkg.name).join(' -> '),
+  );
+  console.log('::endgroup::');
 
   const updatedPackages = [];
   for (const pkg of sortedPackagesToPublish) {
     const updatedPkg = await publish(pkg, channel);
     updatedPackages.push(updatedPkg);
   }
-
-  // Add updated dependencies
-  await addUpdatedPackageJsonToStage(sortedPackagesToPublish);
+  // We did some changes to package's depencies, we need to update lockfile as well.
+  await $`yarn`;
 
   // Git tag & commit
+  console.log(`::group::Tag, commit and push to repository.`);
   logAsSection(`Git Tagging..`);
   const tagOptions = channel === 'prod' ? { skipGitTagging: false } : { skipGitTagging: true };
-  await tagPackages(updatedPackages, tagOptions);
+  await tagPackagesAndCommit(updatedPackages, tagOptions);
 
   logAsSection(`Pushing tags to remote...`);
   const branch = channel === 'prod' ? 'main' : 'next';
   await pushToRemote(branch);
   logAsSection(`Pushed.`);
+  console.log('::endgroup::');
 }
 
 run().catch((e) => {
@@ -85,7 +95,8 @@ run().catch((e) => {
 // TODO: add yarn.lock
 async function publish(changedPkg, channel) {
   // TODO: If affected pkg, commit a fix: upgrade pkg
-  console.log('Publish:', `${changedPkg.name} (${channel})`);
+  console.log(`::group::Publish ${changedPkg.name} (${channel})`);
+  performance.mark(`start-publish-${changedPkg.name}`);
 
   // 1. Version
   let updatedPackages;
@@ -104,7 +115,6 @@ async function publish(changedPkg, channel) {
   // 2. Changelog & Github Release
   if (channel === 'prod') {
     await Promise.all(updatedPackages.map((pkg) => generateChangelog(pkg, { saveToFile: true })));
-    await addChangelogsToStage(updatedPackages);
 
     await Promise.all(updatedPackages.map(makeGithubRelease));
     logAsSection(`[x] Github Release & Changelog generated.`);
@@ -113,11 +123,25 @@ async function publish(changedPkg, channel) {
   // 3. Build & Publish on NPM
   const packages = groupPackagesForDeploy(updatedPackages);
   if (packages.npm.length) {
+    performance.mark(`start-publish-build-${changedPkg.name}`);
     await buildPackages(packages.npm);
-    logAsSection('[x] Build for NPM');
+    performance.mark(`end-publish-build-${changedPkg.name}`);
+    const duration_build = performance.measure(
+      `publish-build-${changedPkg.name}`,
+      `start-publish-build-${changedPkg.name}`,
+      `end-publish-build-${changedPkg.name}`,
+    ).duration;
+    logAsSection(`[x] Build for NPM ${duration_build}ms`);
+    performance.mark(`start-publish-npm-${changedPkg.name}`);
     const distTag = channel === 'prod' ? 'latest' : 'next';
     await publishPackages(packages.npm, distTag);
-    logAsSection('[x] Publish on NPM');
+    performance.mark(`end-publish-npm-${changedPkg.name}`);
+    const duration_npm = performance.measure(
+      `publish-npm-${changedPkg.name}`,
+      `start-publish-npm-${changedPkg.name}`,
+      `end-publish-npm-${changedPkg.name}`,
+    ).duration;
+    logAsSection(`[x] Publish on NPM ${duration_npm}ms`);
   }
 
   // 4. Publish to Vercel
@@ -136,8 +160,23 @@ async function publish(changedPkg, channel) {
   }
 
   // 6. Yarn upgrade-all
-  await execa('yarn', ['upgrade-all', changedPkg.name]);
+  const { stdout: upgradeStdOut } = await execa('yarn', [
+    'upgrade-all',
+    changedPkg.name,
+    '--no-install',
+  ]);
+  console.log(`::debug::yarn upgrade-all ${changedPkg.name} '--no-install'`);
+  console.log(`::debug::${upgradeStdOut}`);
   logAsSection('[x] Upgrade all package users.');
+
+  performance.mark(`end-publish-${changedPkg.name}`);
+  const duration_publish = performance.measure(
+    `publish-${changedPkg.name}`,
+    `start-publish-${changedPkg.name}`,
+    `end-publish-${changedPkg.name}`,
+  ).duration;
+  console.log(`Execution time: ${duration_publish}ms`);
+  console.log(`::endgroup::`);
 
   return updatedPackages[0];
 }
