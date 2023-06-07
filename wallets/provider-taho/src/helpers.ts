2bc9b77ff52912142872a52579c3a69406700f1e
mikasackermn <mikasa@rango.exchange>
2023-06-07 15:44:46 +0000

    fixed supported chains for taho wallet

--- wallets/provider-taho/src/helpers.ts
+++ wallets/provider-taho/src/helpers.ts
@@ -1,3 +1,5 @@
+import { Network } from '@rango-dev/wallets-shared';
+
 export function taho() {
   const { tally } = window;
 
@@ -5,3 +7,12 @@ export function taho() {
 
   return tally;
 }
+
+export const TAHO_WALLET_SUPPORTED_CHAINS = [
+  Network.ETHEREUM,
+  Network.POLYGON,
+  Network.OPTIMISM,
+  Network.ARBITRUM,
+  Network.AVAX_CCHAIN,
+  Network.BINANCE,
+];
