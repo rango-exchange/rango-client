57e9a898dc5be0109ef5821bce5059a052dc7aba
RanGojo <rangojo@rango.exchange>
2023-06-10 16:44:58 +0000

    fix: fix signer wait change network issues

--- signers/signer-evm/src/signer.ts
+++ signers/signer-evm/src/signer.ts
@@ -8,6 +8,38 @@ import { providers } from 'ethers';
 import { cleanEvmError } from './helper';
 import { SignerError, SignerErrorCode } from 'rango-types';
 
+const waitMs = (ms: number) =>
+  new Promise((resolve) => setTimeout(resolve, ms));
+
+const waitWithMempoolCheck = async (
+  provider: providers.Web3Provider,
+  tx: TransactionResponse,
+  txHash: string,
+  confirmations?: number
+) => {
+  let finished = false;
+  return await Promise.race([
+    (async () => {
+      await tx.wait(confirmations);
+      finished = true;
+    })(),
+    (async () => {
+      while (!finished) {
+        await waitMs(3_000);
+        if (finished) return null;
+        try {
+          const mempoolTx = await provider.getTransaction(txHash);
+          if (!mempoolTx) return null;
+        } catch (error) {
+          console.log({ error });
+          return null;
+        }
+      }
+      return null;
+    })(),
+  ]);
+};
+
 export class DefaultEvmSigner implements GenericSigner<EvmTransaction> {
   private signer: providers.JsonRpcSigner;
   private provider: providers.Web3Provider;
@@ -31,14 +63,11 @@ export class DefaultEvmSigner implements GenericSigner<EvmTransaction> {
     chainId: string | null
   ): Promise<{ hash: string; response: TransactionResponse }> {
     try {
+      this.signer = this.provider.getSigner(tx.from ?? undefined);
       const transaction = DefaultEvmSigner.buildTx(tx);
       const signerChainId = await this.signer.getChainId();
       const signerAddress = await this.signer.getAddress();
-      if (
-        !!chainId &&
-        !!signerChainId &&
-        signerChainId.toString().toLowerCase() !== chainId?.toLowerCase()
-      ) {
+      if (!!chainId && !!signerChainId && signerChainId !== parseInt(chainId)) {
         throw new SignerError(
           SignerErrorCode.UNEXPECTED_BEHAVIOUR,
           `Signer chainId: '${signerChainId}' doesn't match with required chainId: '${chainId}' for tx.`
@@ -54,8 +83,24 @@ export class DefaultEvmSigner implements GenericSigner<EvmTransaction> {
           `Signer address: '${signerAddress.toLowerCase()}' doesn't match with required address: '${address.toLowerCase()}' for tx.`
         );
       }
-      const response = await this.signer.sendTransaction(transaction);
-      return { hash: response.hash, response };
+      try {
+        const response = await this.signer.sendTransaction(transaction);
+        return { hash: response.hash, response };
+      } catch (error: any) {
+        // retrying EIP-1559 without v2 related fields
+        if (
+          !!error?.message &&
+          typeof error.message === 'string' &&
+          error.message.indexOf('EIP-1559') !== -1
+        ) {
+          console.log('retrying EIP-1559 error without v2 fields ...');
+          const transaction = DefaultEvmSigner.buildTx(tx, true);
+          const response = await this.signer.sendTransaction(transaction);
+          return { hash: response.hash, response };
+        } else {
+          throw error;
+        }
+      }
     } catch (error) {
       throw cleanEvmError(error);
     }
@@ -63,20 +108,35 @@ export class DefaultEvmSigner implements GenericSigner<EvmTransaction> {
 
   async wait(
     txHash: string,
-    _chainId: string,
+    chainId?: string,
     txResponse?: TransactionResponse,
-    confirmations?: number | undefined
+    confirmations?: number
   ): Promise<{ hash: string; response?: TransactionResponse }> {
     try {
+      // if we have transaction response, use that to wait
+      // otherwise, try to get tx response from the wallet provider
       if (txResponse) {
+        // if we use waitWithMempoolCheck here, we can't detect replaced tx anymore
         await txResponse?.wait(confirmations);
         return { hash: txHash };
       }
-      if (!this.provider) return { hash: txHash }; // wallet not connected yet
+
+      // ignore wait if wallet not connected yet
+      if (!this.provider) return { hash: txHash };
+      this.signer = this.provider.getSigner();
+
+      // don't proceed if signer chain changed or chain id is not specified
+      // because if user change the wallet network, we receive null on getTransaction
+      if (!chainId) return { hash: txHash };
+      const signerChainId = await this.signer.getChainId();
+      if (!signerChainId || parseInt(chainId) != signerChainId)
+        return { hash: txHash };
+
       const tx = await this.provider.getTransaction(txHash);
       if (!tx)
         throw Error(`Transaction hash '${txHash}' not found in blockchain.`);
-      await tx.wait(confirmations);
+
+      await waitWithMempoolCheck(this.provider, tx, txHash, confirmations);
       return { hash: txHash };
     } catch (err) {
       const error = err as any; // TODO find a proper type
@@ -86,7 +146,7 @@ export class DefaultEvmSigner implements GenericSigner<EvmTransaction> {
     }
   }
 
-  static buildTx(evmTx: EvmTransaction): TransactionRequest {
+  static buildTx(evmTx: EvmTransaction, disableV2 = false): TransactionRequest {
     let tx: TransactionRequest = {};
     if (evmTx.from) tx = { ...tx, from: evmTx.from };
     if (evmTx.to) tx = { ...tx, to: evmTx.to };
@@ -99,8 +159,9 @@ export class DefaultEvmSigner implements GenericSigner<EvmTransaction> {
         ...tx,
         gasPrice: '0x' + parseInt(evmTx.gasPrice).toString(16),
       };
-    if (evmTx.maxFeePerGas) tx = { ...tx, maxFeePerGas: evmTx.maxFeePerGas };
-    if (evmTx.maxPriorityFeePerGas)
+    if (evmTx.maxFeePerGas && !disableV2)
+      tx = { ...tx, maxFeePerGas: evmTx.maxFeePerGas };
+    if (evmTx.maxPriorityFeePerGas && !disableV2)
       tx = { ...tx, maxPriorityFeePerGas: evmTx.maxPriorityFeePerGas };
     return tx;
   }
