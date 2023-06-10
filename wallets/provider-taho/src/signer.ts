8b88a09b046976601f04f51c49ba6130c3ec8f30
mikasackermn <mikasa@rango.exchange>
2023-06-10 07:54:56 +0000

    changed network to networks

--- wallets/provider-taho/src/signer.ts
+++ wallets/provider-taho/src/signer.ts
@@ -1,5 +1,9 @@
 import { DefaultEvmSigner } from '@rango-dev/signer-evm';
-import { DefaultSignerFactory, SignerFactory, TransactionType as TxType } from 'rango-types';
+import {
+  DefaultSignerFactory,
+  SignerFactory,
+  TransactionType as TxType,
+} from 'rango-types';
 
 export default function getSigners(provider: any): SignerFactory {
   const signers = new DefaultSignerFactory();
