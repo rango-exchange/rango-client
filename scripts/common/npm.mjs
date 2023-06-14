eb4d43a8e2a940501b70efe44bb8e9b6a59cfb92
Eren Yeager <eren@rango.exchange>
2023-06-14 04:09:07 +0000

    chore: fix publish packages for the first time

--- scripts/common/npm.mjs
+++ scripts/common/npm.mjs
@@ -33,7 +33,7 @@ export async function packageVersionOnNPM(project, dist) {
 
   if (content.private) {
     console.log(
-      `[info] local_version as npm_version because it is a private package.`
+      `::notice::considering npm_version same as local_version because it is a private package.`
     );
     return {
       npm_version: local_version,
@@ -42,12 +42,24 @@ export async function packageVersionOnNPM(project, dist) {
   }
 
   // Continue if it's not a private package.
-  const { stdout: npmInfo, stderr } =
-    await $`yarn info ${project}@${dist} --json`;
-  if (!!stderr) {
-    throw new Error(stderr);
+
+  const { status, body } = await requestNpmPackageInfo(project).catch((e) => {
+    console.log(`::error::npm_request_failed: ${e}`);
+    throw new Error(e);
+  });
+
+  // A new package which never has been published on npm.
+  if (status === 404) {
+    console.log(
+      `::notice::considering npm_version same as local_version because it is a new package.`
+    );
+    return {
+      npm_version: local_version,
+      local_version,
+    };
   }
-  const versions = JSON.parse(npmInfo).data['dist-tags'];
+
+  const versions = body['dist-tags'];
 
   // Fallback to local version, if package isn't published on NPM yet.
   let npm_version = versions[dist] || local_version;
@@ -94,4 +106,28 @@ export async function overrideNPMVersionOnLocal(project, dist) {
   }
 
   return versions;
+}
+
+async function requestNpmPackageInfo(name) {
+  const headers = new Headers();
+  // This is to use less bandwidth unless we really need to get the full response.
+  // See https://github.com/npm/npm-registry-client#request
+  headers.append(
+    'Accept',
+    'application/vnd.npm.install-v1+json; q=1.0, application/json; q=0.8, */*'
+  );
+  const res = await fetch(`https://registry.npmjs.org/${escapeName(name)}`, {
+    headers,
+  });
+  const body = await res.json();
+
+  return {
+    status: res.status,
+    body,
+  };
+}
+
+function escapeName(name) {
+  // scoped packages contain slashes and the npm registry expects them to be escaped
+  return name.replace('/', '%2f');
 }
