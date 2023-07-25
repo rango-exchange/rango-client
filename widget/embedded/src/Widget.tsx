f7414b0507dbf751bcad9d5de8476b9a9c32f811
Shinji <shinji@rango.exchange>
2023-07-25 17:36:37 +0000

    fix: fix zustand persistance issue with ssr

--- widget/embedded/src/Widget.tsx
+++ widget/embedded/src/Widget.tsx
@@ -18,6 +18,7 @@ import { navigationRoutes } from './constants/navigationRoutes';
 import { initConfig } from './utils/configs';
 import { WidgetContext, WidgetWallets } from './Wallets';
 import { WidgetEvents } from './components/WidgetEvents';
+import { useSettingsStore } from './store/settings';
 
 const MainContainer = styled('div', {
   width: '100%',
@@ -50,6 +51,7 @@ export function Main(props: PropsWithChildren<WidgetProps>) {
   }, [config]);
 
   useEffect(() => {
+    useSettingsStore.persist.rehydrate();
     widgetContext.onConnectWallet(setLastConnectedWalletWithNetwork);
   }, []);
 
