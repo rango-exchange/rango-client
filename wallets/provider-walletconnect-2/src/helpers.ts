b32dca24f6e43a521fb7f9b8b6ce5fe3a76c1df5
samobasquiat <samo.basquiat@rango.exchange>
2023-07-30 12:49:01 +0000

    fix: update package & tsconfig & remove comments in wallet-connect-2

--- wallets/provider-walletconnect-2/src/helpers.ts
+++ wallets/provider-walletconnect-2/src/helpers.ts
@@ -1,7 +1,6 @@
 import { EthereumProvider } from '@walletconnect/ethereum-provider';
 
-const projectId = 'f5196d081862c6f2b81c04520ea9301c';
-// const relayUrl = 'wss://relay.walletconnect.com';
+const PROJECT_ID = 'f5196d081862c6f2b81c04520ea9301c';
 
 export function supportsForSwitchNetworkRequest(provider: any): boolean {
   const wallets = ['metamask'];
@@ -21,35 +20,30 @@ export async function makeConnection(options: {
 }): Promise<any> {
   const { provider, chainId = 1, force = false } = options;
 
-  const ethProvider =
-    provider ||
-    (await EthereumProvider.init({
-      projectId, // REQUIRED your projectId
-      chains: [chainId],
-      // REQUIRED chain ids
-      showQrModal: true, // REQUIRED set to "true" to use @web3modal/standalone,
-      optionalMethods: [
-        'eth_sendTransaction',
-        'eth_signTransaction',
-        'eth_sign',
-        'personal_sign',
-        'eth_signTypedData',
-        'wallet_switchEthereumChain',
-        'wallet_addEthereumChain',
-      ],
-      events: [
-        'chainChanged',
-        'accountsChanged',
-        'message',
-        'connect',
-        'disconnect',
-      ],
-      qrModalOptions: {
-        explorerAllowList: [],
-        explorerDenyList: [],
-        themeVariables: {
-          '--w3m-z-index': '999999999',
-        },
+  const ethProvider = await EthereumProvider.init({
+    projectId: PROJECT_ID, // REQUIRED your projectId
+    chains: [chainId],
+    // REQUIRED chain ids
+    showQrModal: true, // REQUIRED set to "true" to use @web3modal/standalone,
+    optionalMethods: [
+      'eth_sendTransaction',
+      'eth_signTransaction',
+      'eth_sign',
+      'personal_sign',
+      'eth_signTypedData',
+      'wallet_switchEthereumChain',
+      'wallet_addEthereumChain',
+    ],
+    events: [
+      'chainChanged',
+      'accountsChanged',
+      'message',
+      'connect',
+      'disconnect',
+    ],
+    qrModalOptions: {
+      themeVariables: {
+        '--w3m-z-index': '999999999',
       },
     }));
 
