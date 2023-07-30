46a4e53976554fd3877772196f3af2ebd91d88c0
Shinji <shinji@rango.exchange>
2023-07-30 11:33:30 +0000

    chore: improve auto connect wallets

--- wallets/provider-leap-cosmos/src/index.ts
+++ wallets/provider-leap-cosmos/src/index.ts
@@ -5,6 +5,7 @@ import {
   Subscribe,
   getCosmosAccounts,
   WalletInfo,
+  CanEagerConnect,
 } from '@rango-dev/wallets-shared';
 import { leap_cosmos_instance, getSupportedChains } from './helpers';
 import signer from './signer';
@@ -41,6 +42,8 @@ export const subscribe: Subscribe = ({ connect, disconnect }) => {
 };
 
 export const getSigners: (provider: any) => SignerFactory = signer;
+
+export const canEagerConnect: CanEagerConnect = () => Promise.resolve(true);
 
 export const getWalletInfo: (allBlockChains: BlockchainMeta[]) => WalletInfo = (
   allBlockChains
