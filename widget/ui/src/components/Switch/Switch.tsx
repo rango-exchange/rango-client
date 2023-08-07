eefda492514f9b7651e18140b84090ce4787946f
samobasquiat <samo.basquiat@rango.exchange>
2023-08-07 07:06:55 +0000

    feat: re-design switch & radio component

--- widget/ui/src/components/Switch/Switch.tsx
+++ widget/ui/src/components/Switch/Switch.tsx
@@ -1,49 +1,6 @@
 import React from 'react';
-import * as RadixSwitch from '@radix-ui/react-switch';
-
-import { styled } from '../../theme';
-
-const StyledSwitchRoot = styled(RadixSwitch.Root, {
-  boxSizing: 'border-box',
-  boxShadow: 'none',
-  borderStyle: 'solid',
-  width: '42px',
-  height: '22px',
-  backgroundColor: '$neutral600',
-  borderColor: '$neutral600',
-  borderRadius: '99999px',
-  position: 'relative',
-  padding: '0',
-  cursor: 'pointer',
-
-  '&[data-state="checked"]': {
-    backgroundColor: '$success',
-    borderColor: '$success',
-  },
-  '-webkit-tap-highlight-color': 'rgba(0, 0, 0, 0)',
-});
-
-const StyledSwitchThumb = styled(RadixSwitch.Thumb, {
-  position: 'absolute',
-  top: '0',
-  boxSizing: 'border-box',
-  display: 'block',
-  width: '18px',
-  height: '18px',
-  backgroundColor: '$white',
-  transition: ' transform 300ms',
-  borderRadius: '999999px',
-  willChange: 'transform',
-
-  '&[data-state="checked"]': {
-    transform: 'translateX(20px)',
-  },
-});
-
-export interface PropTypes {
-  checked: boolean;
-  onChange?: (checked: boolean) => void;
-}
+import { StyledSwitchRoot, StyledSwitchThumb } from './Switch.styles';
+import { PropTypes } from './Switch.types';
 
 export function Switch(props: PropTypes) {
   const { checked, onChange } = props;
