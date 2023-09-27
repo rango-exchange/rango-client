881d8048b7921af5c632bc92cd96ed9f330b7791
mikasackermn <mikasa@rango.exchange>
2023-09-27 10:25:26 +0000

    update

--- widget/ui/src/components/ListItem/ListItem.types.ts
+++ widget/ui/src/components/ListItem/ListItem.types.ts
@@ -3,9 +3,8 @@ export type ListItemProps = {
   description?: string | React.ReactElement;
   start?: React.ReactNode;
   end?: React.ReactNode;
-  as?: 'div' | 'li' | 'button';
+  as?: 'div' | 'li';
   onClick?: () => void;
   onKeyUp?: React.KeyboardEventHandler<HTMLLIElement>;
   hasDivider?: boolean;
-  style?: React.CSSProperties;
 };
