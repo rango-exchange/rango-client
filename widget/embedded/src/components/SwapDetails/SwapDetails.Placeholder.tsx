825406fd85971d8e6f0ef9ea411565137817cd11
Nikaru <nikaru@rango.exchange>
2023-10-09 10:19:36 +0000

    feat: loading swap history item

--- widget/embedded/src/components/SwapDetails/SwapDetails.Placeholder.tsx
+++ widget/embedded/src/components/SwapDetails/SwapDetails.Placeholder.tsx
@@ -1,33 +1,69 @@
 import type { SwapDetailsPlaceholderPropTypes } from './SwapDetails.types';
 
 import { i18n } from '@lingui/core';
-import { NotFound, Spinner } from '@rango-dev/ui';
+import {
+  CopyIcon,
+  IconButton,
+  NotFound,
+  Skeleton,
+  Typography,
+} from '@rango-dev/ui';
 import React from 'react';
 
 import { navigationRoutes } from '../../constants/navigationRoutes';
 import { useNavigateBack } from '../../hooks/useNavigateBack';
 import { SuffixContainer } from '../HeaderButtons/HeaderButtons.styles';
 import { Layout } from '../Layout';
+import { LoadingSwapDetails } from '../LoadingSwapDetails';
 
-import { LoaderContainer, PlaceholderContainer } from './SwapDetails.styles';
+import {
+  Container,
+  HeaderDetails,
+  PlaceholderContainer,
+} from './SwapDetails.styles';
 
 export function SwapDetailsPlaceholder(props: SwapDetailsPlaceholderPropTypes) {
-  const { requestId, loading } = props;
+  const { requestId, showSkeleton } = props;
   const { navigateBackFrom } = useNavigateBack();
 
   return (
     <Layout
+      noPadding
       header={{
         title: i18n.t('Swap and Bridge'),
         onBack: navigateBackFrom.bind(null, navigationRoutes.swapDetails),
         suffix: <SuffixContainer />,
       }}>
-      <PlaceholderContainer>
-        {loading ? (
-          <LoaderContainer>
-            <Spinner size={24} />
-          </LoaderContainer>
-        ) : (
+      {showSkeleton && (
+        <Container>
+          <HeaderDetails>
+            <div className="row">
+              <Typography variant="label" size="large" color="neutral900">
+                {`${i18n.t('Request ID')}:`}
+              </Typography>
+              <div className="request-id">
+                <Typography variant="label" size="small" color="neutral900">
+                  <Skeleton width={60} height={10} variant="rounded" />
+                </Typography>
+                <IconButton variant="ghost">
+                  <CopyIcon size={16} color="gray" />
+                </IconButton>
+              </div>
+            </div>
+            <div className="row">
+              <Typography variant="label" size="large" color="neutral900">
+                {`${i18n.t('Created at')}:`}
+              </Typography>
+              <Typography variant="label" size="small" color="neutral900">
+                <Skeleton width={60} height={10} variant="rounded" />
+              </Typography>
+            </div>
+          </HeaderDetails>
+          <LoadingSwapDetails />
+        </Container>
+      )}
+      {!showSkeleton && (
+        <PlaceholderContainer>
           <NotFound
             title={i18n.t('Not found')}
             description={i18n.t(
@@ -36,8 +72,8 @@ export function SwapDetailsPlaceholder(props: SwapDetailsPlaceholderPropTypes) {
               { message: 'Swap with request ID = ${requestId} not found.' }
             )}
           />
-        )}
-      </PlaceholderContainer>
+        </PlaceholderContainer>
+      )}
     </Layout>
   );
 }
