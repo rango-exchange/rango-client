df40ea82582c35e836319a79f62c0dbbaed51147
RanGojo <rangojo@rango.exchange>
2023-10-11 17:07:51 +0000

    fix: update playground export code ui

--- widget/playground/src/App.tsx
+++ widget/playground/src/App.tsx
@@ -2,7 +2,7 @@ import { Widget, WidgetWallets } from '@rango-dev/widget-embedded';
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-import { WC_PROJECT_ID } from './configs';
+import { RANGO_PUBLIC_API_KEY, WC_PROJECT_ID } from './configs';
 import { ConfigContainer } from './containers/configContainer';
 import { useTheme } from './hook/useTheme';
 import { useConfigStore } from './store/config';
@@ -10,6 +10,8 @@ import { useConfigStore } from './store/config';
 export function App() {
   const { activeStyle } = useTheme();
   const config = useConfigStore.use.config();
+
+  const overridedConfig = { ...config, apiKey: RANGO_PUBLIC_API_KEY };
 
   return (
     <div className={activeStyle}>
@@ -21,14 +23,14 @@ export function App() {
           }}>
           <ConfigContainer>
             <Routes>
-              <Route path="/*" element={<Widget config={config} />} />
+              <Route path="/*" element={<Widget config={overridedConfig} />} />
             </Routes>
           </ConfigContainer>
         </WidgetWallets>
       ) : (
         <ConfigContainer>
           <Routes>
-            <Route path="/*" element={<Widget config={config} />} />
+            <Route path="/*" element={<Widget config={overridedConfig} />} />
           </Routes>
         </ConfigContainer>
       )}
---
+++ widget/playground/src/components/ExportConfigModal/ExportConfigModal.tsx
@@ -0,0 +1,147 @@
+import type {
+  ExportConfigModalProps,
+  ExportType,
+} from './ExportConfigModal.types';
+
+import {
+  CloseIcon,
+  Divider,
+  ExternalLinkIcon,
+  Modal,
+  ModalHeader,
+  TextField,
+  Typography,
+} from '@rango-dev/ui';
+import React, { Fragment, useState } from 'react';
+import {
+  atomDark as dark,
+  prism,
+} from 'react-syntax-highlighter/dist/esm/styles/prism';
+
+import { filterConfig, formatConfig } from '../../helpers';
+import { useTheme } from '../../hook/useTheme';
+import { initialConfig, useConfigStore } from '../../store/config';
+
+import { CodeBlock } from './CodeBlock';
+import {
+  APIKeyInputContainer,
+  ButtonsContainer,
+  ExternalLinkIconContainer,
+  Head,
+  HelpLinksContainer,
+  Link,
+  LinkContainer,
+  ModalFlex,
+  StyledButton,
+  StyledIconButton,
+} from './ExportConfigModal.styles';
+import { typesOfCodeBlocks } from './ExportConfigModal.types';
+
+export function ExportConfigModal(props: ExportConfigModalProps) {
+  const { open, onClose, config } = props;
+
+  const { activeTheme } = useTheme();
+  const [selected, setSelected] = useState<ExportType>('embedded');
+  const syntaxHighlighterTheme = activeTheme === 'dark' ? dark : prism;
+  const { filteredConfigForExport } = filterConfig(config, initialConfig);
+  const formatedConfig = formatConfig(filteredConfigForExport);
+  const apiKey = useConfigStore.use.config().apiKey;
+  const onChangeApiKey = useConfigStore.use.onChangeApiKey();
+
+  return (
+    <Modal
+      containerStyle={{
+        maxWidth: '1109px',
+        width: '90%',
+        maxHeight: '865px',
+        height: '90%',
+        padding: '$20',
+        fontFamily: '$primary',
+      }}
+      hasLogo={false}
+      header={
+        <ModalHeader>
+          <Typography variant="headline" size="large">
+            Export Code
+          </Typography>
+          <ModalFlex>
+            <StyledIconButton onClick={onClose} variant="ghost">
+              <CloseIcon color="gray" size={22} />
+            </StyledIconButton>
+          </ModalFlex>
+        </ModalHeader>
+      }
+      open={open}
+      onClose={onClose}
+      title="Export Code"
+      anchor="center">
+      <Divider size={32} />
+      <Head>
+        <APIKeyInputContainer>
+          <TextField
+            size="large"
+            variant="contained"
+            style={{ background: '$neutral100', borderRadius: '10px' }}
+            onChange={(e) => {
+              onChangeApiKey(e.target.value);
+            }}
+            name="apiKey"
+            value={apiKey}
+            label="Replace your key"
+            type="string"
+            placeholder="Enter API Key"
+          />
+        </APIKeyInputContainer>
+        <HelpLinksContainer>
+          <LinkContainer>
+            <Typography variant="title" size="xmedium" color="$neutral900">
+              <Link
+                href="https://docs.rango.exchange/widget-integration/overview"
+                target="_blank">
+                <ExternalLinkIconContainer>
+                  <ExternalLinkIcon color="gray" size={16} />
+                </ExternalLinkIconContainer>
+                &nbsp;Full Instructions
+              </Link>
+            </Typography>
+          </LinkContainer>
+          <LinkContainer>
+            <Typography variant="title" size="xmedium" color="$neutral900">
+              <Link
+                href="https://github.com/rango-exchange/widget-examples"
+                target="_blank">
+                <ExternalLinkIconContainer>
+                  <ExternalLinkIcon color="gray" size={16} />
+                </ExternalLinkIconContainer>
+                &nbsp;More Examples
+              </Link>
+            </Typography>
+          </LinkContainer>
+        </HelpLinksContainer>
+      </Head>
+      <Divider size={32} />
+      <ButtonsContainer>
+        {Object.keys(typesOfCodeBlocks).map((type, index) => {
+          const key = `block-${index}`;
+          return (
+            <Fragment key={key}>
+              <StyledButton
+                size="medium"
+                variant="contained"
+                type={selected === type ? 'secondary' : undefined}
+                onClick={() => setSelected(type as ExportType)}>
+                {type}
+              </StyledButton>
+            </Fragment>
+          );
+        })}
+      </ButtonsContainer>
+      <Divider size={12} />
+      <CodeBlock
+        language={typesOfCodeBlocks[selected].language}
+        theme={syntaxHighlighterTheme}>
+        {typesOfCodeBlocks[selected].generateCode(formatedConfig)}
+      </CodeBlock>
+    </Modal>
+  );
+}
