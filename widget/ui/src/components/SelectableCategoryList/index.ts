f32c7e1618e47a34b3ceffbb841c699735474f08
Nikaru <nikaru@rango.exchange>
2023-11-22 13:20:17 +0000

    fix: update blockchain category icons

--- widget/ui/src/components/SelectableCategoryList/index.ts
+++ widget/ui/src/components/SelectableCategoryList/index.ts
@@ -1,2 +1,3 @@
 export { SelectableCategoryList } from './SelectableCategoryList';
 export { BlockchainCategories } from './SelectableCategoryList.types';
+export { getCountCategories } from './SelectableCategoryList.helpers';
