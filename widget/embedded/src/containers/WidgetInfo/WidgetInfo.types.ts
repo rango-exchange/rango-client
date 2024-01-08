c665ed312dd73e76b97d039ca0ee9db922364ab1
Nikaru <nikaru@rango.exchange>
2024-01-08 09:57:09 +0000

    fix: persist language in store

--- widget/embedded/src/containers/WidgetInfo/WidgetInfo.types.ts
+++ widget/embedded/src/containers/WidgetInfo/WidgetInfo.types.ts
@@ -18,4 +18,5 @@ export interface WidgetInfoContextInterface {
     swappers: SwapperMeta[];
     loadingStatus: FetchStatus;
   };
+  resetLanguage: () => void;
 }
--- widget/playground/src/containers/StyleLayout/StyleLayout.General.tsx
+++ widget/playground/src/containers/StyleLayout/StyleLayout.General.tsx
@@ -7,6 +7,7 @@ import {
   LanguageIcon,
   Typography,
 } from '@rango-dev/ui';
+import { useWidget } from '@rango-dev/widget-embedded';
 import React, { useCallback, useState } from 'react';
 
 import { ItemPicker } from '../../components/ItemPicker';
@@ -37,6 +38,8 @@ export function General() {
   const fontFamily =
     useConfigStore.use.config().theme?.fontFamily || FONTS[0].value;
   const language = useConfigStore.use.config().language || LANGUAGES[0].value;
+  const { resetLanguage } = useWidget();
+
   const handleFontChange = (value: string) => {
     if (value) {
       onChangeTheme({
@@ -49,6 +52,7 @@ export function General() {
   const handleLanguageChange = (value: string) => {
     if (value) {
       onChangeLanguage(value);
+      resetLanguage();
     }
     onBack();
   };
