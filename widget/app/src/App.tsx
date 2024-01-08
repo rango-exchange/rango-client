90786576ffc347f6d3f340b34782d0ed721850a6
samobasquiat <samo.basquiat@rango.exchange>
2024-01-08 07:11:00 +0000

    fix: address apikey config bug in iframe

--- widget/app/src/App.tsx
+++ widget/app/src/App.tsx
@@ -14,32 +14,33 @@ export function App() {
 
   let config: WidgetConfig | undefined = undefined;
 
-  if (!!configParam) {
-    try {
-      config = JSON.parse(configParam, (_, value) => {
-        if (typeof value === 'string' && value[0] === '$') {
-          return value.replace('$', '#');
-        }
-        return value;
-      });
-    } catch (error) {
-      console.error('Widget config param is invalid!');
+  if (!configRef.current) {
+    if (!!configParam) {
+      try {
+        config = JSON.parse(configParam, (_, value) => {
+          if (typeof value === 'string' && value[0] === '$') {
+            return value.replace('$', '#');
+          }
+          return value;
+        });
+      } catch (error) {
+        console.error('Widget config param is invalid!');
+      }
+    } else {
+      /*
+       *TODO:
+       *The assumption here is this object won't be created on iframe so we are on dev mode.
+       *We should consider a more proper way.
+       */
+
+      config = {
+        apiKey: '',
+        walletConnectProjectId: WC_PROJECT_ID,
+      };
     }
-  } else {
-    /*
-     *TODO:
-     *The assumption here is this object won't be created on `iframe` so we are on dev mode.
-     *We should consider a more proper way.
-     */
-
-    config = {
-      apiKey: '',
-      walletConnectProjectId: WC_PROJECT_ID,
-    };
-  }
-
-  if (!!config) {
-    configRef.current = config;
+    if (!!config) {
+      configRef.current = config;
+    }
   }
 
   return (
