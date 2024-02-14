e43a48936a63804d688f3ad1408244d7f2ff32f2
RyukTheCoder <ryuk@rango.exchange>
2024-02-14 07:26:49 +0000

    feat: add cosmos account and signer for math wallet

--- wallets/provider-math-wallet/src/index.ts
+++ wallets/provider-math-wallet/src/index.ts
@@ -83,6 +83,9 @@ export const getWalletInfo: (allBlockChains: BlockchainMeta[]) => WalletInfo = (
 ) => {
   const evms = evmBlockchains(allBlockChains);
   const solana = solanaBlockchain(allBlockChains);
+  const cosmos = allBlockChains.filter(
+    (blockchainMeta) => blockchainMeta.name === Networks.COSMOS
+  );
   return {
     name: 'Math Wallet',
     img: 'https://raw.githubusercontent.com/rango-exchange/assets/main/wallets/math/icon.svg',
@@ -94,6 +97,6 @@ export const getWalletInfo: (allBlockChains: BlockchainMeta[]) => WalletInfo = (
       DEFAULT: 'https://mathwallet.org/en-us/',
     },
     color: '#2b2f25',
-    supportedChains: [...evms, ...solana],
+    supportedChains: [...evms, ...solana, ...cosmos],
   };
 };
