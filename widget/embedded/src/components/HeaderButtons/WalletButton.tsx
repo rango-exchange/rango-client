a8d2dd959ae7e25646f27c19c15e7eb674a564b9
mikasackermn <mikasa@rango.exchange>
2024-03-10 20:00:43 +0000

    fix: fix widget icon size issue and add connected wallet tooltip

--- widget/embedded/src/components/HeaderButtons/WalletButton.tsx
+++ widget/embedded/src/components/HeaderButtons/WalletButton.tsx
@@ -1,17 +1,37 @@
 import type { PropTypes } from './HeaderButtons.types';
 
 import { i18n } from '@lingui/core';
-import { Tooltip, WalletIcon } from '@rango-dev/ui';
+import { Image, Tooltip, WalletIcon } from '@rango-dev/ui';
 import React from 'react';
 
-import { ConnectedIcon, HeaderButton } from './HeaderButtons.styles';
+import { useWalletList } from '../../hooks/useWalletList';
+
+import {
+  ConnectedIcon,
+  HeaderButton,
+  rowStyles,
+  WalletImageContainer,
+} from './HeaderButtons.styles';
 
 function WalletButton(props: PropTypes) {
+  const { list } = useWalletList({});
+  const connectedWallets = list.filter(
+    (wallet) => wallet.state === 'connected'
+  );
+  const content = !connectedWallets.length ? (
+    i18n.t('Connect Wallet')
+  ) : (
+    <div className={rowStyles()}>
+      {connectedWallets.map((wallet) => (
+        <WalletImageContainer key={wallet.title}>
+          <Image src={wallet.image} size={14} />
+        </WalletImageContainer>
+      ))}
+    </div>
+  );
+
   return (
-    <Tooltip
-      container={props.container}
-      side="top"
-      content={i18n.t('Connect Wallet')}>
+    <Tooltip container={props.container} side="bottom" content={content}>
       <HeaderButton variant="ghost" size="small" onClick={props.onClick}>
         {props.isConnected && <ConnectedIcon />}
         <WalletIcon size={18} color="black" />
--- widget/ui/src/components/Tooltip/Tooltip.styles.ts
+++ widget/ui/src/components/Tooltip/Tooltip.styles.ts
@@ -1,6 +1,6 @@
 import * as RadixTooltip from '@radix-ui/react-tooltip';
 
-import { styled } from '../../theme';
+import { darkTheme, styled } from '../../theme';
 import { Typography } from '../Typography';
 
 export const TooltipContent = styled(RadixTooltip.Content, {
@@ -17,9 +17,11 @@ export const TooltipContent = styled(RadixTooltip.Content, {
 export const TooltipTypography = styled(Typography, {
   borderRadius: '$md',
   padding: '$5 $10',
-  boxShadow:
-    'inset 0 0 0.5px 1px rgba(255, 255, 255, 0.075), 0 0 0 1px rgba(0, 0, 0, 0.05), 0 0.3px 0.4px rgba(0, 0, 0, 0.02), 0.9px 1.5px rgba(0, 0, 0, 0.045), 0 3.5px 6px rgba(0, 0, 0, 0.09)',
-  backgroundColor: '$neutral100',
+  boxShadow: '5px 5px 10px 0px rgba(0, 0, 0, 0.10)',
+  backgroundColor: '$neutral200',
+  [`.${darkTheme} &`]: {
+    backgroundColor: '$neutral500',
+  },
 });
 
 export const TriggerContent = styled('div', {
