88c47e24ffa67d0d9b17c2f91ba5d707fd983712
Shinji <shinji@rango.exchange>
2024-03-11 07:41:21 +0000

    fix: fix queue manager context in widget embedded

--- widget/embedded/src/components/AppRouter.tsx
+++ widget/embedded/src/components/AppRouter.tsx
@@ -1,39 +1,11 @@
-import type { WalletType } from '@rango-dev/wallets-shared';
 import type { PropsWithChildren } from 'react';
 
-import { useQueueManager } from '@rango-dev/queue-manager-rango-preset';
-import { useWallets } from '@rango-dev/wallets-react';
-import { isEvmBlockchain } from 'rango-types';
 import React, { Fragment } from 'react';
 import { MemoryRouter, useInRouterContext } from 'react-router';
 
-import { useForceAutoConnect } from '../hooks/useForceAutoConnect';
-import { useAppStore } from '../store/AppStore';
-
-export function AppRouter({
-  children,
-  ...props
-}: PropsWithChildren & {
-  lastConnectedWallet: string;
-  disconnectedWallet: WalletType | undefined;
-  clearDisconnectedWallet: () => void;
-}) {
+export function AppRouter({ children }: PropsWithChildren) {
   const isRouterInContext = useInRouterContext();
   const Router = isRouterInContext ? Fragment : MemoryRouter;
-  const blockchains = useAppStore().blockchains();
-  const { canSwitchNetworkTo } = useWallets();
-
-  useForceAutoConnect();
-
-  const evmChains = blockchains.filter(isEvmBlockchain);
-
-  useQueueManager({
-    lastConnectedWallet: props.lastConnectedWallet,
-    clearDisconnectedWallet: props.clearDisconnectedWallet,
-    disconnectedWallet: props.disconnectedWallet,
-    evmChains,
-    canSwitchNetworkTo,
-  });
 
   return <Router>{children}</Router>;
 }
