1f0e0a1c6f6ee34c49dab6c316e2b2439cd22428
rangojo <rangojo@rango.exchange>
2024-03-11 20:13:22 +0000

    chore: fix translations for sort preference

--- widget/embedded/src/components/Quotes/SelectStrategy.tsx
+++ widget/embedded/src/components/Quotes/SelectStrategy.tsx
@@ -1,13 +1,38 @@
+import type { PreferenceType } from 'rango-sdk';
+
+import { i18n } from '@lingui/core';
 import { Select } from '@rango-dev/ui';
 import React from 'react';
 
-import { ROUTE_STRATEGY } from '../../constants/quote';
 import { useQuoteStore } from '../../store/quote';
 
 import { SelectContainer } from './Quotes.styles';
 
 export function SelectStrategy(props: { container: HTMLElement }) {
   const { updateQuotePartialState, sortStrategy } = useQuoteStore();
+
+  const ROUTE_STRATEGY: { value: PreferenceType; label: string }[] = [
+    {
+      value: 'SMART',
+      label: i18n.t('Smart Routing'),
+    },
+    {
+      value: 'FEE',
+      label: i18n.t('Lowest Fee'),
+    },
+    {
+      value: 'SPEED',
+      label: i18n.t('Fastest Transfer'),
+    },
+    {
+      value: 'NET_OUTPUT',
+      label: i18n.t('Maximum Return'),
+    },
+    {
+      value: 'PRICE',
+      label: i18n.t('Maximum Output'),
+    },
+  ];
 
   return (
     <SelectContainer>
