c833b6ac55f3c60a24e58b31c94e56098ac1e532
mikasackermn <mikasa@rango.exchange>
2024-03-13 10:43:45 +0000

    feat: show a modal with page reload when meta hasn't loaded

---
+++ widget/embedded/src/components/RefreshModal/RefreshModal.styles.ts
@@ -0,0 +1,9 @@
+import { Button, styled } from '@rango-dev/ui';
+
+export const RefreshButton = styled(Button, {
+  '& span': {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
