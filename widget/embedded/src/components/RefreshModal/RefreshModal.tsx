0d3842f9c2d7210cfd5a967d1f0e663ef3125421
mikasackermn <mikasa@rango.exchange>
2024-03-13 10:43:45 +0000

    feat: show a modal with page reload when meta hasn't loaded

---
+++ widget/embedded/src/components/RefreshModal/RefreshModal.tsx
@@ -0,0 +1,39 @@
+import type { PropTypes } from './RefreshModal.types';
+
+import { i18n } from '@lingui/core';
+import { Divider, MessageBox, RefreshIcon } from '@rango-dev/ui';
+import React from 'react';
+
+import { getContainer } from '../../utils/common';
+import { WatermarkedModal } from '../common/WatermarkedModal';
+
+import { RefreshButton } from './RefreshModal.styles';
+
+export function RefreshModal(props: PropTypes) {
+  const { open, onClose } = props;
+
+  return (
+    <WatermarkedModal
+      open={open}
+      dismissible
+      onClose={onClose}
+      container={getContainer()}>
+      <MessageBox
+        title={i18n.t('Something wrong')}
+        type="error"
+        description={i18n.t('Something Wrong. Please refresh the app')}>
+        <Divider size={30} />
+        <RefreshButton
+          variant="outlined"
+          size="large"
+          type="primary"
+          fullWidth
+          onClick={() => location.reload()}>
+          <RefreshIcon size={20} color="primary" />
+          <Divider size={4} direction="horizontal" />
+          {i18n.t('Refresh')}
+        </RefreshButton>
+      </MessageBox>
+    </WatermarkedModal>
+  );
+}
---
+++ widget/embedded/src/components/RefreshModal/index.ts
@@ -0,0 +1,1 @@
+export { Quotes } from './Quotes';
