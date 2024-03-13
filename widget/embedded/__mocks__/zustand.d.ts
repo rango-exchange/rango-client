9bdda6587c442a7c107ebbfef2f5d0a4cc3de74f
Eren Yeager <eren@rango.exchange>
2024-03-13 13:44:11 +0000

    fix: we don't need to mock zustand, since we are recrating store befor each test

--- widget/embedded/__mocks__/zustand.d.ts
+++
@@ -1,3 +0,0 @@
-import type { StateCreator } from 'zustand';
-export declare const create: () => <S>(createState: StateCreator<S>) => import("zustand").UseBoundStore<import("zustand").StoreApi<S>>;
-//# sourceMappingURL=zustand.d.ts.map
--- widget/embedded/vitest.config.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vitest" />
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  test: {
-    watch: false,
-    setupFiles: ['./src/setup-vitest.ts'],
-  },
-});
