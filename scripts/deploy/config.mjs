e8a2994a57f57f429963df8ece4455cf580ba4ca
Nikaru <nikaru@rango.exchange>
2024-04-09 11:20:03 +0000

    feat: add preview deployment to pr

--- scripts/deploy/config.mjs
+++ scripts/deploy/config.mjs
@@ -4,6 +4,9 @@ import process from 'node:process';
 const scope = `@rango-dev`;
 export const VERCEL_ORG_ID = process.env.VERCEL_ORG_ID;
 export const VERCEL_TOKEN = process.env.VERCEL_TOKEN;
+export const ENABLE_PREVIEW_DEPLOY = process.env.ENABLE_PREVIEW_DEPLOY;
+export const EXCLUDED_PACKAGES = ['@rango-dev/widget-iframe'];
+
 export const VERCEL_PACKAGES = {
   [`${scope}/wallets-demo`]: getEnvWithFallback('VERCEL_PROJECT_WALLETS'),
   [`${scope}/queue-manager-demo`]: getEnvWithFallback('VERCEL_PROJECT_Q'),
