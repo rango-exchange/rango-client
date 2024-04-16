ed38b057429793c11ddb355f32fa3010fd8364f0
RanGojo <rangojo@rango.exchange>
2024-04-16 14:41:54 +0000

    fix: improve solana transaction sign flow

--- signers/signer-solana/src/signer.ts
+++ signers/signer-solana/src/signer.ts
@@ -1,37 +1,9 @@
+import type { SolanaExternalProvider } from './utils/types';
 import type { GenericSigner, SolanaTransaction } from 'rango-types';
-
-import { executeSolanaTransaction } from './helpers';
-import {
-  PublicKey,
-  SendOptions,
-  Transaction,
-  TransactionSignature,
-  VersionedTransaction,
-} from '@solana/web3.js';
 
 import { SignerError, SignerErrorCode } from 'rango-types';
 
-// https://github.com/solana-labs/wallet-adapter/blob/01c6316ce0725e0a075d6adb237bbcb4128e76ad/packages/wallets/phantom/src/adapter.ts#L30
-export interface SolanaExternalProvider {
-  isPhantom?: boolean;
-  publicKey?: { toBytes(): Uint8Array };
-  isConnected: boolean;
-  signTransaction<T extends Transaction | VersionedTransaction>(
-    transaction: T
-  ): Promise<T>;
-  signAllTransactions<T extends Transaction | VersionedTransaction>(
-    transactions: T[]
-  ): Promise<T[]>;
-  signAndSendTransaction<T extends Transaction | VersionedTransaction>(
-    transaction: T,
-    options?: SendOptions
-  ): Promise<{ signature: TransactionSignature }>;
-  signMessage(message: Uint8Array): Promise<{ signature: Uint8Array }>;
-  request(...args: any[]): Promise<any>;
-  connect(...args: any[]): Promise<any>;
-  disconnect(): Promise<void>;
-  accountChanged(newPublicKey: PublicKey): any;
-}
+import { executeSolanaTransaction } from './utils/main';
 
 export class DefaultSolanaSigner implements GenericSigner<SolanaTransaction> {
   private provider: SolanaExternalProvider;
@@ -56,11 +28,7 @@ export class DefaultSolanaSigner implements GenericSigner<SolanaTransaction> {
   }
 
   async signAndSendTx(tx: SolanaTransaction): Promise<{ hash: string }> {
-    try {
-      const hash = await executeSolanaTransaction(tx, this.provider);
-      return { hash };
-    } catch (error) {
-      throw new SignerError(SignerErrorCode.SEND_TX_ERROR, undefined, error);
-    }
+    const hash = await executeSolanaTransaction(tx, this.provider);
+    return { hash };
   }
 }
