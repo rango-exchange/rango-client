42aa2b039dd910e8e44db473e1acd28689a8b43b
RyukTheCoder <ryuk@rango.exchange>
2024-04-23 08:26:54 +0000

    feat: add solflare snap connect and signer

--- signers/signer-solana/src/utils/index.ts
+++ signers/signer-solana/src/utils/index.ts
@@ -1,2 +1,5 @@
 export type { SolanaWeb3Signer, SolanaExternalProvider } from './types';
 export { generalSolanaTransactionExecutor } from './main';
+export { prepareTransaction } from './prepare';
+export { getSolanaConnection } from './helpers';
+export { simulateTransaction } from './simulate';
