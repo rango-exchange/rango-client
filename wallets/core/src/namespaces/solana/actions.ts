0ef9fe526b4997e25c63696e2baae557cdfd0fb0
Eren Yeager <eren@rango.exchange>
2024-04-27 15:12:45 +0000

    chore: add missing account subscriber for solana

--- wallets/core/src/namespaces/solana/actions.ts
+++ wallets/core/src/namespaces/solana/actions.ts
@@ -1,3 +1,44 @@
+import type { NamespaceProvider } from './types';
+import type { SubscriberCb } from '../../hub/namespace';
+
+import { AccountId } from 'caip';
+
 import { recommended as commonRecommended } from '../common/actions';
 
 export const recommended = [...commonRecommended];
+
+export function changeAccountSubscriber(
+  instance: () => NamespaceProvider
+): SubscriberCb {
+  return (context) => {
+    const solanaInstance = instance();
+
+    if (!solanaInstance) {
+      throw new Error(
+        'Are your wallet injected correctly and is evm compatible?'
+      );
+    }
+
+    const [, setState] = context.state();
+
+    solanaInstance.on('accountChanged', (publicKey: any) => {
+      setState('accounts', [
+        AccountId.format({
+          address: publicKey.toString(),
+          // TODO: export from core
+          chainId: {
+            namespace: 'solana',
+            reference: '5eykt4UsFv8P8NJdTREpY1vzqKqZKvdp',
+          },
+        }),
+      ]);
+      console.log('[solana] Accounts changed:', publicKey.toString(), {
+        context,
+      });
+    });
+
+    return () => {
+      // TODO: Write clean up
+    };
+  };
+}
