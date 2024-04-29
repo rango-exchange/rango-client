96396361fc2c3b85ef114d4b94543c3ecda6e107
Eren Yeager <eren@rango.exchange>
2024-04-29 15:31:36 +0000

    fix: replace vercel build command with a custom script

--- scripts/deploy/utils.mjs
+++ scripts/deploy/utils.mjs
@@ -1,9 +1,26 @@
 import { execa } from 'execa';
+import { cp, readFile, writeFile } from 'node:fs/promises';
 import { detectChannel } from '../common/github.mjs';
-import { ENABLE_PREVIEW_DEPLOY, EXCLUDED_PACKAGES, VERCEL_ORG_ID, VERCEL_PACKAGES, VERCEL_TOKEN } from './config.mjs';
+import {
+  ENABLE_PREVIEW_DEPLOY,
+  EXCLUDED_PACKAGES,
+  VERCEL_ORG_ID,
+  VERCEL_PACKAGES,
+  VERCEL_TOKEN,
+} from './config.mjs';
 import * as actionCore from '@actions/core';
 import { VercelError } from '../common/errors.mjs';
-import { packageNameWithoutScope, workspacePackages } from '../common/utils.mjs';
+import {
+  convertPackageLocationToFullPath,
+  packageJson,
+  packageNameWithoutScope,
+  workspacePackages,
+} from '../common/utils.mjs';
+import { getTransformedRoutes } from '@vercel/routing-utils';
+import { dirname, join } from 'node:path';
+import { mkdir } from 'node:fs/promises';
+
+import { fileURLToPath } from 'node:url';
 
 export function getVercelProjectId(packageName) {
   return VERCEL_PACKAGES[packageName];
@@ -41,6 +58,10 @@ export async function deploySingleProjectToVercel(pkg) {
     { env }
   );
 
+  const currentDir = dirname(fileURLToPath(import.meta.url));
+  const configPath = join(currentDir, 'vercel.json');
+  await makeOutputFolderForBuildOutputApi(pkg.location, configPath);
+
   await execa(
     'vercel',
     ['build', '--cwd', pkg.location, '--token', VERCEL_TOKEN],
@@ -51,16 +72,17 @@ export async function deploySingleProjectToVercel(pkg) {
     'vercel',
     [pkg.location, '--prebuilt', '--token', VERCEL_TOKEN],
     { env }
-  ).then((result) => result.stdout)
-  .catch((err) => {
-    throw new VercelError(
-      `An error occurred on deploy ${pkg.name} package \n ${err.message} \n ${err.stderr}`
-    );
-  });
+  )
+    .then((result) => result.stdout)
+    .catch((err) => {
+      throw new VercelError(
+        `An error occurred on deploy ${pkg.name} package \n ${err.message} \n ${err.stderr}`
+      );
+    });
 
   // Run tail -1 on the stdout to get the last line, because `vercel` command returns the URL in the last line.
-  const uRLPreview = await execa('tail', ['-1'], { input: vercelResult })
-    .then(result => result.stdout)
+  const urlPreview = await execa('tail', ['-1'], { input: vercelResult })
+    .then((result) => result.stdout)
     .catch((err) => {
       throw new VercelError(
         `An error occurred on get url preview for ${pkg.name} package \n ${err.message} \n ${err.stderr}`
@@ -69,9 +91,9 @@ export async function deploySingleProjectToVercel(pkg) {
 
   // set package name and url preview to github output for use in workflow
   const tagName = packageNameWithoutScope(pkg.name);
-  actionCore.setOutput(`${tagName}-url`, uRLPreview);
+  actionCore.setOutput(`${tagName}-url`, urlPreview);
 
-  console.log(`${tagName}-url:`, uRLPreview);
+  console.log(`${tagName}-url:`, urlPreview);
   console.log(`${pkg.name} deployed.`);
 }
 
@@ -94,7 +116,7 @@ export function groupPackagesForDeploy(packages) {
   return output;
 }
 
-export async function getClientsListToBeDeployed(){
+export async function getClientsListToBeDeployed() {
   /*
     Deploys packages based on the state of the `ENABLE_PREVIEW_DEPLOY` environment variable.
     if ENABLE_PREVIEW_DEPLOY is true, only packages that has project id in workflow environments will be deployed.
@@ -107,24 +129,117 @@ export async function getClientsListToBeDeployed(){
     if (EXCLUDED_PACKAGES.includes(pkg.name)) return false;
 
     if (ENABLE_PREVIEW_DEPLOY) {
-      const hasProjectId = getVercelProjectId(pkg.name) && getVercelProjectId(pkg.name) !== 'NOT SET' ;
+      const hasProjectId =
+        getVercelProjectId(pkg.name) &&
+        getVercelProjectId(pkg.name) !== 'NOT SET';
       return pkg.private && hasProjectId;
-    } 
-    else {
+    } else {
       return pkg.private;
     }
   });
 
-  if(ENABLE_PREVIEW_DEPLOY){
+  if (ENABLE_PREVIEW_DEPLOY) {
     console.log('preview deployment is enabled.');
-    console.log('these packages will be deployed:', listPackagesToBeDeployed.map(pkg=>pkg.name).join(', '));
-    console.log('note: if you need add more packages to be deployed, first you need to add vercel project id to workflow environments then follow documentation there.');
-  }
-  else{
+    console.log(
+      'these packages will be deployed:',
+      listPackagesToBeDeployed.map((pkg) => pkg.name).join(', ')
+    );
+    console.log(
+      'note: if you need add more packages to be deployed, first you need to add vercel project id to workflow environments then follow documentation there.'
+    );
+  } else {
     console.log('preview deployment is disabled.');
-    console.log('these packages will be deployed:', listPackagesToBeDeployed.map(pkg=>pkg.name).join(', '));
+    console.log(
+      'these packages will be deployed:',
+      listPackagesToBeDeployed.map((pkg) => pkg.name).join(', ')
+    );
   }
-
 
   return listPackagesToBeDeployed;
+}
+
+/**
+ *
+ * Reading a vercel.json file and convert into `Build Output API` compatible format.
+ * And also adding some more configs in addition to what exists in `vercel.json`.
+ *
+ * References:
+ * https://vercel.com/docs/build-output-api/v3/features
+ * https://vercel.com/docs/projects/project-configuration
+ *
+ * @param {Path} configPath
+ */
+export async function getVercelJsonAndMakeBuildOutputApi(configPath) {
+  const vercelJson = await readFile(configPath);
+  const parsedVercelJson = JSON.parse(vercelJson);
+
+  const supportedConfigs = ['rewrites'];
+  const unsupportedKeys = Object.keys(parsedVercelJson).filter(
+    (key) => !supportedConfigs.includes(key)
+  );
+  if (unsupportedKeys.length > 0) {
+    throw new Error(
+      "You've used unsupported config in vercel.json. Please make sure you've implemented it first. Supported configs:" +
+        supportedConfigs.join(',')
+    );
+  }
+  const configJsonRewrites = parsedVercelJson.rewrites;
+  const routes = getTransformedRoutes({
+    rewrites: configJsonRewrites,
+  });
+
+  // This copied from `vercel build` output.
+  routes.routes.unshift({
+    src: '^/[^./]+\\.[0-9a-f]{8}\\.(css|js|png|jpg|webp|avif|svg)$',
+    headers: {
+      'cache-control': 's-maxage=31536000, immutable',
+    },
+    continue: true,
+  });
+
+  const config = {
+    version: 3,
+    routes,
+  };
+
+  return config;
+}
+
+/**
+ *
+ * Vercel's Build Output API needs to create `.vercel/output` and put static files and a config in there.
+ * This function should be run after a client has been built
+ * then by running this function it copies dist folder and create a config.json that vercel needs for deployments.
+ *
+ * @param {Path} pkgLocation
+ * @param {Path} configPath
+ */
+export async function makeOutputFolderForBuildOutputApi(
+  pkgLocation,
+  configPath
+) {
+  const pkg = packageJson(pkgLocation);
+
+  if (!pkg.main) {
+    throw new Error(
+      "We relying on `main` field inside your package.json. Make sure you've set it."
+    );
+  }
+
+  // Paths
+  const pkgPath = convertPackageLocationToFullPath(pkgLocation);
+  const mainDir = dirname(join(pkgPath, pkg.main));
+  const targetDir = join(pkgPath, '.vercel/output');
+  const targetStaticDir = join(targetDir, 'static');
+
+  console.log(
+    `copying build artifacts to .vercel/output for ${pkgLocation}...`
+  );
+  await mkdir(targetStaticDir, { recursive: true });
+  await cp(mainDir, targetStaticDir, { recursive: true });
+
+  console.log(`creating config.json for ${pkgLocation}...`);
+  const buildOutput = await getVercelJsonAndMakeBuildOutputApi(configPath);
+  const targetConfigPath = join(targetDir, 'config.json');
+  await writeFile(targetConfigPath, JSON.stringify(buildOutput));
 }
