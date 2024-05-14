7c33ce99852010d848600b77f2e5f364c23d1d9f
RanGojo <rangojo@rango.exchange>
2024-05-14 05:29:21 +0000

    chore: update solana default rpc url

--- signers/signer-solana/src/utils/helpers.ts
+++ signers/signer-solana/src/utils/helpers.ts
@@ -2,7 +2,7 @@ import { Connection } from '@solana/web3.js';
 
 const IS_DEV = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
 const SOLANA_RPC_URL = !IS_DEV
-  ? 'https://icy-crimson-wind.solana-mainnet.quiknode.pro/c83f94ebeb39a6d6a9d2ab03d4cba2c2af83c5c0/'
+  ? 'https://purple-practical-friday.solana-mainnet.quiknode.pro/d94ab067f793d48c81354c78c86ae908d9fc1582/'
   : 'https://fluent-still-scion.solana-mainnet.discover.quiknode.pro/fc8be9b8ac7aea382ec591359628e16d8c52ef6a/';
 
 export function getSolanaConnection(): Connection {
