6edecbb14fd008fc741c892bfa3e025c10160b9b
mikasackermn <mikasa@rango.exchange>
2024-07-07 14:10:48 +0000

    feat: add support for Trezor hardware wallet

--- examples/wallets-adapter-demo/src/constants.ts
+++ examples/wallets-adapter-demo/src/constants.ts
@@ -1,1 +1,5 @@
 export const WC_PROJECT_ID = 'e24844c5deb5193c1c14840a7af6a40b';
+export const TREZOR_MANIFEST = {
+  appUrl: 'https://widget.rango.exchange/',
+  email: 'hi+trezorwidget@rango.exchange',
+};
