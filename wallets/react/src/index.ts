fd7d863ed774a2f772568cab7a59f055dc0b84e5
Eren Yeager <eren@rango.exchange>
2024-07-24 05:45:15 +0000

    chore: enable nodenext module system for wallets/react

--- wallets/react/src/index.ts
+++ wallets/react/src/index.ts
@@ -1,7 +1,7 @@
-export * from './legacy/helpers';
-export { default as Provider } from './provider';
-export { useWallets } from './legacy/hooks';
-export * from './legacy/types';
+export * from './legacy/helpers.js';
+export { default as Provider } from './provider.js';
+export { useWallets } from './legacy/hooks.js';
+export * from './legacy/types.js';
 
 export type { EventHandler } from '@rango-dev/wallets-core';
 export { Events, readAccountAddress } from '@rango-dev/wallets-core';
