535fb74e43a5db73d7932877ec4371d97f7f5ba9
Eren Yeager <eren@rango.exchange>
2024-07-24 05:34:31 +0000

    chore: move wallets react code to a subfolder called legacy
    this is part of preparation for our upcomin wallet management

--- wallets/react/src/index.ts
+++ wallets/react/src/index.ts
@@ -1,7 +1,7 @@
-export * from './helpers';
+export * from './legacy/helpers';
 export { default as Provider } from './provider';
-export { useWallets } from './hooks';
-export * from './types';
+export { useWallets } from './legacy/hooks';
+export * from './legacy/types';
 
 export type { EventHandler } from '@rango-dev/wallets-core';
 export { Events, readAccountAddress } from '@rango-dev/wallets-core';
