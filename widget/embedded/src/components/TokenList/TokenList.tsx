a4808ce22749ed89ce33751584828f2cddb5f659
mikasackermn <mikasa@rango.exchange>
2024-08-07 09:26:10 +0000

    fix: deleted list tag from TokenList

--- widget/embedded/src/components/TokenList/TokenList.tsx
+++ widget/embedded/src/components/TokenList/TokenList.tsx
@@ -127,132 +127,130 @@ export function TokenList(props: PropTypes) {
 
   const renderList = () => {
     return (
-      <List as="ul">
-        <VirtualizedList
-          endReached={hasNextPage ? loadNextPage : undefined}
-          itemContent={(index) => {
-            const token = tokens[index];
-            const tokenBalance = formatBalance(getBalanceFor(token));
-            const address = token.address || '';
-            const blockchain = blockchains.find(
-              (blockchain) => blockchain.name === token.blockchain
-            ) as BlockchainMeta;
-            const colors = createTintsAndShades(blockchain.color, 'main');
-            const customCssForTag = {
-              $$color: colors.main150,
-              [`.${darkTheme} &`]: {
-                $$color: colors.main750,
-              },
-              backgroundColor: '$$color',
-            };
-
-            const customCssForTagTitle = {
+      <VirtualizedList
+        endReached={hasNextPage ? loadNextPage : undefined}
+        itemContent={(index) => {
+          const token = tokens[index];
+          const tokenBalance = formatBalance(getBalanceFor(token));
+          const address = token.address || '';
+          const blockchain = blockchains.find(
+            (blockchain) => blockchain.name === token.blockchain
+          ) as BlockchainMeta;
+          const colors = createTintsAndShades(blockchain.color, 'main');
+          const customCssForTag = {
+            $$color: colors.main150,
+            [`.${darkTheme} &`]: {
               $$color: colors.main750,
-              [`.${darkTheme} &`]: {
-                $$color: colors.main150,
-              },
-              color: '$$color',
-            };
+            },
+            backgroundColor: '$$color',
+          };
 
-            return (
-              <div
+          const customCssForTagTitle = {
+            $$color: colors.main750,
+            [`.${darkTheme} &`]: {
+              $$color: colors.main150,
+            },
+            color: '$$color',
+          };
+
+          return (
+            <div
+              style={{
+                paddingRight: 5,
+              }}>
+              <ListItemButton
                 style={{
-                  paddingRight: 5,
-                }}>
-                <ListItemButton
-                  style={{
-                    width: '100%',
-                    overflow: 'hidden',
-                    height: '60px',
-                  }}
-                  tab-index={index}
-                  key={`${token.symbol}${token.address}`}
-                  id={`${token.symbol}${token.address}`}
-                  hasDivider
-                  onClick={() => onChange && onChange(tokens[index])}
-                  start={
-                    <ImageSection>
-                      <Image src={token.image} size={30} />
-                      {props.type !== 'custom-token' &&
-                        isTokenPinned(token, props.type) && (
-                          <Pin>
-                            <PinIcon size={12} color="gray" />
-                          </Pin>
-                        )}
-                    </ImageSection>
-                  }
-                  title={
-                    blockchain?.type === 'COSMOS' ||
-                    !!token.name ||
-                    (!token.name && !address) ? (
-                      <Title>
-                        <Typography variant="title" size="xmedium">
-                          {token.symbol}
-                        </Typography>
-                        <Divider direction="horizontal" size={4} />
-                        <Tag css={customCssForTag}>
-                          <TagTitle
+                  width: '100%',
+                  overflow: 'hidden',
+                  height: '60px',
+                }}
+                tab-index={index}
+                key={`${token.symbol}${token.address}`}
+                id={`${token.symbol}${token.address}`}
+                hasDivider
+                onClick={() => onChange && onChange(tokens[index])}
+                start={
+                  <ImageSection>
+                    <Image src={token.image} size={30} />
+                    {props.type !== 'custom-token' &&
+                      isTokenPinned(token, props.type) && (
+                        <Pin>
+                          <PinIcon size={12} color="gray" />
+                        </Pin>
+                      )}
+                  </ImageSection>
+                }
+                title={
+                  blockchain?.type === 'COSMOS' ||
+                  !!token.name ||
+                  (!token.name && !address) ? (
+                    <Title>
+                      <Typography variant="title" size="xmedium">
+                        {token.symbol}
+                      </Typography>
+                      <Divider direction="horizontal" size={4} />
+                      <Tag css={customCssForTag}>
+                        <TagTitle
+                          variant="body"
+                          size="xsmall"
+                          css={customCssForTagTitle}>
+                          {token.blockchain}
+                        </TagTitle>
+                      </Tag>
+                    </Title>
+                  ) : undefined
+                }
+                description={
+                  !!blockchain?.info &&
+                  !!address &&
+                  blockchain.type !== 'COSMOS'
+                    ? renderDesc({
+                        address,
+                        token: tokens[index],
+                        customCssForTag,
+                        customCssForTagTitle,
+                        name: token.name,
+                        url: blockchain.info.addressUrl
+                          .split('{wallet}')
+                          .join(address),
+                      })
+                    : token.name || undefined
+                }
+                end={
+                  action ? (
+                    action(token)
+                  ) : loadingWallet ? (
+                    <End>
+                      <Skeleton variant="text" size="large" width={70} />
+                      <Divider size={4} />
+                      <Skeleton variant="text" size="medium" width={50} />
+                    </End>
+                  ) : (
+                    tokenBalance && (
+                      <BalanceContainer>
+                        <TokenBalance variant="title" size="small">
+                          {tokenBalance.amount}
+                        </TokenBalance>
+                        <div />
+                        {tokenBalance.usdValue && (
+                          <Typography
                             variant="body"
-                            size="xsmall"
-                            css={customCssForTagTitle}>
-                            {token.blockchain}
-                          </TagTitle>
-                        </Tag>
-                      </Title>
-                    ) : undefined
-                  }
-                  description={
-                    !!blockchain?.info &&
-                    !!address &&
-                    blockchain.type !== 'COSMOS'
-                      ? renderDesc({
-                          address,
-                          token: tokens[index],
-                          customCssForTag,
-                          customCssForTagTitle,
-                          name: token.name,
-                          url: blockchain.info.addressUrl
-                            .split('{wallet}')
-                            .join(address),
-                        })
-                      : token.name || undefined
-                  }
-                  end={
-                    action ? (
-                      action(token)
-                    ) : loadingWallet ? (
-                      <End>
-                        <Skeleton variant="text" size="large" width={70} />
-                        <Divider size={4} />
-                        <Skeleton variant="text" size="medium" width={50} />
-                      </End>
-                    ) : (
-                      tokenBalance && (
-                        <BalanceContainer>
-                          <TokenBalance variant="title" size="small">
-                            {tokenBalance.amount}
-                          </TokenBalance>
-                          <div />
-                          {tokenBalance.usdValue && (
-                            <Typography
-                              variant="body"
-                              className={usdValueStyles()}
-                              size="xsmall">
-                              {`$${tokenBalance.usdValue}`}
-                            </Typography>
-                          )}
-                        </BalanceContainer>
-                      )
+                            className={usdValueStyles()}
+                            size="xsmall">
+                            {`$${tokenBalance.usdValue}`}
+                          </Typography>
+                        )}
+                      </BalanceContainer>
                     )
-                  }
-                />
-              </div>
-            );
-          }}
-          totalCount={tokens.length}
-          key={`${selectedBlockchain}-${searchedFor}-${balanceKey}`}
-        />
-      </List>
+                  )
+                }
+              />
+            </div>
+          );
+        }}
+        totalCount={tokens.length}
+        key={`${selectedBlockchain}-${searchedFor}-${balanceKey}`}
+      />
     );
   };
 
