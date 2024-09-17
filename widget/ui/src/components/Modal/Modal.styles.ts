a99d28b0c3d5b1ba06e668fcf52099e80bf0b569
RyukTheCoder <ryuk@rango.exchange>
2024-09-17 19:56:52 +0000

    fix: use radix data attributes

--- widget/ui/src/components/Modal/Modal.styles.ts
+++ widget/ui/src/components/Modal/Modal.styles.ts
@@ -1,7 +1,25 @@
 import * as Dialog from '@radix-ui/react-dialog';
 
-import { styled } from '../../theme.js';
+import { keyframes, styled } from '../../theme.js';
 import { IconButton } from '../IconButton/index.js';
+
+const DialogOverlayAnimateIn = keyframes({
+  '0%': {
+    backgroundColor: 'transparent',
+  },
+  '100%': {
+    backgroundColor: 'color-mix(in srgb, $neutral500 70%, transparent)',
+  },
+});
+
+const DialogOverlayAnimateOut = keyframes({
+  '0%': {
+    backgroundColor: 'color-mix(in srgb, $neutral500 70%, transparent)',
+  },
+  '100%': {
+    backgroundColor: 'transparent',
+  },
+});
 
 export const DialogOverlay = styled(Dialog.Overlay, {
   position: 'absolute',
@@ -11,18 +29,74 @@ export const DialogOverlay = styled(Dialog.Overlay, {
   bottom: '0',
   width: '100%',
   height: '100%',
-  backgroundColor: 'transparent',
   zIndex: 10,
   borderRadius: '$primary',
   overflow: 'hidden',
-  transition: 'background .35s',
+  backgroundColor: 'transparent',
 
-  variants: {
-    active: {
-      true: {
-        backgroundColor: 'color-mix(in srgb, $neutral500 70%, transparent)',
-      },
-    },
+  "&[data-state='open']": {
+    backgroundColor: 'color-mix(in srgb, $neutral500 70%, transparent)',
+    animation: `${DialogOverlayAnimateIn} .45s ease-in-out`,
+  },
+  "&[data-state='closed']": {
+    backgroundColor: 'transparent',
+    animation: `${DialogOverlayAnimateOut} .45s ease-in-out`,
+  },
+});
+
+const DialogContentRightAnimateIn = keyframes({
+  '0%': {
+    transform: 'translateX(100%)',
+  },
+  '100%': {
+    transform: 'translateX(0%)',
+  },
+});
+
+const DialogContentRightAnimateOut = keyframes({
+  '0%': {
+    transform: 'translateX(0%)',
+  },
+  '100%': {
+    transform: 'translateX(100%)',
+  },
+});
+const DialogContentCenterAnimateIn = keyframes({
+  '0%': {
+    top: '100%',
+    transform: 'translate(-50%, 100%)',
+  },
+  '100%': {
+    top: '50%',
+    transform: 'translate(-50%, -50%)',
+  },
+});
+
+const DialogContentCenterAnimateOut = keyframes({
+  '0%': {
+    top: '50%',
+    transform: 'translate(-50%, -50%)',
+  },
+  '100%': {
+    top: '100%',
+    transform: 'translate(-50%, 100%)',
+  },
+});
+const DialogContentBottomAnimateIn = keyframes({
+  '0%': {
+    transform: 'translateY(100%)',
+  },
+  '100%': {
+    transform: 'translateY(0%)',
+  },
+});
+
+const DialogContentBottomAnimateOut = keyframes({
+  '0%': {
+    transform: 'translateY(0%)',
+  },
+  '100%': {
+    transform: 'translateY(100%)',
   },
 });
 
@@ -34,52 +108,41 @@ export const DialogContent = styled(Dialog.Content, {
   flexDirection: 'column',
   zIndex: 9999999,
   position: 'absolute',
-  transition:
-    'transform .45s ease-in-out, top .45s ease-in-out, left .45s ease-in-out',
 
   variants: {
     anchor: {
       right: {
-        left: '100%',
+        right: '0%',
+        "&[data-state='open']": {
+          animation: `${DialogContentRightAnimateIn} .45s ease-in-out`,
+        },
+        "&[data-state='closed']": {
+          animation: `${DialogContentRightAnimateOut} .45s ease-in-out`,
+        },
       },
       center: {
-        top: '100%',
-        left: '50%',
-        transform: 'translateX(-50%)',
-      },
-      bottom: {
-        top: '100%',
-      },
-    },
-    active: {
-      true: {},
-    },
-  },
-  compoundVariants: [
-    {
-      active: true,
-      anchor: 'right',
-      css: {
-        transform: 'translateX(-100%)',
-      },
-    },
-    {
-      active: true,
-      anchor: 'center',
-      css: {
-        top: '50%',
         left: '50%',
         transform: 'translate(-50%, -50%)',
+        "&[data-state='open']": {
+          top: '50%',
+          animation: `${DialogContentCenterAnimateIn} .45s ease-in-out`,
+        },
+        "&[data-state='closed']": {
+          top: '100%',
+          animation: `${DialogContentCenterAnimateOut} .45s ease-in-out`,
+        },
+      },
+      bottom: {
+        bottom: 0,
+        "&[data-state='open']": {
+          animation: `${DialogContentBottomAnimateIn} .45s ease-in-out`,
+        },
+        "&[data-state='closed']": {
+          animation: `${DialogContentBottomAnimateOut} .45s ease-in-out`,
+        },
       },
     },
-    {
-      active: true,
-      anchor: 'bottom',
-      css: {
-        transform: 'translateY(-100%)',
-      },
-    },
-  ],
+  },
 });
 
 export const Flex = styled('div', {
--- widget/ui/src/components/Modal/Modal.tsx
+++ widget/ui/src/components/Modal/Modal.tsx
@@ -2,7 +2,7 @@ import type { ModalPropTypes } from './Modal.types.js';
 import type { PropsWithChildren } from 'react';
 
 import * as Dialog from '@radix-ui/react-dialog';
-import React, { useEffect, useRef, useState } from 'react';
+import React from 'react';
 
 import { CloseIcon } from '../../icons/index.js';
 import { BottomLogo } from '../BottomLogo/index.js';
@@ -10,7 +10,6 @@ import { Divider } from '../Divider/index.js';
 import { IconButton } from '../IconButton/index.js';
 import { Typography } from '../Typography/index.js';
 
-import { forceReflow } from './Modal.helpers.js';
 import {
   Content,
   DialogContent,
@@ -25,7 +24,6 @@ export function Modal(props: PropsWithChildren<ModalPropTypes>) {
     title,
     open,
     onClose,
-    onExit,
     styles,
     anchor = 'bottom',
     container = document.body,
@@ -39,107 +37,49 @@ export function Modal(props: PropsWithChildren<ModalPropTypes>) {
     hasCloseIcon = true,
   } = props;
 
-  const [status, setStatus] = useState<
-    'unmounted' | 'mounted' | 'activated' | 'deactivated'
-  >('unmounted');
-  const active = status === 'activated';
-  const isMount =
-    status == 'mounted' || status === 'activated' || status === 'deactivated';
-  const modalContainerRef = useRef<HTMLElement | null>(null);
-  const exitCallbackRef = useRef<ModalPropTypes['onExit']>();
-
   const handleBackDropClick = (open: boolean) => {
     if (dismissible && !open) {
       onClose();
     }
   };
 
-  useEffect(() => {
-    exitCallbackRef.current = onExit;
-  }, [onExit]);
-
-  useEffect(() => {
-    if (exitCallbackRef.current) {
-      modalContainerRef.current?.addEventListener(
-        'transitionend',
-        exitCallbackRef.current
-      );
-    }
-
-    if (!open) {
-      if (status === 'activated') {
-        setStatus('deactivated');
-        modalContainerRef.current?.addEventListener('transitionend', () => {
-          setStatus('unmounted');
-        });
-      } else {
-        setStatus('unmounted');
-      }
-    } else {
-      setStatus('mounted');
-    }
-
-    return () => {
-      if (exitCallbackRef.current) {
-        modalContainerRef.current?.removeEventListener(
-          'transitionend',
-          exitCallbackRef.current
-        );
-      }
-    };
-  }, [open]);
-
-  useEffect(() => {
-    if (!!container && isMount) {
-      if (modalContainerRef.current) {
-        forceReflow(modalContainerRef.current);
-      }
-      setStatus('activated');
-    }
-  }, [isMount, container]);
-
   return (
-    <Dialog.Root
-      open={status !== 'unmounted' && !!container}
-      onOpenChange={handleBackDropClick}>
+    <Dialog.Root open={open} onOpenChange={handleBackDropClick}>
       <Dialog.Portal container={container}>
-        <DialogOverlay active={active} />
-        <DialogContent
-          active={active}
-          css={styles?.container}
-          anchor={anchor}
-          ref={(ref) => (modalContainerRef.current = ref)}>
-          {header ?? (
-            <ModalHeader noTitle={!title && dismissible && !prefix}>
-              {prefix}
-              {title && (
-                <Typography variant="title" size="small">
-                  {title}
-                </Typography>
-              )}
-              <Flex>
-                {suffix}
-                {dismissible && hasCloseIcon && (
-                  <IconButton onClick={onClose} variant="ghost">
-                    <CloseIcon color="gray" size={14} />
-                  </IconButton>
+        <DialogOverlay>
+          <DialogContent css={styles?.container} anchor={anchor}>
+            {header ?? (
+              <ModalHeader noTitle={!title && dismissible && !prefix}>
+                {prefix}
+                {title && (
+                  <Typography variant="title" size="small">
+                    {title}
+                  </Typography>
                 )}
-              </Flex>
-            </ModalHeader>
-          )}
-          <Content css={styles?.content}>{children}</Content>
-          <Footer css={styles?.footer}>
-            {footer && <div className="footer__content">{footer}</div>}
+                <Flex>
+                  {suffix}
+                  {dismissible && hasCloseIcon && (
+                    <IconButton onClick={onClose} variant="ghost">
+                      <CloseIcon color="gray" size={14} />
+                    </IconButton>
+                  )}
+                </Flex>
+              </ModalHeader>
+            )}
+            <Content css={styles?.content}>{children}</Content>
+            <Footer css={styles?.footer}>
+              {footer && <div className="footer__content">{footer}</div>}
 
-            <div
-              className={`footer__logo ${
-                hasWatermark ? 'logo__show' : 'logo__hidden'
-              }`}>
-              <Divider size={12} />
-              <BottomLogo />
-            </div>
-          </Footer>
-        </DialogContent>
+              <div
+                className={`footer__logo ${
+                  hasWatermark ? 'logo__show' : 'logo__hidden'
+                }`}>
+                <Divider size={12} />
+                <BottomLogo />
+              </div>
+            </Footer>
+          </DialogContent>
+        </DialogOverlay>
       </Dialog.Portal>
     </Dialog.Root>
   );
