0e12d11b50a84eab95502feba99586b393fd5e67
RyukTheCoder <ryuk@rango.exchange>
2024-10-05 07:03:11 +0000

    fix: remove null memo from utxo transactions

--- wallets/provider-xdefi/src/cosmos-signer.ts
+++ wallets/provider-xdefi/src/cosmos-signer.ts
@@ -58,7 +58,7 @@ export class CustomCosmosSigner implements GenericSigner<CosmosTransaction> {
       recipient,
       binanceProvider,
       method,
-      memo
+      memo ?? undefined
     );
     return { hash };
   }
