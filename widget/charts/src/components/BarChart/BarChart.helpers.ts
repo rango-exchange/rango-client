bae5e72272807a297545592d4688c6998a60c764
mikasackermn <mikasa@rango.exchange>
2024-10-08 10:26:32 +0000

    fix: add prepare data function for chart package

--- widget/charts/src/components/BarChart/BarChart.helpers.ts
+++ widget/charts/src/components/BarChart/BarChart.helpers.ts
@@ -1,5 +1,12 @@
 /* eslint-disable @typescript-eslint/no-magic-numbers */
-import type { BarStackDataType } from './BarChart.types.js';
+import type {
+  BarStackDataType,
+  ChartOptionsType,
+  ColorBucketMapType,
+  DailyDataType,
+} from './BarChart.types.js';
+
+import { MAX_BAR_BUCKETS } from './BarChart.constants.js';
 
 export const getTotalValueDates = (
   data: BarStackDataType[],
@@ -53,4 +60,89 @@ export const getDaysRange = (lengthValue: number) => {
     return 30;
   }
   return 90;
+};
+
+export const prepareBarChartData = (chartOption: ChartOptionsType) => {
+  const { dailyData, label = 'Count' } = chartOption;
+  const chartData: BarStackDataType[] = [];
+  const colorBucketMap: ColorBucketMapType = new Map();
+  const buckets: string[] = [];
+
+  const { barChartColors } = chartOption;
+
+  // map sum of value for each bucket
+  const sumBucketMap = new Map<string, number>();
+  dailyData.forEach((dailyItem) => {
+    const keyItem = dailyItem.bucket || label;
+
+    const sum = sumBucketMap.get(keyItem) || 0;
+    const newValue = dailyItem.value;
+    sumBucketMap.set(keyItem, sum + newValue);
+  });
+
+  const sortedBucket = Array.from(sumBucketMap).sort((a, b) => b[1] - a[1]);
+
+  // get top buckets for stack bars
+  const topBucket = sortedBucket
+    .map((sortedItem) => sortedItem[0])
+    .slice(0, MAX_BAR_BUCKETS);
+
+  const bucketCount = sumBucketMap.size;
+
+  // create map structure for assign color for each bucket
+  topBucket.forEach((bucketItem, index) => {
+    colorBucketMap.set(
+      bucketItem,
+      barChartColors[index % barChartColors.length]
+    );
+    buckets.push(bucketItem);
+  });
+
+  if (bucketCount > MAX_BAR_BUCKETS) {
+    colorBucketMap.set('Others', barChartColors[barChartColors.length - 1]);
+    buckets.push('Others');
+  }
+
+  // create map structure for assign chart data for each date
+  const dateMap = new Map<string, DailyDataType[]>();
+  dailyData.forEach((dailyItem) => {
+    if (!dateMap.has(dailyItem.date)) {
+      dateMap.set(dailyItem.date, []);
+    }
+
+    const dateItem = dateMap.get(dailyItem.date);
+    dateItem?.push(dailyItem);
+  });
+
+  // create data result for bar stack chart
+  dateMap.forEach((dateDailyList, keyDate) => {
+    const dataItem: BarStackDataType = { date: keyDate };
+    dateDailyList
+      .filter((dailyItem) => topBucket.includes(dailyItem.bucket || label))
+      .forEach((topDailyItem) => {
+        const bucketValue = topDailyItem.value;
+        dataItem[topDailyItem.bucket || label] = bucketValue
+          ? bucketValue.toString()
+          : '0';
+      });
+
+    topBucket.forEach((topItem) => {
+      if (!(topItem in dataItem)) {
+        dataItem[topItem] = '0';
+      }
+    });
+
+    if (bucketCount > MAX_BAR_BUCKETS) {
+      const otherBuckets = dateDailyList.filter(
+        (dailyItem) => dailyItem.bucket && !topBucket.includes(dailyItem.bucket)
+      );
+      const othersValue = otherBuckets
+        .map((dailyItem) => dailyItem.value)
+        .reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+      dataItem['Others'] = othersValue.toString();
+    }
+
+    chartData.push(dataItem);
+  });
+  return { chartData, colorBucketMap, buckets };
 };
--- widget/charts/src/index.ts
+++ widget/charts/src/index.ts
@@ -2,5 +2,7 @@ export type {
   BarStackDataType,
   ColorBucketMapType,
   BarChartPropTypes,
+  ChartOptionsType,
+  DailyDataType,
 } from './components/BarChart/index.js';
-export { BarChart } from './components/BarChart/index.js';
+export { BarChart, prepareBarChartData } from './components/BarChart/index.js';
