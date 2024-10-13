0e211876b65f2cc0afe5f5f4ed895ba7fdc9d74c
mikasackermn <120931936+mikasackermn@users.noreply.github.com>
2024-10-13 17:35:58 +0330

    Update Widget.tsx

--- widget/embedded/src/containers/Widget/Widget.tsx
+++ widget/embedded/src/containers/Widget/Widget.tsx
@@ -9,7 +9,7 @@ import { DEFAULT_CONFIG } from '../../store/slices/config';
 import { Main } from '../App';
 import { WidgetProvider } from '../WidgetProvider';
 
-const WEB_NOTIFICATIONS_PROJECT_ID = 'ca78db14099bb7eb0935d32a93605eb6';
+const WEB_NOTIFICATIONS_PROJECT_ID = '731bc4e45a7d79d131daf407d6a86b64';
 const APP_DOMAIN = 'https://app.rango.exchange/';
 
 export function Widget(props: PropsWithChildren<WidgetProps>) {
