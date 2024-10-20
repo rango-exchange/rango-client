5ce48733753380674f6502b6321d43e6973e9b77
RyukTheCoder <ryuk@rango.exchange>
2024-10-20 00:34:23 +0000

    chore: add extract translations job to check workflow and add a wrapper to exit on extract error

---
+++ scripts/lingui/command.js
@@ -0,0 +1,20 @@
+// Wrapper around lingui command that exits with a non zero value if an error is detected. Lingui commands do not fail with a non-zero exit code in some cases. There is an open issue here: https://github.com/lingui/js-lingui/issues/1419
+const { exec } = require('child_process');
+
+const args = process.argv.splice(2);
+const command = `yarn lingui ${args.join(' ')}`;
+
+const handleLinguiCommand = function (error, stdout, stderr) {
+  if (error) {
+    // Handle exec command error (lingui command not found?)
+    console.log(`error: ${error}`);
+    throw error;
+  }
+  // Display command standard output
+  console.log(`stdout: ${stdout}`);
+  // Display command standard error
+  console.log(`stderr: ${stderr}`);
+  const errorExists = stderr?.trim() !== '✔';
+  process.exit(errorExists ? 1 : 0);
+};
+exec(command, handleLinguiCommand);
