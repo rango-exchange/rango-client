35e1dbcd4e0b648bd13fbd972719ecb6489822d3
mikasackermn <mikasa@rango.exchange>
2024-11-04 07:30:57 +0000

    feat: add more languages to widget

--- widget/ui/src/components/Flags/index.ts
+++ widget/ui/src/components/Flags/index.ts
@@ -1,20 +1,34 @@
 import Bengali from './Bengali.js';
+import Catalonia from './Catalonia.js';
 import Chinese from './Chinese.js';
+import Cyprus from './Cyprus.js';
+import Denmark from './Denmark.js';
 import English from './English.js';
 import Finland from './Finland.js';
 import French from './French.js';
 import German from './German.js';
 import Greece from './Greece.js';
 import Hindi from './Hindi.js';
+import Hungary from './Hungary.js';
+import India from './India.js';
 import Indonesian from './Indonesian.js';
 import Italian from './Italian.js';
 import Japanese from './Japanese.js';
+import Korea from './Korea.js';
+import Lithuania from './Lithuania.js';
 import Malay from './Malay.js';
 import Netherlands from './Netherlands.js';
+import Pakistan from './Pakistan.js';
+import Philippines from './Philippines.js';
 import Poland from './Poland.js';
 import Portuguese from './Portuguese.js';
 import Russian from './Russian.js';
+import SaudiArabia from './SaudiArabia.js';
+import Serbia from './Serbia.js';
+import Slovakia from './Slovakia.js';
+import SouthAfrica from './SouthAfrica.js';
 import Spanish from './Spanish.js';
+import Swahili from './Swahili.js';
 import Swedish from './Swedish.js';
 import Thai from './Thai.js';
 import Turkish from './Turkish.js';
@@ -46,4 +60,18 @@ export {
   Greece,
   Italian,
   Poland,
+  Catalonia,
+  Cyprus,
+  Denmark,
+  Hungary,
+  India,
+  Korea,
+  Lithuania,
+  Pakistan,
+  Philippines,
+  SaudiArabia,
+  Serbia,
+  Slovakia,
+  SouthAfrica,
+  Swahili,
 };
