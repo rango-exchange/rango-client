ce11ae7ceceee598ded03e140e5489a80af1bcb7
RyukTheCoder <ryuk@rango.exchange>
2024-11-25 12:33:11 +0000

    chore: improve and document code splitting

--- wallets/provider-solflare/src/signer.ts
+++ wallets/provider-solflare/src/signer.ts
@@ -3,11 +3,12 @@ import type { SignerFactory } from 'rango-types';
 
 import { DefaultSignerFactory, TransactionType as TxType } from 'rango-types';
 
+import { CustomSolanaSigner } from './signers/solanaSigner.js';
+
 export default async function getSigners(
   provider: Solflare
 ): Promise<SignerFactory> {
   const signers = new DefaultSignerFactory();
-  const { CustomSolanaSigner } = await import('./signers/solanaSigner.js');
   signers.registerSigner(TxType.SOLANA, new CustomSolanaSigner(provider));
   return signers;
 }
