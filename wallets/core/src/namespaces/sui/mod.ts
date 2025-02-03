8186f042ffc33411138124504dfe931203f7dedc
Eren Yeager <eren@rango.exchange>
2025-02-03 01:11:26 +0000

    feat: sui integration

---
+++ wallets/core/src/namespaces/sui/mod.ts
@@ -0,0 +1,6 @@
+export type { ProviderAPI, SuiActions } from './types.js';
+
+export * as actions from './actions.js';
+export * as builders from './builders.js';
+
+export { CAIP_NAMESPACE, CAIP_SUI_CHAIN_ID } from './constants.js';
