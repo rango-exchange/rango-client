185791ffcc8eadffab9ed9ee0084ba35d9c035ce
RyukTheCoder <ryuk@rango.exchange>
2025-02-04 06:23:47 +0000

    chore: improve wallets-react interface

--- wallets/react/src/index.ts
+++ wallets/react/src/index.ts
@@ -2,7 +2,3 @@ export * from './legacy/helpers.js';
 export { default as Provider } from './provider.js';
 export { useWallets } from './legacy/hooks.js';
 export * from './legacy/types.js';
-export {
-  separateLegacyAndHubProviders,
-  getAllLegacyProviders,
-} from './hub/mod.js';
