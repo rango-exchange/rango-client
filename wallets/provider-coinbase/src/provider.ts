cbce2dafd027e10a4264f617e0807c7de64658f1
arlert-armin <arlert@rango.exchange>
2025-03-04 18:42:34 +0000

    feat: syncing with next and adding eager connect

--- wallets/provider-coinbase/src/provider.ts
+++ wallets/provider-coinbase/src/provider.ts
@@ -5,18 +5,19 @@ import { evm } from './namespaces/evm.js';
 import { solana } from './namespaces/solana.js';
 import { coinbase as coinbaseInstance } from './utils.js';
 
-const provider = new ProviderBuilder(WALLET_ID)
-  .init(function (context) {
-    const [, setState] = context.state();
+const buildProvider = () =>
+  new ProviderBuilder(WALLET_ID)
+    .init(function (context) {
+      const [, setState] = context.state();
 
-    if (coinbaseInstance()) {
-      setState('installed', true);
-      console.debug('[coinbase] instance detected.', context);
-    }
-  })
-  .config('info', info)
-  .add('solana', solana)
-  .add('evm', evm)
-  .build();
+      if (coinbaseInstance()) {
+        setState('installed', true);
+        console.debug('[coinbase] instance detected.', context);
+      }
+    })
+    .config('info', info)
+    .add('solana', solana)
+    .add('evm', evm)
+    .build();
 
-export { provider };
+export { buildProvider };
