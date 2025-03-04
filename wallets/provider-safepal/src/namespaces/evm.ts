9add00e732e0d260b3cc7d5e3536fc14954b7864
arlert-armin <arlert@rango.exchange>
2025-03-04 18:48:18 +0000

    feat: add eager connect

--- wallets/provider-safepal/src/namespaces/evm.ts
+++ wallets/provider-safepal/src/namespaces/evm.ts
@@ -19,8 +19,13 @@ const disconnect = commonBuilders
   .disconnect<EvmActions>()
   .after(changeAccountCleanup)
   .build();
+const canEagerConnect = builders
+  .canEagerConnect()
+  .action(actions.canEagerConnect(evmSafepal))
+  .build();
 const evm = new NamespaceBuilder<EvmActions>('EVM', WALLET_ID)
   .action(connect)
   .action(disconnect)
+  .action(canEagerConnect)
   .build();
 export { evm };
