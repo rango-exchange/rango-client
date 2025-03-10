a6ae19463021c5d253574fb551b5f50db356b004
arlert-armin <arlert@rango.exchange>
2025-03-10 17:28:11 +0000

    feat: moved connect to solana actions

--- wallets/provider-trustwallet/src/namespaces/solana.ts
+++ wallets/provider-trustwallet/src/namespaces/solana.ts
@@ -1,16 +1,8 @@
-import type { CaipAccount } from '@rango-dev/wallets-core/namespaces/common';
 import type { SolanaActions } from '@rango-dev/wallets-core/namespaces/solana';
 
 import { NamespaceBuilder } from '@rango-dev/wallets-core';
 import { builders as commonBuilders } from '@rango-dev/wallets-core/namespaces/common';
-import {
-  actions,
-  builders,
-  CAIP_NAMESPACE,
-  CAIP_SOLANA_CHAIN_ID,
-} from '@rango-dev/wallets-core/namespaces/solana';
-import { CAIP } from '@rango-dev/wallets-core/utils';
-import { getSolanaAccounts } from '@rango-dev/wallets-shared';
+import { actions, builders } from '@rango-dev/wallets-core/namespaces/solana';
 
 import { WALLET_ID } from '../constants.js';
 import { solanaTrustWallet } from '../utils.js';
@@ -20,31 +12,7 @@ const [changeAccountSubscriber, changeAccountCleanup] =
 
 const connect = builders
   .connect()
-  .action(async function () {
-    const solanaInstance = solanaTrustWallet();
-    const result = await getSolanaAccounts({
-      instance: solanaInstance,
-      meta: [],
-    });
-    if (Array.isArray(result)) {
-      throw new Error(
-        'Expecting solana response to be a single value, not an array.'
-      );
-    }
-
-    const formatAccounts = result.accounts.map(
-      (account) =>
-        CAIP.AccountId.format({
-          address: account,
-          chainId: {
-            namespace: CAIP_NAMESPACE,
-            reference: CAIP_SOLANA_CHAIN_ID,
-          },
-        }) as CaipAccount
-    );
-
-    return formatAccounts;
-  })
+  .action(actions.connect(solanaTrustWallet))
   .before(changeAccountSubscriber)
   .or(changeAccountCleanup)
   .build();
