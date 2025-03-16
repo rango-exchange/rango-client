4a5abc326b7aa573506484f28f30c18924e68aa2
arlert-armin <arlert@rango.exchange>
2025-03-16 06:13:52 +0000

    feat: review comments

--- wallets/provider-rabby/src/namespaces/evm.ts
+++ wallets/provider-rabby/src/namespaces/evm.ts
@@ -22,9 +22,15 @@ const disconnect = commonBuilders
   .after(changeAccountCleanup)
   .build();
 
+const canEagerConnect = builders
+  .canEagerConnect()
+  .action(actions.canEagerConnect(evmRabby))
+  .build();
+
 const evm = new NamespaceBuilder<EvmActions>('EVM', WALLET_ID)
   .action(connect)
   .action(disconnect)
+  .action(canEagerConnect)
   .build();
 
 export { evm };
