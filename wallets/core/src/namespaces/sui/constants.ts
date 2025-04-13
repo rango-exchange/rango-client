2869a86d9eef86bbda17ff4eb27c78ae9a6e4474
arlert-armin <arlert@rango.exchange>
2025-04-13 15:53:18 +0000

    feat: update sui to consider recent api changes

--- wallets/core/src/namespaces/sui/constants.ts
+++ wallets/core/src/namespaces/sui/constants.ts
@@ -4,5 +4,5 @@
  */
 
 // if the value neeeds to be change make sure you will update mapCaipNamespaceToLegacyNetworkName as well
-export const CAIP_NAMESPACE = 'move';
+export const CAIP_NAMESPACE = 'sui';
 export const CAIP_SUI_CHAIN_ID = 'sui';
