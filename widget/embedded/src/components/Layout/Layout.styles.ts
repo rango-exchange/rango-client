2fb4aa99b5562183a9623d0a9ca219919a316c5e
Shinji <shinji@rango.exchange>
2025-05-12 13:07:09 +0000

    fix: resolve widget height issues in iframe

--- widget/embedded/src/components/Layout/Layout.styles.ts
+++ widget/embedded/src/components/Layout/Layout.styles.ts
@@ -25,9 +25,7 @@ export const Container = styled('div', {
       fixed: {
         minHeight: WIDGET_MIN_HEIGHT,
         maxHeight: WIDGET_MAX_HEIGHT,
-        '@sm': {
-          height: WIDGET_MAX_HEIGHT,
-        },
+        height: WIDGET_MAX_HEIGHT,
       },
     },
     showBanner: {
