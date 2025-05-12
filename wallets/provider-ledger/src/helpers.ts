0728ac40a67f648d254db2461627b7cd408a28c5
RyukTheCoder <ryuk@rango.exchange>
2025-05-12 12:13:33 +0000

    feat: add derivation path to swap wallets

--- wallets/provider-ledger/src/helpers.ts
+++ wallets/provider-ledger/src/helpers.ts
@@ -1,7 +1,11 @@
 import type Transport from '@ledgerhq/hw-transport';
 
 import { getAltStatusMessage } from '@ledgerhq/errors';
-import { ETHEREUM_CHAIN_ID, Networks } from '@rango-dev/wallets-shared';
+import {
+  ETHEREUM_CHAIN_ID,
+  Networks,
+  type ProviderConnectResult,
+} from '@rango-dev/wallets-shared';
 import bs58 from 'bs58';
 
 import { getDerivationPath } from './state.js';
@@ -26,6 +30,7 @@ function getLedgerErrorMessage(statusCode: number): string {
   )}`; // Hexadecimal numbers are more commonly recognized and utilized for representing ledger error codes
 }
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function getLedgerError(error: any) {
   if (error?.statusCode) {
     return new Error(getLedgerErrorMessage(error.statusCode));
@@ -50,37 +55,34 @@ export function getLedgerInstance() {
   return instances;
 }
 
-export async function getEthereumAccounts(): Promise<{
-  accounts: string[];
-  chainId: string;
-}> {
+export async function getEthereumAccounts(): Promise<ProviderConnectResult> {
   try {
     const transport = await transportConnect();
+    const derivationPath = getDerivationPath();
 
     const eth = new (await import('@ledgerhq/hw-app-eth')).default(transport);
 
     const accounts: string[] = [];
 
-    const result = await eth.getAddress(getDerivationPath(), false, true);
+    const result = await eth.getAddress(derivationPath, false, true);
     accounts.push(result.address);
 
     return {
       accounts: accounts,
       chainId: ETHEREUM_CHAIN_ID,
+      derivationPath,
     };
-  } catch (error: any) {
+  } catch (error) {
     throw getLedgerError(error);
   } finally {
     await transportDisconnect();
   }
 }
 
-export async function getSolanaAccounts(): Promise<{
-  accounts: string[];
-  chainId: string;
-}> {
+export async function getSolanaAccounts(): Promise<ProviderConnectResult> {
   try {
     const transport = await transportConnect();
+    const derivationPath = getDerivationPath();
 
     const solana = new (await import('@ledgerhq/hw-app-solana')).default(
       transport
@@ -88,14 +90,15 @@ export async function getSolanaAccounts(): Promise<{
 
     const accounts: string[] = [];
 
-    const result = await solana.getAddress(getDerivationPath());
+    const result = await solana.getAddress(derivationPath);
     accounts.push(bs58.encode(result.address));
 
     return {
       accounts: accounts,
       chainId: Networks.SOLANA,
+      derivationPath,
     };
-  } catch (error: any) {
+  } catch (error) {
     throw getLedgerError(error);
   } finally {
     await transportDisconnect();
