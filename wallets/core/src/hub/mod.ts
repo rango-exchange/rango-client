4ac5fd8dce8ae703b480416dc6eadeb279208010
arlert-armin <arlert@rango.exchange>
2025-05-27 10:21:20 +0000

    feat: apply reviews

--- wallets/core/src/hub/mod.ts
+++ wallets/core/src/hub/mod.ts
@@ -2,11 +2,7 @@ export type { Subscriber, SubscriberCleanUp } from './namespaces/mod.js';
 export { Namespace } from './namespaces/mod.js';
 
 export { Provider } from './provider/mod.js';
-export type {
-  CommonNamespaces,
-  CommonNamespaceKeys,
-  Context,
-} from './provider/mod.js';
+export type { CommonNamespaces, CommonNamespaceKeys } from './provider/mod.js';
 
 export { Hub } from './hub.js';
 export type { Store, State, ProviderInfo } from './store/mod.js';
