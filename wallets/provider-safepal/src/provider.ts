cb434376ca6cf7c6410fc833ac7248f41e1012fb
arlert-armin <arlert@rango.exchange>
2025-06-01 15:02:28 +0000

    feat: migrate safepal to hub

---
+++ wallets/provider-safepal/src/provider.ts
@@ -0,0 +1,23 @@
+import { ProviderBuilder } from '@rango-dev/wallets-core';
+
+import { info, WALLET_ID } from './constants.js';
+import { evm } from './namespaces/evm.js';
+import { solana } from './namespaces/solana.js';
+import { safepal as safepalInstance } from './utils.js';
+
+const buildProvider = () =>
+  new ProviderBuilder(WALLET_ID)
+    .init(function (context) {
+      const [, setState] = context.state();
+
+      if (safepalInstance()) {
+        setState('installed', true);
+        console.debug('[safepal] instance detected.', context);
+      }
+    })
+    .config('info', info)
+    .add('solana', solana)
+    .add('evm', evm)
+    .build();
+
+export { buildProvider };
