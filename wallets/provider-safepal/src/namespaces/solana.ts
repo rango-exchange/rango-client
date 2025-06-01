ebf6600b3a227ff443b12b56de668f4181741962
arlert-armin <arlert@rango.exchange>
2025-06-01 15:02:28 +0000

    feat: migrate safepal to hub

---
+++ wallets/provider-safepal/src/namespaces/solana.ts
@@ -0,0 +1,47 @@
+import type { CaipAccount } from '@rango-dev/wallets-core/namespaces/common';
+import type { SolanaActions } from '@rango-dev/wallets-core/namespaces/solana';
+
+import { NamespaceBuilder } from '@rango-dev/wallets-core';
+import { builders as commonBuilders } from '@rango-dev/wallets-core/namespaces/common';
+import {
+  actions,
+  builders,
+  CAIP_NAMESPACE,
+  CAIP_SOLANA_CHAIN_ID,
+} from '@rango-dev/wallets-core/namespaces/solana';
+import { CAIP } from '@rango-dev/wallets-core/utils';
+
+import { WALLET_ID } from '../constants.js';
+import { getSolanaAccounts, solanaSafepal } from '../utils.js';
+
+const [changeAccountSubscriber, changeAccountCleanup] =
+  actions.changeAccountSubscriber(solanaSafepal);
+const connect = builders
+  .connect()
+  .action(async function () {
+    const solanaInstance = solanaSafepal();
+    const result = await getSolanaAccounts(solanaInstance);
+    const formatAccounts = result.accounts.map(
+      (account) =>
+        CAIP.AccountId.format({
+          address: account,
+          chainId: {
+            namespace: CAIP_NAMESPACE,
+            reference: CAIP_SOLANA_CHAIN_ID,
+          },
+        }) as CaipAccount
+    );
+    return formatAccounts;
+  })
+  .before(changeAccountSubscriber)
+  .or(changeAccountCleanup)
+  .build();
+const disconnect = commonBuilders
+  .disconnect<SolanaActions>()
+  .after(changeAccountCleanup)
+  .build();
+const solana = new NamespaceBuilder<SolanaActions>('Solana', WALLET_ID)
+  .action(connect)
+  .action(disconnect)
+  .build();
+export { solana };
