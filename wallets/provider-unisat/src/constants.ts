a5392ec2f9dfff9b4fe1439598f903d8fd2550d1
Nikaru <nikaru@rango.exchange>
2025-06-02 10:25:33 +0000

    feat: integrating unisat wallet

---
+++ wallets/provider-unisat/src/constants.ts
@@ -0,0 +1,20 @@
+import { type ProviderInfo } from '@rango-dev/wallets-core';
+
+export const WALLET_ID = 'unisat';
+
+export const info: ProviderInfo = {
+  name: 'UniSat',
+  icon: 'https://raw.githubusercontent.com/rango-exchange/assets/main/wallets/xdefi/icon.svg',
+  extensions: {
+    chrome:
+      'https://chromewebstore.google.com/detail/unisat-wallet/ppbibelpcjmhbdihakflkdcoccbgbkpo',
+    homepage: 'https://unisat.io/',
+  },
+  properties: [
+    {
+      name: 'detached',
+      // if you are adding a new namespace, don't forget to also update `getWalletInfo`
+      value: ['UTXO'],
+    },
+  ],
+};
---
+++ wallets/provider-unisat/src/legacy/index.ts
@@ -0,0 +1,64 @@
+import type { LegacyProviderInterface } from '@rango-dev/wallets-core/legacy';
+import type { WalletInfo } from '@rango-dev/wallets-shared';
+
+import { LegacyNetworks as Networks } from '@rango-dev/wallets-core/legacy';
+import { chains as utxoChains } from '@rango-dev/wallets-core/namespaces/utxo';
+import { type BlockchainMeta, type SignerFactory } from 'rango-types';
+
+import { info, WALLET_ID } from '../constants.js';
+import { type Provider, unisat as unisat_instance } from '../utils.js';
+
+import signer from './signer.js';
+
+const config = {
+  type: WALLET_ID,
+};
+
+export const getInstance = unisat_instance;
+
+const getWalletInfo: (allBlockChains: BlockchainMeta[]) => WalletInfo = (
+  allBlockChains
+) => {
+  const supportedChains: BlockchainMeta[] = [];
+  const btc = allBlockChains.find((chain) => chain.name === Networks.BTC);
+  if (btc) {
+    supportedChains.push(btc);
+  }
+
+  return {
+    name: info.name,
+    img: info.icon,
+    installLink: {
+      CHROME: info.extensions.chrome,
+      DEFAULT: info.extensions.homepage || '',
+    },
+    color: '#4d40c6',
+    // if you are adding a new namespace, don't forget to also update `properties`
+    needsNamespace: {
+      selection: 'multiple',
+      data: [
+        {
+          label: 'BTC',
+          value: 'UTXO',
+          id: 'BTC',
+          chains: [utxoChains.bitcoin],
+        },
+      ],
+    },
+    supportedChains,
+  };
+};
+
+const getSigners: (provider: Provider) => Promise<SignerFactory> = signer;
+
+const buildLegacyProvider: () => LegacyProviderInterface = () => ({
+  config: config,
+  getWalletInfo: getWalletInfo,
+  connect: async () => {
+    return [];
+  },
+  getInstance,
+  getSigners,
+});
+
+export { buildLegacyProvider };
