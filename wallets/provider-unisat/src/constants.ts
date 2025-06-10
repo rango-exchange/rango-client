131807c61f1a2645209e0ea8c2bdcd4f767a3af9
Nikaru <nikaru@rango.exchange>
2025-06-10 16:29:20 +0000

    feat: integrating unisat wallet

---
+++ wallets/provider-unisat/src/constants.ts
@@ -0,0 +1,36 @@
+import type { BlockchainMeta, TransferBlockchainMeta } from 'rango-types';
+
+import { type ProviderInfo } from '@rango-dev/wallets-core';
+import { Networks } from '@rango-dev/wallets-shared';
+
+export const WALLET_ID = 'unisat';
+
+export const info: ProviderInfo = {
+  name: 'UniSat',
+  icon: 'https://raw.githubusercontent.com/rango-exchange/assets/main/wallets/unisat/icon.svg',
+  extensions: {
+    chrome:
+      'https://chromewebstore.google.com/detail/unisat-wallet/ppbibelpcjmhbdihakflkdcoccbgbkpo',
+    homepage: 'https://unisat.io/',
+  },
+  properties: [
+    {
+      name: 'namespaces',
+      value: {
+        selection: 'single',
+        data: [
+          {
+            label: 'BTC',
+            value: 'UTXO',
+            id: 'BTC',
+            getSupportedChains: (allBlockchains: BlockchainMeta[]) =>
+              allBlockchains.filter(
+                (chain): chain is TransferBlockchainMeta =>
+                  chain.name === Networks.BTC
+              ),
+          },
+        ],
+      },
+    },
+  ],
+};
