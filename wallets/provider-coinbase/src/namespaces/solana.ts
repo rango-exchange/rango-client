67170ce70c384e6dd8e2b9c67ed3afc47eab47af
arlert-armin <arlert@rango.exchange>
2025-06-11 08:54:18 +0000

    feat: restructre and docs

--- wallets/provider-coinbase/src/namespaces/solana.ts
+++ wallets/provider-coinbase/src/namespaces/solana.ts
@@ -1,41 +1,19 @@
-import type { CaipAccount } from '@rango-dev/wallets-core/namespaces/common';
 import type { SolanaActions } from '@rango-dev/wallets-core/namespaces/solana';
 
 import { ActionBuilder, NamespaceBuilder } from '@rango-dev/wallets-core';
 import { builders as commonBuilders } from '@rango-dev/wallets-core/namespaces/common';
-import {
-  actions,
-  builders,
-  CAIP_NAMESPACE,
-  CAIP_SOLANA_CHAIN_ID,
-} from '@rango-dev/wallets-core/namespaces/solana';
-import { CAIP } from '@rango-dev/wallets-core/utils';
+import { actions, builders } from '@rango-dev/wallets-core/namespaces/solana';
 
+import { solanaActions } from '../actions/solana.js';
 import { WALLET_ID } from '../constants.js';
-import { evmCoinbase, getSolanaAccounts, solanaCoinbase } from '../utils.js';
+import { solanaCoinbase } from '../utils.js';
 
 const [changeAccountSubscriber, changeAccountCleanup] =
   actions.changeAccountSubscriber(solanaCoinbase);
 
 const connect = builders
   .connect()
-  .action(async function () {
-    const solanaInstance = solanaCoinbase();
-    const result = await getSolanaAccounts(solanaInstance);
-
-    const formatAccounts = result.accounts.map(
-      (account) =>
-        CAIP.AccountId.format({
-          address: account,
-          chainId: {
-            namespace: CAIP_NAMESPACE,
-            reference: CAIP_SOLANA_CHAIN_ID,
-          },
-        }) as CaipAccount
-    );
-
-    return formatAccounts;
-  })
+  .action(actions.connect(solanaCoinbase))
   .before(changeAccountSubscriber)
   .or(changeAccountCleanup)
   .build();
@@ -44,43 +22,11 @@ const disconnect = commonBuilders
   .disconnect<SolanaActions>()
   .after(changeAccountCleanup)
   .build();
-/*
- * Coinbase does not provide an eager connection mechanism for its Solana wallet.
- * The only workaround is to use the EVM-based eager connect approach.
- * However, this will only work if the EVM namespace has been connected at least once before.
- * If either wallet instance is unavailable, we throw an error.
- */
-export const canEagerConnectAction = async () => {
-  const evmInstance = evmCoinbase();
-  const solanaInstance = solanaCoinbase();
-  // Making sure that the Solana instance is available first to prevent errors.
-  if (!solanaInstance) {
-    throw new Error(
-      'Trying to eagerly connect to your wallet, but seems its solana instance is not available.'
-    );
-  }
-  if (!evmInstance) {
-    throw new Error(
-      'Trying to eagerly connect to your EVM wallet, but seems its instance is not available.'
-    );
-  }
 
-  try {
-    const accounts: string[] = await evmInstance.request({
-      method: 'eth_accounts',
-    });
-    if (accounts.length) {
-      return true;
-    }
-    return false;
-  } catch {
-    return false;
-  }
-};
 const canEagerConnect = new ActionBuilder<SolanaActions, 'canEagerConnect'>(
   'canEagerConnect'
 )
-  .action(canEagerConnectAction)
+  .action(solanaActions.canEagerConnectAction)
   .build();
 
 const solana = new NamespaceBuilder<SolanaActions>('Solana', WALLET_ID)
