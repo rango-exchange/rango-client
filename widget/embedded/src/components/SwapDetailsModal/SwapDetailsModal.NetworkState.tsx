2f0387e83e88dda191f6e033a47369b078f3675f
RyukTheCoder <ryuk@rango.exchange>
2025-07-01 11:50:21 +0000

    feat: improve swap details page switch network modal

--- widget/embedded/src/components/SwapDetailsModal/SwapDetailsModal.NetworkState.tsx
+++ widget/embedded/src/components/SwapDetailsModal/SwapDetailsModal.NetworkState.tsx
@@ -1,17 +1,30 @@
 import type { NetworkStateContentProps } from './SwapDetailsModal.types';
 
 import { i18n } from '@lingui/core';
-import { MessageBox } from '@rango-dev/ui';
+import { Button, Divider, MessageBox } from '@rango-dev/ui';
 import React from 'react';
 
 export const NetworkStateContent = (props: NetworkStateContentProps) => {
-  const { status, message } = props;
+  const { switchNetworkModalState, handleSwitchNetwork } = props;
 
-  const type = status === 'waitingForNetworkChange' ? 'loading' : 'success';
-  const title =
-    status === 'waitingForNetworkChange'
-      ? i18n.t('Change Network')
-      : i18n.t('Network Changed');
-
-  return <MessageBox type={type} title={title} description={message} />;
+  return (
+    <>
+      <MessageBox
+        type={switchNetworkModalState.type}
+        title={switchNetworkModalState.title}
+        description={switchNetworkModalState.description}
+      />
+      {switchNetworkModalState.type === 'error' && (
+        <>
+          <Divider size="30" />
+          <Button
+            id="widget-switch-network-try-again"
+            type="primary"
+            onClick={handleSwitchNetwork}>
+            {i18n.t('Try again')}
+          </Button>
+        </>
+      )}
+    </>
+  );
 };
