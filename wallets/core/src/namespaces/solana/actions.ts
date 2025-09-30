8030bf2e6d6c01457e64097b905048a1fd10f7ce
arlert-armin <arlert@rango.exchange>
2025-09-30 12:25:30 +0000

    refactor: use the format accounts util for connect on solana

--- wallets/core/src/namespaces/solana/actions.ts
+++ wallets/core/src/namespaces/solana/actions.ts
@@ -1,14 +1,10 @@
 import type { ProviderAPI, SolanaActions } from './types.js';
 import type { Context } from '../../hub/namespaces/mod.js';
-import type { CaipAccount } from '../../types/accounts.js';
 import type { FunctionWithContext } from '../../types/actions.js';
-
-import { AccountId } from 'caip';
 
 import { recommended as commonRecommended } from '../common/actions.js';
 
-import { CAIP_NAMESPACE, CAIP_SOLANA_CHAIN_ID } from './constants.js';
-import { getAccounts } from './utils.js';
+import { formatAccountsToCAIP, getAccounts } from './utils.js';
 
 export const recommended = [...commonRecommended];
 
@@ -25,15 +21,6 @@ export function connect(
       );
     }
 
-    return result.accounts.map(
-      (account) =>
-        AccountId.format({
-          address: account,
-          chainId: {
-            namespace: CAIP_NAMESPACE,
-            reference: CAIP_SOLANA_CHAIN_ID,
-          },
-        }) as CaipAccount
-    );
+    return formatAccountsToCAIP(result.accounts);
   };
 }
