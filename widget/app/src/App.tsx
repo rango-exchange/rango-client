977d6d516d8fd36e3890afcceebfda87b2d7abd9
RyukTheCoder <ryuk@rango.exchange>
2025-11-15 12:49:37 +0000

    chore: update api url

--- widget/app/src/App.tsx
+++ widget/app/src/App.tsx
@@ -38,7 +38,7 @@ export function App() {
 
       config = {
         apiKey: '4a624ab5-16ff-4f96-90b7-ab00ddfc342c',
-        apiUrl: 'http://127.0.0.1:8080',
+        apiUrl: 'https://api-104.rng-dev.online',
         walletConnectProjectId: WC_PROJECT_ID,
         trezorManifest: TREZOR_MANIFEST,
         tonConnect: { manifestUrl: TON_CONNECT_MANIFEST_URL },
