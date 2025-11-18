4a2fb70c2311dd7d451e2eafa8f0e29c5b30bbce
arlert-armin <arlert@rango.exchange>
2025-11-18 22:23:08 +0000

    feat: add tron namespace

---
+++ wallets/core/src/namespaces/tron/before.ts
@@ -0,0 +1,3 @@
+import { recommended as commonRecommended } from '../common/before.js';
+
+export const recommended = [...commonRecommended];
---
+++ wallets/core/src/namespaces/tron/constants.ts
@@ -0,0 +1,8 @@
+/*
+ * TODO: I couldn't found tron in caip
+ * @see https://namespaces.chainagnostic.org/
+ */
+
+// if the value needs to be change make sure you will update mapCaipNamespaceToLegacyNetworkName as well
+export const CAIP_NAMESPACE = 'tron';
+export const CAIP_TRON_CHAIN_ID = 'tron';
