53d772adef8af4760853e964f902c25f360fd80c
arlert-armin <arlert@rango.exchange>
2025-11-21 13:59:13 +0000

    feat: migrate braavos to the hub

---
+++ wallets/provider-braavos/src/actions/starknet.ts
@@ -0,0 +1,36 @@
+import type { Context, FunctionWithContext } from '@rango-dev/wallets-core';
+
+import {
+  CAIP_STARKNET_CHAIN_ID,
+  type ProviderAPI,
+  type StarknetActions,
+  utils,
+} from '@rango-dev/wallets-core/namespaces/starknet';
+
+export function connect(
+  instance: () => ProviderAPI
+): FunctionWithContext<StarknetActions['connect'], Context> {
+  return async () => {
+    const starknetInstance = instance();
+
+    const connectResult = await starknetInstance?.enable();
+    if (
+      !connectResult ||
+      !starknetInstance.isConnected ||
+      !connectResult?.length
+    ) {
+      throw new Error('Error during connection');
+    }
+    if (
+      starknetInstance?.chainId &&
+      starknetInstance.chainId !== CAIP_STARKNET_CHAIN_ID
+    ) {
+      throw new Error(
+        `Please switch to Mainnet, current network is ${starknetInstance?.chainId}`
+      );
+    }
+
+    return utils.formatAccountsToCAIP(connectResult);
+  };
+}
+export const starknetActions = { connect };
