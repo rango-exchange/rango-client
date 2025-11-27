fe6eeb3bba00a14d05e83e586d64df92fd0058cd
arlert-armin <arlert@rango.exchange>
2025-11-27 07:31:03 +0000

    feat: migrate leap cosmos to the hub

---
+++ wallets/provider-leap-cosmos/src/builders/cosmos.ts
@@ -0,0 +1,31 @@
+import type {
+  CosmosActions,
+  ProviderAPI,
+} from '@rango-dev/wallets-core/namespaces/cosmos';
+
+import { ChangeAccountSubscriberBuilder } from '@rango-dev/wallets-core/namespaces/common';
+import { utils } from '@rango-dev/wallets-core/namespaces/cosmos';
+
+// Hooks
+const changeAccountSubscriber = (getInstance: () => ProviderAPI) =>
+  new ChangeAccountSubscriberBuilder<Event, ProviderAPI, CosmosActions>()
+    .getInstance(getInstance)
+    .onSwitchAccount((event, context) => {
+      const [getState] = context.state();
+      event.preventDefault();
+      const connectArgs = getState('connectArgs');
+      if (!connectArgs) {
+        throw new Error('Connect args are empty');
+      }
+      context.action('disconnect');
+      void context.action('connect', connectArgs.options);
+    })
+    .format(async (_, __) => utils.formatAccountsToCAIP([]))
+    .addEventListener((_, callback) => {
+      window.addEventListener('leap_keystorechange', callback);
+    })
+    .removeEventListener((_, callback) => {
+      window.removeEventListener('leap_keystorechange', callback);
+    });
+
+export const cosmosBuilders = { changeAccountSubscriber };
