90b9baaabbc594c518137ae6fa9d12605558a3df
RyukTheCoder <ryuk@rango.exchange>
2025-11-29 09:32:30 +0000

    feat: enable forced namespaces in useStatefulConnect hook

--- widget/embedded/src/hooks/useStatefulConnect/useStatefulConnect.types.ts
+++ widget/embedded/src/hooks/useStatefulConnect/useStatefulConnect.types.ts
@@ -3,6 +3,7 @@ import type { Namespace } from '@rango-dev/wallets-core/namespaces/common';
 
 export interface HandleConnectOptions {
   // To have a switch between connect and disconnect when user is clicking on a button, this option can be helpful.
+  forceConnectToNamespaces?: Namespace[];
   disconnectIfConnected?: boolean;
   defaultSelectedChains?: string[];
 }
