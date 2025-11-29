66c4ce7accc051f34c167a0e346e8a5c8da4dbf9
arlert-armin <arlert@rango.exchange>
2025-11-29 15:35:19 +0000

    feat: add btc to bitget wallet

--- wallets/provider-bitget/src/signer.ts
+++ wallets/provider-bitget/src/signer.ts
@@ -10,11 +10,15 @@ import {
 } from '@rango-dev/wallets-shared';
 import { DefaultSignerFactory, TransactionType as TxType } from 'rango-types';
 
+import { BitgetUTXOSigner } from './signers/utxo.js';
+
 export default async function getSigners(
   provider: LegacyNetworkProviderMap
 ): Promise<SignerFactory> {
   const ethProvider = getNetworkInstance(provider, LegacyNetworks.ETHEREUM);
   const tronProvider = getNetworkInstance(provider, LegacyNetworks.TRON);
+  const utxoProvider = getNetworkInstance(provider, LegacyNetworks.BTC);
+
   const signers = new DefaultSignerFactory();
   const { DefaultEvmSigner } = await dynamicImportWithRefinedError(
     async () => await import('@rango-dev/signer-evm')
@@ -24,5 +28,7 @@ export default async function getSigners(
   );
   signers.registerSigner(TxType.EVM, new DefaultEvmSigner(ethProvider));
   signers.registerSigner(TxType.TRON, new DefaultTronSigner(tronProvider));
+  signers.registerSigner(TxType.TRANSFER, new BitgetUTXOSigner(utxoProvider));
+
   return signers;
 }
