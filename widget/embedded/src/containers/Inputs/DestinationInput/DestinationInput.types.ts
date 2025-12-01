614aba75a1c22ea610f7658ea87e77c0562aa21d
Shinji <shinji@rango.exchange>
2025-12-01 09:52:41 +0000

    feat: implement flow for connecting destination wallet

---
+++ widget/embedded/src/containers/Inputs/DestinationInput/DestinationInput.types.ts
@@ -0,0 +1,5 @@
+export type PropTypes = {
+  fetchingQuote: boolean;
+  isExpandable?: boolean;
+  onClickToken: () => void;
+};
--- widget/embedded/src/containers/Inputs/Inputs.tsx
+++ widget/embedded/src/containers/Inputs/Inputs.tsx
@@ -1,102 +1,21 @@
 import type { PropTypes } from './Inputs.types';
 
-import { i18n } from '@lingui/core';
-import { SwapInput } from '@rango-dev/ui';
 import React from 'react';
 
-import { errorMessages } from '../../constants/errors';
-import {
-  PERCENTAGE_CHANGE_MAX_DECIMALS,
-  PERCENTAGE_CHANGE_MIN_DECIMALS,
-  TOKEN_AMOUNT_MAX_DECIMALS,
-  TOKEN_AMOUNT_MIN_DECIMALS,
-  USD_VALUE_MAX_DECIMALS,
-  USD_VALUE_MIN_DECIMALS,
-} from '../../constants/routing';
-import { useSwapMode } from '../../hooks/useSwapMode';
-import { useQuoteStore } from '../../store/quote';
-import { getContainer } from '../../utils/common';
-import { numberToString } from '../../utils/numbers';
-import { getPriceImpact, getPriceImpactLevel } from '../../utils/quote';
-import { canComputePriceImpact } from '../../utils/swap';
-
+import { DestinationInput } from './DestinationInput/DestinationInput';
 import { Container } from './Inputs.styles';
 import { SourceInput } from './SourceInput/SourceInput';
 
 export function Inputs(props: PropTypes) {
-  const { fetchingQuote, fetchMetaStatus, onClickToken, isExpandable } = props;
-  const {
-    toToken,
-    toBlockchain,
-    inputAmount,
-    inputUsdValue,
-    outputAmount,
-    outputUsdValue,
-    selectedQuote,
-  } = useQuoteStore()();
-  const { swapMode } = useSwapMode();
-
-  const priceImpactOutputCanNotBeComputed = !canComputePriceImpact(
-    selectedQuote,
-    inputAmount,
-    outputUsdValue
-  );
-
-  const percentageChange =
-    !inputUsdValue || !outputUsdValue || !outputUsdValue.gt(0)
-      ? null
-      : getPriceImpact(inputUsdValue.toString(), outputUsdValue.toString());
+  const { onClickToken, fetchingQuote, isExpandable } = props;
 
   return (
     <Container>
       <SourceInput onClickToken={() => onClickToken('from')} />
-      <SwapInput
-        selectionType={swapMode === 'swap' ? 'token' : 'chain'}
-        sharpBottomStyle={!isExpandable && (!!selectedQuote || fetchingQuote)}
-        label={i18n.t('To')}
-        mode="To"
-        id="widget-swap-to-input"
+      <DestinationInput
         fetchingQuote={fetchingQuote}
-        chain={{
-          displayName: toBlockchain?.displayName || '',
-          image: toBlockchain?.logo,
-        }}
-        token={{
-          displayName: toToken?.symbol || '',
-          image: toToken?.image,
-          securityWarning: !!toToken?.warning,
-        }}
-        percentageChange={numberToString(
-          getPriceImpact(inputUsdValue, outputUsdValue),
-          PERCENTAGE_CHANGE_MIN_DECIMALS,
-          PERCENTAGE_CHANGE_MAX_DECIMALS
-        )}
-        warningLevel={getPriceImpactLevel(percentageChange ?? 0)}
-        price={{
-          value: numberToString(
-            outputAmount,
-            TOKEN_AMOUNT_MIN_DECIMALS,
-            TOKEN_AMOUNT_MAX_DECIMALS
-          ),
-          usdValue: priceImpactOutputCanNotBeComputed
-            ? undefined
-            : numberToString(
-                outputUsdValue,
-                USD_VALUE_MIN_DECIMALS,
-                USD_VALUE_MAX_DECIMALS
-              ),
-          realValue: outputAmount?.toString(),
-          realUsdValue: priceImpactOutputCanNotBeComputed
-            ? undefined
-            : outputUsdValue?.toString(),
-          error: priceImpactOutputCanNotBeComputed
-            ? errorMessages().unknownPriceError.impactTitle
-            : undefined,
-        }}
+        isExpandable={isExpandable}
         onClickToken={() => onClickToken('to')}
-        disabled={fetchMetaStatus === 'failed'}
-        loading={fetchMetaStatus === 'loading'}
-        tooltipContainer={getContainer()}
       />
     </Container>
   );
