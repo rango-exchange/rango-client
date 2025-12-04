2b1539ff01b6c270503a5d84fccaee064592a367
arlert-armin <arlert@rango.exchange>
2025-12-04 03:03:38 +0000

    feat: add cosmos namespace to the hub

--- wallets/core/src/namespaces/cosmos/types.ts
+++ wallets/core/src/namespaces/cosmos/types.ts
@@ -1,3 +1,4 @@
+import type { Accounts } from '../common/mod.js';
 import type {
   AutoImplementedActionsByRecommended,
   CommonActions,
@@ -6,10 +7,20 @@ import type {
 export interface CosmosActions
   extends AutoImplementedActionsByRecommended,
     CommonActions {
-  // TODO
-  connect: () => Promise<string>;
+  connect: (options?: ConnectOptions) => Promise<Accounts>;
   canEagerConnect: () => Promise<boolean>;
 }
-
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type ProviderAPI = Record<string, any>;
+export type CosmosChainAccounts = {
+  accounts: string[];
+  chainId: string;
+};
+
+export type ConnectOptions = {
+  chainIds: string[];
+  /**
+   * For chains that may not be natively supported by the provider.
+   */
+  customChainIds?: string[];
+};
