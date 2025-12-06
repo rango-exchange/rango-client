08598ccdaf209e47a94c4cea49bcae2aac0fa5f8
arlert-armin <arlert@rango.exchange>
2025-12-06 14:14:02 +0000

    feat: migrate keplr to the hub

--- wallets/provider-keplr/src/helpers.ts
+++
@@ -1,4 +0,0 @@
-// Note: for unknown reason CTRL wallet extension is overiding any function named `keplr`. so make sure you will not use this name for any function!
-export function getKeplrInstance() {
-  return window.keplr || null;
-}
