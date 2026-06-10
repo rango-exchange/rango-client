7b650ec1b0020610fac015e2e2cc2220e810a456
llawliet-l-l <l.lawliet@rango.exchange>
2026-06-10 08:59:01 +0000

    fix(wallets): modified provider type

--- wallets/provider-default/src/types.ts
+++ wallets/provider-default/src/types.ts
@@ -1,2 +1,11 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type Provider = Record<string, any>;
+import type { ProviderAPI as EvmProviderApi } from '@hub3js/evm';
+import type { LegacyNetworks } from '@rango-dev/wallets-core/legacy';
+
+type ProviderObject = {
+  [LegacyNetworks.ETHEREUM]: EvmProviderApi;
+};
+
+export type Provider = Map<
+  keyof ProviderObject,
+  ProviderObject[keyof ProviderObject]
+>;
