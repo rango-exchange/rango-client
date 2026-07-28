c826d30342af2242868a4466f8aa53c2a6105502
llawliet-l-l <l.lawliet@rango.exchange>
2026-07-28 13:37:23 +0000

    build: use rangutopia CLI to build library packages

--- scripts/common/constants.mjs
+++ scripts/common/constants.mjs
@@ -1,6 +1,5 @@
 /** A fixed subject for using in publish commit. */
 export const PUBLISH_COMMIT_SUBJECT = 'chore(release): publish';
 export const NPM_ORG_NAME = '@rango-dev';
-export const BUILD_META_FILE_SUFFIX = '.build.json';
 export const ROOT_VERSIONS_COMMIT_SUBJECT =
   'chore(release): bump the repo and client versions';
